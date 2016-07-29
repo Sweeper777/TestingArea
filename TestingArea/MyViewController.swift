4ae51abbc9b05b6e5081d1ca625e2d7ff3a2266f
Sweeper777 <sumulang@gmail.com>
2016-07-29 08:10:59 +0800

    tested AVAudioPlayer

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,43 +1,20 @@
 import UIKit
-import GoogleMobileAds
+import AVFoundation
+import EZSwiftExtensions
 
-class MyViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    @IBOutlet var picker: UIPickerView!
+class MyViewController: UIViewController {
+    @IBOutlet var lbl: UILabel!
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        let player = try! AVAudioPlayer(contentsOfURL: NSBundle.mainBundle().URLForResource("beep", withExtension: "mp3", subdirectory: nil, localization: nil)!)
+        player.prepareToPlay()
         
-    }
-    
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 3
-    }
-    
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        switch component {
-        case 0:
-            return 25
-        case 1, 2:
-            return 60
-        default:
-            return 0
+        NSTimer.runThisEvery(seconds: 1) {
+            _ in
+            self.lbl.hidden = !self.lbl.hidden
+            player.play()
+            player.prepareToPlay()
         }
-    }
-    
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        var suffix = ""
-        
-        switch component {
-        case 0:
-            suffix = " h"
-        case 1:
-            suffix = " m"
-        case 2:
-            suffix = " s"
-        default:
-            suffix = ""
-        }
-        
-        return String(row) + suffix
     }
 }
