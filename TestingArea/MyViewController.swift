c8eeab540d917b12e37efc19c1ffa67d4a10312b
Sweeper777 <sumulang@gmail.com>
2016-08-02 12:53:29 +0800

    briefly tested XLForm

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,20 +1,29 @@
 import UIKit
-import AVFoundation
 import EZSwiftExtensions
+import XLForm
 
-class MyViewController: UIViewController {
-    @IBOutlet var lbl: UILabel!
+class MyViewController: XLFormViewController {
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initializeForm()
+    }
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        let player = try! AVAudioPlayer(contentsOfURL: NSBundle.mainBundle().URLForResource("beep", withExtension: "mp3", subdirectory: nil, localization: nil)!)
-        player.prepareToPlay()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.initializeForm()
+    }
+    
+    func initializeForm() {
+        let form = XLFormDescriptor(title: "Add Event")
         
-        NSTimer.runThisEvery(seconds: 1) {
-            _ in
-            self.lbl.hidden = !self.lbl.hidden
-            player.play()
-            player.prepareToPlay()
-        }
+        var section = XLFormSectionDescriptor()
+        form.addFormSection(section)
+        
+        var row = XLFormRowDescriptor(tag: "title", rowType: XLFormRowDescriptorTypeText, title: "Title")
+        row.cellConfigAtConfigure.setObject("Hello", forKey: "textField.text")
+        section.addFormRow(row)
+        
+        self.form = form
     }
 }
