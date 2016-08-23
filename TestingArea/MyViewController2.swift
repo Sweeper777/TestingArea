907a42f189f8dc86085fc257ad8a84cc6dc0f92d
Sweeper777 <sumulang@gmail.com>
2016-08-23 10:13:55 +0100

    tested ISHHoverBar

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,11 +1,23 @@
 import UIKit
 import AVFoundation
 import EZSwiftExtensions
+import ISHHoverBar
 
 class MyViewController2: UIViewController {
     @IBOutlet var textField: UITextField!
     @IBOutlet var recognizer: UISwipeGestureRecognizer!
+    
+    @IBOutlet var item1: UIBarButtonItem!
+    @IBOutlet var item2: UIBarButtonItem!
+    
     @IBAction func item(sender: UIBarButtonItem) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        let bar = ISHHoverBar(frame: CGRectMake(20, 200, 44, 88))
+        bar.orientation = .Vertical
+        bar.items = [item1, item2]
+        view.addSubview(bar)
     }
 }
