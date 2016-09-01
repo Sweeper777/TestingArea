707a28fe501c320c3dab7da9ee30a1f591808fc7
Sweeper777 <sumulang@gmail.com>
2016-09-01 09:06:27 +0100

    tested synchronised timer label

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -9,10 +9,45 @@ class MyViewController: UIViewController, TouchDrawViewDelegate {
         draw.layer.borderWidth = 3
         draw.delegate = self
         draw.setWidth(3)
+        formatter.dateFormat = "hh:mm"
     }
     
+    let formatter = NSDateFormatter()
     
+    var timer: NSTimer?
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        let date = NSDate()
+        label.text = formatter.stringFromDate(date)
+        let secondsUntilMinute = 60 - NSCalendar.currentCalendar().component(.Second, fromDate: date)
+        if secondsUntilMinute != 0 {
+            NSTimer.runThisAfterDelay(seconds: Double(secondsUntilMinute)) {
+                [weak self] in
+                let date = NSDate()
+                self?.label.text = self?.formatter.stringFromDate(date)
+                if let myself = self {
+                    myself.timer = NSTimer.runThisEvery(seconds: 60) {
+                        [weak self] t in
+                        if self == nil {
+                            t.invalidate()
+                        }
+                        let date = NSDate()
+                        self?.label.text = self?.formatter.stringFromDate(date)
+                    }
+                }
+            }
+        } else {
+            self.timer = NSTimer.runThisEvery(seconds: 60) {
+                [weak self] t in
+                if self == nil {
+                    t.invalidate()
+                }
+                let date = NSDate()
+                self?.label.text = self?.formatter.stringFromDate(date)
+            }
+        }
+    }
     
     override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
         draw.clearDrawing()
