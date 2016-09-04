07f8f78ccf2945e355beabf14ec006705dff13e8
Sweeper777 <sumulang@gmail.com>
2016-09-04 09:29:26 +0100

    tested add annotation

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -6,7 +6,10 @@ class MyViewController: UIViewController {
     @IBOutlet var map: MKMapView!
     
     override func viewDidLoad() {
-        
+        map.centerCoordinate = CLLocationCoordinate2DMake(25, 133)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2DMake(25, 133)
+        map.addAnnotation(annotation)
     }
     
     override func viewDidAppear(animated: Bool) {
