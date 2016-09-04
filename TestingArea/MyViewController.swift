da80d5e468bcf1ede9863109684b7503d013714b
Sweeper777 <sumulang@gmail.com>
2016-09-04 09:12:26 +0100

    started testing mapkit

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,47 +1,15 @@
 import UIKit
+import MapKit
 import EZSwiftExtensions
-import TouchDraw
 
-class MyViewController: UIViewController, TouchDrawViewDelegate {
-    @IBOutlet var draw: TouchDrawView!
-    @IBOutlet var image: UIImageView!
+class MyViewController: UIViewController {
+    @IBOutlet var map: MKMapView!
+    
     override func viewDidLoad() {
-        draw.layer.borderWidth = 3
-        draw.delegate = self
-        draw.setWidth(3)
+        
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-    }
-    
-    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
-        let stack = draw.exportStack()
-        let data = NSKeyedArchiver.archivedDataWithRootObject(stack)
-        print(data.length)
-    }
-    
-    func undoEnabled() {
-        
-    }
-    
-    func undoDisabled() {
-        
-    }
-    
-    func redoEnabled() {
-        
-    }
-    
-    func redoDisabled() {
-        
-    }
-    
-    func clearEnabled() {
-        
-    }
-    
-    func clearDisabled() {
-        
     }
 }
