7c637ba770f86b99cb687408b601a12c445bb1b0
Sweeper777 <sumulang@gmail.com>
2016-09-05 08:19:49 +0100

    tested some OP's code

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -2,17 +2,22 @@ import UIKit
 import MapKit
 import EZSwiftExtensions
 
-class MyViewController: UIViewController {
-    @IBOutlet var map: MKMapView!
+class MyViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
-        map.centerCoordinate = CLLocationCoordinate2DMake(25, 133)
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2DMake(25, 133)
-        map.addAnnotation(annotation)
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        let datePicker = UIDatePicker()
+        textField.inputView = datePicker
+        datePicker.addTarget(self, action: #selector(datePickerChanged), forControlEvents: .ValueChanged)
+    }
+    
+    func datePickerChanged(sender: UIDatePicker){
+        print("Hello")
     }
 }
