a0ee96f4e4a11825a994980f8d207d1df3438dcf
Sweeper777 <sumulang@gmail.com>
2016-09-07 21:02:57 +0100

    tested adding markers

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -5,10 +5,13 @@ import GoogleMaps
 
 class MyViewController: UIViewController {
     override func viewDidLoad() {
-        let camera = GMSCameraPosition.cameraWithLatitude(0, longitude: 0, zoom: 6)
+        let camera = GMSCameraPosition.cameraWithLatitude(0, longitude: 0, zoom: 3)
         let mapView = GMSMapView.mapWithFrame(CGRect.zero, camera: camera)
         mapView.myLocationEnabled = true
         view = mapView
+        let marker = GMSMarker(position: CLLocationCoordinate2DMake(0, 0))
+        marker.snippet = "Hello World"
+        marker.map = mapView
     }
     
     override func viewDidAppear(animated: Bool) {
