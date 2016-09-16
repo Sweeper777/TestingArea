deee0c685c1c4baba0fb3115d535daff5fca7741
Sweeper777 <sumulang@gmail.com>
2016-09-16 16:48:38 +0100

    tested popover controller on map view

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,19 +1,26 @@
 import UIKit
+import GoogleMaps
 
 class MyViewController: UIViewController {
-    @IBOutlet var myView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let camera = GMSCameraPosition.cameraWithLatitude(0, longitude: 0, zoom: 3)
+        let mapView = GMSMapView.mapWithFrame(CGRect.zero, camera: camera)
+        mapView.myLocationEnabled = true
+        view = mapView
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        performSegueWithIdentifier("show", sender: self)
-        NSTimer.runThisAfterDelay(seconds: 3) {
-            [weak self] in
-            self?.myView.hidden = true
-        }
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewControllerWithIdentifier("VC")
+        vc.modalInPopover = true
+        vc.modalPresentationStyle = .Popover
+        
+        vc.popoverPresentationController!.sourceRect = CGRectMake(100, 100, 0, 0)
+        vc.popoverPresentationController?.sourceView = self.view
+        
+        self.presentVC(vc)
     }
 }
