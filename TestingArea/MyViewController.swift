231cd8311b03bb7f423d5215dd1337cf58c92064
Sweeper777 <sumulang@gmail.com>
2016-09-19 19:23:26 +0100

    tested GMSMapView settings

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -3,28 +3,15 @@ import GoogleMaps
 import TouchDraw
 
 class MyViewController: UIViewController {
-    @IBOutlet var touchDrawView: TouchDrawView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        touchDrawView.layer.borderWidth = 3
-        touchDrawView.setWidth(2)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
         
-    }
-    @IBAction func export(_ sender: AnyObject) {
-        let stack = touchDrawView.exportStack()
-        let data = NSKeyedArchiver.archivedData(withRootObject: stack)
-        UserDefaults.standard.set(data, forKey: "drawing")
-        touchDrawView.clearDrawing()
-    }
-    
-    @IBAction func `import`(_ sender: AnyObject) {
-        let data = UserDefaults.standard.data(forKey: "drawing")!
-        let stack = NSKeyedUnarchiver.unarchiveObject(with: data) as! [Stroke]
-        touchDrawView.importStack(stack)
+        let map = GMSMapView()
+        self.view = map
+        map.settings.myLocationButton = true
+        map.settings.setAllGesturesEnabled(true)
+        map.settings.compassButton = true
+        map.isMyLocationEnabled = true
+        map.mapType = kGMSTypeSatellite
     }
 }
