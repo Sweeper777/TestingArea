36b3610caf42b480a7636a7e510e59097a2f5d2b
Sweeper777 <sumulang@gmail.com>
2016-09-22 13:17:39 +0100

    tested panorama with marker

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -7,5 +7,7 @@ class MyViewController: UIViewController, GMSMapViewDelegate {
         super.viewDidLoad()
         let panorama = GMSPanoramaView.panorama(withFrame: self.view.frame, nearCoordinate: CLLocationCoordinate2D(latitude: 22.287, longitude: 114.138))
         self.view = panorama
+        let marker = GMSMarker(position: CLLocationCoordinate2D(latitude: 22.287, longitude: 114.138))
+        marker.panoramaView = self.view as! GMSPanoramaView
     }
 }
