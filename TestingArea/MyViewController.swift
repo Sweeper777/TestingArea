2b1e19e78a69cca410919e666c50a98412bbd29d
Sweeper777 <sumulang@gmail.com>
2016-09-22 08:24:28 +0100

    tested panorama view

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -5,18 +5,7 @@ import TouchDraw
 class MyViewController: UIViewController, GMSMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let map = GMSMapView()
-        self.view = map
-        map.settings.myLocationButton = true
-        map.settings.setAllGesturesEnabled(true)
-        map.settings.compassButton = true
-        map.isMyLocationEnabled = true
-        map.mapType = kGMSTypeSatellite
-        map.camera = GMSCameraPosition(target: map.camera.target, zoom: 16, bearing: 0, viewingAngle: 65)
-        let marker = GMSMarker(position: map.camera.target)
-        marker.isFlat = false
-        marker.rotation = 90
-        marker.map = map
+        let panorama = GMSPanoramaView.panorama(withFrame: self.view.frame, nearCoordinate: CLLocationCoordinate2D(latitude: 22.287, longitude: 114.138))
+        self.view = panorama
     }
 }
