78e0f7d81e73c9ab9bdfe91b8da0dc5581a19f35
Sweeper777 <sumulang@gmail.com>
2016-10-20 06:58:35 +0100

    tested watch kit table view

--- Watch Extension/InterfaceController.swift
+++ Watch Extension/InterfaceController.swift
@@ -11,12 +11,22 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-    var number = 0
-
-    @IBOutlet var label: WKInterfaceLabel!
-    @IBAction func buttonClicked() {
-        label.setText("The number is \(number)")
-        number += 1
+    @IBOutlet var table: WKInterfaceTable!
+    
+    override init() {
+        super.init()
+        table.setNumberOfRows(4, withRowType: "MyRow1")
+        let row2 = table.rowController(at: 1) as! MyRowController
+        let row3 = table.rowController(at: 2) as! MyRowController
+        let row4 = table.rowController(at: 3) as! MyRowController
+        row2.btn1.setTitle("4")
+        row2.btn2.setTitle("5")
+        row2.btn3.setTitle("6")
+        row3.btn1.setTitle("7")
+        row3.btn2.setTitle("8")
+        row3.btn3.setTitle("9")
+        row4.btn1.setTitle(".")
+        row4.btn2.setTitle("0")
+        row4.btn3.setTitle("←")
     }
-
 }
---
+++ Watch Extension/MyRowController.swift
@@ -0,0 +1,7 @@
+import WatchKit
+
+class MyRowController: NSObject {
+    @IBOutlet var btn1: WKInterfaceButton!
+    @IBOutlet var btn2: WKInterfaceButton!
+    @IBOutlet var btn3: WKInterfaceButton!
+}
