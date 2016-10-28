2199a747ddd35f9fcf44b9a4611540141fbebe51
Sweeper777 <sumulang@gmail.com>
2016-10-28 08:20:27 +0100

    tested real time clock

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,6 +1,16 @@
 import UIKit
 import EZSwiftExtensions
+import BEMAnalogClock
 
 class MyViewController: UIViewController {
+    @IBOutlet var clock: BEMAnalogClockView!
     
+    override func viewDidLoad() {
+        clock.realTime = true
+        clock.setClockToCurrentTime(animated: false)
+    }
+    
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        print(clock.seconds)
+    }
 }
