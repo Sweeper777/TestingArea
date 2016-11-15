2e42e228d113b7f5f14ff715c0ab79e88c31e3f3
Sweeper777 <sumulang@gmail.com>
2016-11-15 06:58:00 +0000

    added test 1

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,13 +1,54 @@
 import UIKit
 import EZSwiftExtensions
+import Eureka
 
 class MyViewController: UIViewController {
     
+}
+
+
+class TestVC1: FormViewController {
     override func viewDidLoad() {
+        super.viewDidLoad()
         
+        form +++ StepperRow("stepper") {
+            row in
+            row.value = 1
+            row.title = NSLocalizedString("Some Number", comment: "")
+            row.cell.stepper.maximumValue = 10
+            row.cell.stepper.minimumValue = 1
+            row.cell.valueLabel.textColor = UIApplication.shared.keyWindow!.tintColor
+        }
+        
+        for i in 1...10 {
+            form +++ Section("section \(i)") {
+                section in
+                section.hidden = Condition.function(["stepper"]) {
+                    let num: StepperRow = $0.rowBy(tag: "stepper")!
+                    
+                    if Int(num.value!) < i {
+                        return true
+                    }
+                    return false
+                }
+            }
+                <<< CustomRow(tag: "custom" + String(i)) {
+                    row in
+                    row.title = "Push!"
+                    row.value = 1
+                }
+                <<< TextRow("text" + String(i)) {
+                    row in
+                    row.title = "Message:"
+                    row.placeholder = "Blah"
+                }.cellUpdate { cell, row in
+                    cell.textField.textAlignment = .left
+                }
+        }
+
     }
-    
-    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+}
+
         
     }
     
