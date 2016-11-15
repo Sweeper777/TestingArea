60b290ef366b10140328d6e80bdb61825e358687
Sweeper777 <sumulang@gmail.com>
2016-11-15 06:58:18 +0000

    added second test

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -49,10 +49,72 @@ class TestVC1: FormViewController {
     }
 }
 
+class TestVC2: FormViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        form +++ SwitchRow("switch1") {
+            row in
+            row.title = "Switch me on!"
+            row.value = false
+        }
+            <<< SwitchRow("switch2") {
+                row in
+                row.title = "Switch me on!"
+                row.value = false
+        }
+        
+        form +++ StepperRow("stepper") {
+            row in
+            row.value = 1
+            row.title = NSLocalizedString("Some Number", comment: "")
+            row.cell.stepper.maximumValue = 10
+            row.cell.stepper.minimumValue = 1
+            row.cell.valueLabel.textColor = UIApplication.shared.keyWindow!.tintColor
+            
+            row.hidden = Condition.function(["switch1", "switch2"]) {
+                let switchRow1: SwitchRow = $0.rowBy(tag: "switch1")!
+                let switchRow2: SwitchRow = $0.rowBy(tag: "switch2")!
+                
+                if (switchRow1.value ?? true) && (switchRow2.value ?? true) {
+                    return false
+                }
+                return true
+            }
+        }
+        
+        for i in 1...10 {
+            form +++ Section("section \(i)") {
+                section in
+                section.hidden = Condition.function(["stepper", "switch1", "switch2"]) {
+                    let num: StepperRow = $0.rowBy(tag: "stepper")!
+                    let switchRow1: SwitchRow = $0.rowBy(tag: "switch1")!
+                    let switchRow2: SwitchRow = $0.rowBy(tag: "switch2")!
+                    
+                    
+                    if Int(num.value!) < i || !((switchRow1.value ?? true) && (switchRow2.value ?? true)) {
+                        return true
+                    }
+                    return false
+                }
+                }
+                <<< CustomRow(tag: "custom" + String(i)) {
+                    row in
+                    row.title = "Push!"
+                    row.value = 1
+                }
+                <<< TextRow("text" + String(i)) {
+                    row in
+                    row.title = "Message:"
+                    row.placeholder = "Blah"
+                    }.cellUpdate { cell, row in
+                        cell.textField.textAlignment = .left
+            }
+        }
         
     }
-    
-    override func viewDidLayoutSubviews() {
+}
+
         
     }
 }
