9b07a2a1be33973f10dc859aa9870d7e7b5e1b1c
Sweeper777 <sumulang@gmail.com>
2016-11-23 06:51:34 +0000

    tested printing table view

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -4,10 +4,30 @@ import AVFoundation
 
 class MyViewController: UIViewController {
     override func viewDidLoad() {
-        let rect = "Hello".boundingRect(with: CGSize.zero, context: nil)
-        let lbl = UILabel(frame: CGRect(x: 200, y: 200, w: rect.w, h: rect.h))
-        lbl.backgroundColor = UIColor.black
-        lbl.textColor = UIColor.white
-        lbl.text = "Hello"
+        
+    }
+    
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        // 1
+        let printController = UIPrintInteractionController.shared
+        // 2
+        let printInfo = UIPrintInfo(dictionary:nil)
+        printInfo.outputType = UIPrintInfoOutputType.general
+        printInfo.jobName = "print Job"
+        printController.printInfo = printInfo
+        
+        // 3
+        var text = ""
+        for _ in 0..<2000 {
+            text += "X"
+        }
+        
+        let formatter = self.view.viewWithTag(1)!.viewPrintFormatter()
+        
+        formatter.contentInsets = UIEdgeInsets(top: 72, left: 72, bottom: 72, right: 72)
+        printController.printFormatter = formatter
+        
+        // 4
+        printController.present(animated: true, completionHandler: nil)
     }
 }
