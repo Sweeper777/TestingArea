753dabaea29b64dbc39ff54eada84aad3bd36ea5
Sweeper777 <sumulang@gmail.com>
2016-11-23 06:51:11 +0000

    tested printing text

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -1,25 +1,42 @@
 import UIKit
 
-class MyTableViewController: UIViewController, UITableViewDataSource {
-    let people = [
-        ("Pankaj","Dhaka"),
-        ("Asish","Madaripur"),
-        ("Anup","Narail")
-    ]
-     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+class MyTableViewController: UITableViewController {
+     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "People"
     }
-     func numberOfSections(in tableView: UITableView) -> Int {
+     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return people.count
+     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
-     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        let (personName , personLocation) = people[indexPath.row]
-        cell.textLabel?.text = personName
-        cell.textLabel?.text = personLocation
-        return cell
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell!.textLabel!.text = "Hello \(indexPath.row)"
+        return cell!
+    }
+    
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        // 1
+        let printController = UIPrintInteractionController.shared
+        // 2
+        let printInfo = UIPrintInfo(dictionary:nil)
+        printInfo.outputType = UIPrintInfoOutputType.general
+        printInfo.jobName = "print Job"
+        printController.printInfo = printInfo
+        
+        // 3
+        var text = ""
+        for _ in 0..<2000 {
+            text += "X"
+        }
+        
+        let formatter = self.view.viewPrintFormatter()
+        
+        formatter.contentInsets = UIEdgeInsets(top: 72, left: 72, bottom: 72, right: 72)
+        printController.printFormatter = formatter
+        
+        // 4
+        printController.present(animated: true, completionHandler: nil)
     }
 }
