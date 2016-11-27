87a74db19c9d76ab5b434abe41c18873ae2c4b6f
Sweeper777 <sumulang@gmail.com>
2016-11-27 07:49:06 +0000

    tested sending mail with PDF

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,11 +1,12 @@
 import UIKit
 import EZSwiftExtensions
 import MessageUI
-import PDFGenerator
+import BNHtmlPdfKit
 
-class MyViewController: UIViewController, MFMailComposeViewControllerDelegate {
+class MyViewController: UIViewController, MFMailComposeViewControllerDelegate, BNHtmlPdfKitDelegate {
+    var pdfGenerator: BNHtmlPdfKit!
+    
     override func viewDidLoad() {
-        
     }
     
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
@@ -30,19 +31,35 @@ class MyViewController: UIViewController, MFMailComposeViewControllerDelegate {
 //        
 //        // 4
 //        printController.present(animated: true, completionHandler: nil)
-        
-        //Check to see the device can send email.
+        pdfGenerator = BNHtmlPdfKit()
+        pdfGenerator.delegate = self
+        pdfGenerator.saveHtml(asPdf: (view.viewWithTag(1) as! UITextView).text)
+    }
+    
+    func htmlPdfKit(_ htmlPdfKit: BNHtmlPdfKit!, didSavePdfData data: Data!) {
         if( MFMailComposeViewController.canSendMail() ) {
             print("Can send email.")
             
             let mailComposer = MFMailComposeViewController()
             mailComposer.mailComposeDelegate = self
-        mailComposer.modalPresentationStyle = .formSheet
+            mailComposer.modalPresentationStyle = .formSheet
             
-            if let pdf = try? PDFGenerator.generated(by: [self.view.viewWithTag(1)!]) {
-                mailComposer.addAttachmentData(pdf, mimeType: "application/pdf", fileName: "lorem")
-                self.presentVC(mailComposer)
-            }
+//            do {
+//                let pdf = try PDFGenerator.generated(by: ["Hello World"])
+//                mailComposer.addAttachmentData(pdf, mimeType: "application/pdf", fileName: "lorem")
+//                self.presentVC(mailComposer)
+//            } catch (let error) {
+//                print(error)
+//            }
+            mailComposer.addAttachmentData(data, mimeType: "application/pdf", fileName: "lorem")
+            self.presentVC(mailComposer)
+        }
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismissVC(completion: nil)
+    }
+    
     var touchIndicators: [UIView] = []
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
