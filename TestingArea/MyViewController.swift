86f90063e4c8a4ed64834fd6c19f7c9421ac8514
Sweeper777 <sumulang@gmail.com>
2016-11-30 07:16:52 +0000

    added dummy data

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -2,14 +2,25 @@ import UIKit
 import EZSwiftExtensions
 import MessageUI
 import BNHtmlPdfKit
+import CoreData
 
 class MyViewController: UIViewController, MFMailComposeViewControllerDelegate, BNHtmlPdfKitDelegate {
     var pdfGenerator: BNHtmlPdfKit!
     
     override func viewDidLoad() {
+        
     }
     
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).managedObjectContext
+        for i in 0..<100 {
+            let managedObj = Test(entity: NSEntityDescription.entity(forEntityName: "Test", in: context)!, insertInto: context)
+            managedObj.property1 = Int32(i)
+            managedObj.property2 = i.description
+            managedObj.property3 = Date(timeIntervalSinceNow: TimeInterval(i)) as NSDate
+            managedObj.property4 = i % 2 == 0
+        }
+        try? context.save()
     }
     
     func htmlPdfKit(_ htmlPdfKit: BNHtmlPdfKit!, didSavePdfData data: Data!) {
