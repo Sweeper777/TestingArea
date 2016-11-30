595f20cc2a9465a05e7df405797dbfc98fb581ba
Sweeper777 <sumulang@gmail.com>
2016-11-30 07:17:52 +0000

    tested fetching data with NSPredicate

--- TestingArea/AppDelegate.swift
+++ TestingArea/AppDelegate.swift
@@ -13,6 +13,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
+    
+    func test1() {
+        let request = NSFetchRequest<Test>(entityName: "Test")
+        request.predicate = NSPredicate(format: "property1 >= %x", 50)
+        let array = try! managedObjectContext.fetch(request)
+        print(array.count)
+        print(array)
+    }
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
