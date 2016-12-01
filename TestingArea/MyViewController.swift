b27cad29c1c8186f17bf3eaaf7262a5348837e8f
Sweeper777 <sumulang@gmail.com>
2016-12-01 06:50:32 +0000

    added date extension

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -39,5 +39,22 @@ class MyViewController: UIViewController, MFMailComposeViewControllerDelegate, B
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         controller.dismissVC(completion: nil)
     }
+}
+
+
+extension Date {
+    func isTheSameDayAs(other: Date) -> Bool {
+        let calendar = Calendar(identifier: Calendar.Identifier.gregorian)
+        var components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: other)
+        components.hour = 00
+        components.minute = 00
+        components.second = 00
+        let startDate = calendar.date(from: components)!
+        components.hour = 23
+        components.minute = 59
+        components.second = 59
+        let endDate = calendar.date(from: components)!
+        
+        return (startDate.compare(self) == .orderedAscending || startDate.compare(self) == .orderedSame) && (endDate.compare(self) == .orderedDescending || endDate.compare(self) == .orderedSame)
     }
 }
