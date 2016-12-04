cf2d7f5124eb683903bc3dcd9c21d0459ef5b9c2
Sweeper777 <sumulang@gmail.com>
2016-12-04 08:10:41 +0000

    removed BNHtmlPdfKit test code

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -15,28 +15,4 @@ class MyTableViewController: UITableViewController {
         cell!.textLabel!.text = "Hello \(indexPath.row)"
         return cell!
     }
-    
-    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
-        // 1
-        let printController = UIPrintInteractionController.shared
-        // 2
-        let printInfo = UIPrintInfo(dictionary:nil)
-        printInfo.outputType = UIPrintInfoOutputType.general
-        printInfo.jobName = "print Job"
-        printController.printInfo = printInfo
-        
-        // 3
-        var text = ""
-        for _ in 0..<2000 {
-            text += "X"
-        }
-        
-        let formatter = self.view.viewPrintFormatter()
-        
-        formatter.contentInsets = UIEdgeInsets(top: 72, left: 72, bottom: 72, right: 72)
-        printController.printFormatter = formatter
-        
-        // 4
-        printController.present(animated: true, completionHandler: nil)
-    }
 }
--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,12 +1,9 @@
 import UIKit
 import EZSwiftExtensions
 import MessageUI
-import BNHtmlPdfKit
 import CoreData
 
-class MyViewController: UIViewController, MFMailComposeViewControllerDelegate, BNHtmlPdfKitDelegate {
-    var pdfGenerator: BNHtmlPdfKit!
-    
+class MyViewController: UIViewController, MFMailComposeViewControllerDelegate {
     override func viewDidLoad() {
         
     }
@@ -21,24 +18,7 @@ class MyViewController: UIViewController, MFMailComposeViewControllerDelegate, B
             managedObj.property4 = i % 2 == 0
         }
         try? context.save()
-    }
-    
-    func htmlPdfKit(_ htmlPdfKit: BNHtmlPdfKit!, didSavePdfData data: Data!) {
-        if( MFMailComposeViewController.canSendMail() ) {
-            print("Can send email.")
-            
-            let mailComposer = MFMailComposeViewController()
-            mailComposer.mailComposeDelegate = self
-            mailComposer.modalPresentationStyle = .formSheet
-            
-            mailComposer.addAttachmentData(data, mimeType: "application/pdf", fileName: "lorem")
-            self.presentVC(mailComposer)
-        }
-    }
-    
-    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
-        controller.dismissVC(completion: nil)
-    }
+    }    
 }
 
 
