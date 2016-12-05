263c2986800f346e753e53a4e1fc652912282123
Sweeper777 <sumulang@gmail.com>
2016-12-05 07:08:55 +0000

    tested simple chart

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,9 +1,16 @@
 import UIKit
 import AVFoundation
 import EZSwiftExtensions
+import SwiftChart
 
 class MyViewController2: UIViewController {
+    @IBOutlet var chart: Chart!
+    
     override func viewDidLoad() {
-        
+        let labels = ["A", "B", "C", "D", "E", "F", "G", "H", ""]
+        let chartSeries = ChartSeries([10, 5, 7, 60, 3, 6, 4, 7])
+        chartSeries.area = true
+        chart.add(chartSeries)
+        chart.xLabelsFormatter = { index, value in return labels[index] }
     }
 }
