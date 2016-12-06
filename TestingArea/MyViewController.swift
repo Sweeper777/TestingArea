d799d3423aca25b058683645b43f9cdd3aa6c72a
Sweeper777 <sumulang@gmail.com>
2016-12-06 06:52:51 +0000

    removed old tests

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -2,39 +2,12 @@ import UIKit
 import EZSwiftExtensions
 import MessageUI
 import CoreData
-import Charts
 
 class MyViewController: UIViewController, MFMailComposeViewControllerDelegate {
-    @IBOutlet var barChart: BarChartView!
     override func viewDidLoad() {
-        let entry1 = BarChartDataSet(values: [BarChartDataEntry(x: 0, y: 10)], label: nil)
-        let entry2 = BarChartDataSet(values: [BarChartDataEntry(x: 1, y: 5)], label: nil)
-        let entry3 = BarChartDataSet(values: [BarChartDataEntry(x: 2, y: 7)], label: nil)
-        let entry4 = BarChartDataSet(values: [BarChartDataEntry(x: 3, y: 3)], label: nil)
-        let data = BarChartData()
-        data.addDataSet(entry1)
-        data.addDataSet(entry2)
-        data.addDataSet(entry3)
-        data.addDataSet(entry4)
-        barChart.data = data
-        let axis = barChart.xAxis
-        axis.labelCount = 4
-        axis.drawLabelsEnabled = true
-        axis.labelPosition = .bottom
-        axis.valueFormatter = IndexAxisValueFormatter(values: ["Village", "Beech Hanger", "Courtyard", "Other"])
     }
     
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
-        let context = (UIApplication.shared.delegate as! AppDelegate).managedObjectContext
-        for i in 0..<100 {
-            let managedObj = Test(entity: NSEntityDescription.entity(forEntityName: "Test", in: context)!, insertInto: context)
-            managedObj.property1 = Int32(i)
-            managedObj.property2 = i.description
-            managedObj.property3 = Date(timeIntervalSinceNow: TimeInterval(i)) as NSDate
-            managedObj.property4 = i % 2 == 0
-        }
-        try? context.save()
-    }    
 }
 
 
