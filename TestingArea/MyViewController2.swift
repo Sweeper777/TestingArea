78e665dd4eb14daf27dab2dc47f27ecef1434b67
Sweeper777 <sumulang@gmail.com>
2016-12-06 06:51:02 +0000

    tested custom x y labels

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -8,9 +8,17 @@ class MyViewController2: UIViewController {
     
     override func viewDidLoad() {
         let labels = ["A", "B", "C", "D", "E", "F", "G", "H", ""]
-        let chartSeries = ChartSeries([10, 5, 7, 60, 3, 6, 4, 7])
+        let chartSeries = ChartSeries([10, 5, 7, 10, 3, 6, 4, 7])
         chartSeries.area = true
         chart.add(chartSeries)
         chart.xLabelsFormatter = { index, value in return labels[index] }
+        test()
+    }
+    
+    func test() {
+        chart.xLabels = Array(0...8).map { Float($0) }
+        let series = chart.series.first!
+        let max = ceil(series.data.map { $0.y }.max()!)
+        chart.yLabels = Array(stride(from: 0, through: max, by: 1))
     }
 }
