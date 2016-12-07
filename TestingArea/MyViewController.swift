5394488386842cd3929f3daa8bd34f0eb59dc62b
Sweeper777 <sumulang@gmail.com>
2016-12-07 06:39:58 +0000

    tested printing multiple formatters

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -3,11 +3,45 @@ import EZSwiftExtensions
 import MessageUI
 import CoreData
 
+class MyRenderer: UIPrintPageRenderer {
+    override func drawPrintFormatter(_ printFormatter: UIPrintFormatter, forPageAt pageIndex: Int) {
+        printFormatter.draw(in: self.printableRect, forPageAt: pageIndex)
+    }
+}
+
 class MyViewController: UIViewController, MFMailComposeViewControllerDelegate {
+    @IBOutlet var webView: UIWebView!
+    @IBOutlet var webView2: UIWebView!
+    
     override func viewDidLoad() {
+        webView.loadRequest(URLRequest(url: URL(string: "https://en.wikipedia.org/wiki/Papists_Act_1778")!))
+        webView2.loadRequest(URLRequest(url: URL(string: "https://en.wikipedia.org/wiki/Ekaterina_Bolshova")!))
     }
     
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        let printController = UIPrintInteractionController.shared
+        // 2
+        let printInfo = UIPrintInfo(dictionary:nil)
+        printInfo.outputType = UIPrintInfoOutputType.general
+        printInfo.jobName = "print Job"
+        printController.printInfo = printInfo
+        
+        let formatter = webView.viewPrintFormatter()
+        let formatter1 = webView2.viewPrintFormatter()
+        
+        formatter.contentInsets = UIEdgeInsets(top: 72, left: 72, bottom: 72, right: 72)
+        formatter1.contentInsets = UIEdgeInsets(top: 72, left: 72, bottom: 72, right: 72)
+        
+        let renderer = MyRenderer()
+        renderer.addPrintFormatter(formatter, startingAtPageAt: 0)
+        print(formatter.pageCount)
+        renderer.addPrintFormatter(formatter1, startingAtPageAt: formatter.pageCount)
+        
+        printController.printPageRenderer = renderer
+        
+        // 4
+        printController.present(animated: true, completionHandler: nil)
+    }
 }
 
 
