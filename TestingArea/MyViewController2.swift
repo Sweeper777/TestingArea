45d64cffb9beaa2f1053827b53bfbffc5ab630e9
Sweeper777 <sumulang@gmail.com>
2016-12-09 07:14:10 +0000

    tested data label thingy

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -4,7 +4,7 @@ import EZSwiftExtensions
 import SwiftChart
 import MLScreenshot
 
-class MyViewController2: UIViewController {
+class MyViewController2: UIViewController, ChartDelegate {
     @IBOutlet var chart: Chart!
     @IBOutlet var label: UILabel!
     @IBOutlet var labelLeadingMarginConstraint: NSLayoutConstraint!
@@ -23,6 +23,7 @@ class MyViewController2: UIViewController {
         let series = chart.series.first!
         let max = ceil(series.data.map { $0.y }.max()!)
         chart.yLabels = Array(stride(from: 0, through: max, by: 1))
+        chart.delegate = self
     }
     
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
@@ -31,5 +32,39 @@ class MyViewController2: UIViewController {
         imgView.contentMode = .scaleAspectFit
         imgView.image = image
         self.view.addSubview(imgView)
+    }
+    
+    func didTouchChart(_ chart: Chart, indexes: Array<Int?>, x: Float, left: CGFloat) {
+        
+        if let value = chart.valueForSeries(0, atIndex: indexes[0]) {
+            label.isHidden = false
+            let labelLeadingMarginInitialConstant = CGFloat(0)
+            let numberFormatter = NumberFormatter()
+            numberFormatter.minimumFractionDigits = 2
+            numberFormatter.maximumFractionDigits = 2
+            label.text = numberFormatter.string(from: NSNumber(value: value))
+            
+            // Align the label to the touch left position, centered
+            var constant = labelLeadingMarginInitialConstant + left - (label.frame.width / 2)
+            
+            // Avoid placing the label on the left of the chart
+            if constant < labelLeadingMarginInitialConstant {
+                constant = labelLeadingMarginInitialConstant
+            }
+            
+            // Avoid placing the label on the right of the chart
+            let rightMargin = chart.frame.width - label.frame.width
+            if constant > rightMargin {
+                constant = rightMargin
+            }
+            
+            labelLeadingMarginConstraint.constant = constant
+            
+        }
+        
+    }
+    
+    func didFinishTouchingChart(_ chart: Chart) {
+        
     }
 }
