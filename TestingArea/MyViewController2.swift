d695b635a5925b96ce87791814b208618014d6c0
Sweeper777 <sumulang@gmail.com>
2016-12-30 07:26:43 +0800

    tested Iteratable

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,5 +1,13 @@
 import UIKit
-import SwiftUtils
+import SwiftyUtils
 
 class MyViewController2: UIViewController {
+    
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        print(iterateEnum(Foo.self).next())
+    }
+}
+
+enum Foo: Iteratable {
+    case a, b, c, d
 }
