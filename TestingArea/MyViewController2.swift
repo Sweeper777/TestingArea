7d700cdd6795aeaf3de6db83af79b36b0b4ffed1
Sweeper777 <sumulang@gmail.com>
2017-01-17 06:39:38 +0000

    tested behavior of Timers from different pods

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,8 +1,25 @@
 import UIKit
 import SwiftyUtils
+import EZSwiftExtensions
 
 class MyViewController2: UIViewController {
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var label2: UILabel!
+    var n1 = 0
+    var n2 = 0
+    var t1: Timer!
+    var t2: Timer!
     
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        t1 = Timer.runThisEvery(seconds: 1) {
+            _ in
+            self.label1.text = self.n1.description
+            self.n1 += 1
+        }
+        
+        t2 = Timer.every(1) {
+            self.label2.text = self.n2.description
+            self.n2 += 1
+        }
     }
 }
