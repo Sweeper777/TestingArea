0eb3811a0d084ae8ead0f02b65ed307631876472
Sweeper777 <sumulang@gmail.com>
2017-01-17 06:39:18 +0000

    removed old tests

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -6,7 +6,3 @@ class MyViewController2: UIViewController {
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
     }
 }
-
-enum Foo: Iteratable {
-    case a, b, c, d
-}
