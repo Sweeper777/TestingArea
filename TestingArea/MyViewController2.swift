7c2adf866e2df22287c1f72a61363911f2027d11
Sweeper777 <sumulang@gmail.com>
2017-01-22 08:06:04 +0000

    tested behavior of toasts of different duration

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,28 +1,24 @@
 import UIKit
 import SwiftyUtils
 import EZSwiftExtensions
+import ASToast
 
 class MyViewController2: UIViewController {
-    @IBOutlet var label1: UILabel!
-    @IBOutlet var label2: UILabel!
-    var n1 = 0
-    var n2 = 0
-    var t1: Timer!
-    var t2: Timer!
+    
+    @IBOutlet var view1: UIView!
+    @IBOutlet var view2: UIView!
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if self.slideMenuController()!.isRightOpen() {
-            self.slideMenuController()!.closeRight()
-        } else {
-            self.slideMenuController()!.openRight()
-        }
+//        if self.slideMenuController()!.isRightOpen() {
+//            self.slideMenuController()!.closeRight()
+//        } else {
+//            self.slideMenuController()!.openRight()
+//        }
+        view1.makeToast("Message1", duration: 1, position: ASToastPosition.ASToastPositionBotom as AnyObject?, backgroundColor: nil, titleColor: nil, messageColor: nil)
+        view2.makeToast("Message2", duration: 2, position: ASToastPosition.ASToastPositionCenter as AnyObject?, backgroundColor: nil, titleColor: nil, messageColor: nil)
     }
     
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
         print("Hello")
-    }
-    
-    override func viewDidLoad() {
-        
     }
 }
