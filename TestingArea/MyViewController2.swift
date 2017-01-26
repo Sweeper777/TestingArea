90d049d377e00d4a16b7fc9c3f1b8c395ed67ee2
Sweeper777 <sumulang@gmail.com>
2017-01-26 06:47:16 +0000

    tested simple spring animation

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -2,23 +2,17 @@ import UIKit
 import SwiftyUtils
 import EZSwiftExtensions
 import ASToast
-import NBEmojiSearchView
+import pop
 
-class MyViewController2: UIViewController, NBEmojiSearchViewDelegate {
+class MyViewController2: UIViewController {
     
     @IBOutlet var view1: UIView!
-    @IBOutlet var view2: UIView!
-    
-    var searchView1: NBEmojiSearchView!
-    var searchView2: NBEmojiSearchView!
     
     override func viewDidLoad() {
-        searchView1 = NBEmojiSearchView()
-        searchView1.delegate = self
-        searchView1.install(on: view1 as! UITextView)
-        searchView2 = NBEmojiSearchView()
-        searchView2.delegate = self
-        searchView2.install(on: view2 as! UITextView)
+        let anim = POPSpringAnimation()
+        anim.fromValue = 0
+        anim.toValue = 1
+        view1.layer.pop_add(anim, forKey: "borderWidth")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -28,7 +22,6 @@ class MyViewController2: UIViewController, NBEmojiSearchViewDelegate {
 //            self.slideMenuController()!.openRight()
 //        }
         view1.makeToast("Message1", duration: 1, position: ASToastPosition.ASToastPositionBotom as AnyObject?, backgroundColor: nil, titleColor: nil, messageColor: nil)
-        view2.makeToast("Message2", duration: 2, position: ASToastPosition.ASToastPositionCenter as AnyObject?, backgroundColor: nil, titleColor: nil, messageColor: nil)
     }
     
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
