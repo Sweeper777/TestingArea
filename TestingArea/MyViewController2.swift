b6dba3f9f59538ee62ddf988e150365f4d2da2b2
Sweeper777 <sumulang@gmail.com>
2017-02-05 08:45:50 +0000

    added label for testing

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -4,99 +4,12 @@ import EZSwiftExtensions
 import ASToast
 import pop
 import NGORoundedButton
+import LTMorphingLabel
 
 class MyViewController2: UIViewController {
-    
-    @IBOutlet var view1: UILabel!
-    @IBOutlet var view2: UILabel!
-    @IBOutlet var view3: UIView!
-    
-    var i = 0
-    var j = 0
+    @IBOutlet var label: LTMorphingLabel!
     
     override func viewDidLoad() {
-        let button = NGORoundedButton(buttonCustomImage: UIImage(named: "home"), andShape: .circle)!
-        button.frame = CGRect(origin: CGPoint(x: 100, y: 100), size: button.size)
-        self.view.addSubview(button)
-        
-        button.width = 44
-        button.height = 44
-        
-        button.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
-        
-        let closure: (Timer) -> Void = { (timer) -> Void in
-            print("Hello")
-            timer.invalidate()
-        }
-        _ = Timer.every(1, closure)
-        perform(#selector(imageSelected), with: "Hello", afterDelay: 0.1)
-    }
-    
-    func imageSelected(image: String) {
-        print(image)
-    }
-    
-    func buttonClicked(sender: NGORoundedButton) {
-        sender.customImage = UIImage(named: "settings")
-        performSegue(withIdentifier: "mySegue", sender: IndexPath(row: 0, section: 0))
-    }
-    
-    func test1() {
-        let anim = POPSpringAnimation()
-        anim.fromValue = 0
-        anim.toValue = 30
-        anim.velocity = 0.1
-        view1.layer.pop_add(anim, forKey: "borderWidth")
-    }
-    
-    func test2() {
-        let anim = POPSpringAnimation(propertyNamed: kPOPLayerBorderWidth)!
-        anim.toValue = 3
-        anim.springBounciness = 1
-        anim.velocity = 2
-        
-        view1.layer.pop_add(anim, forKey: "borderWidth")
-    }
-    
-    func test3() {
-        let anim = POPBasicAnimation(propertyNamed: kPOPLayerCornerRadius)!
-        anim.toValue = view3.w / 2
-        anim.duration = 0.3
-        anim.timingFunction = CAMediaTimingFunction.init(name: "easeInEaseOut")
-        
-        view3.layer.pop_add(anim, forKey: "cornerRadius")
-    }
-    
-    func test4() {
-        let anim = POPBasicAnimation(propertyNamed: kPOPViewBackgroundColor)!
-        anim.toValue = UIColor.blue
-        anim.duration = 0.3
-        
-        view3.layer.pop_add(anim, forKey: "color")
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        if self.slideMenuController()!.isRightOpen() {
-//            self.slideMenuController()!.closeRight()
-//        } else {
-//            self.slideMenuController()!.openRight()
-//        }
-        test3()
-        test4()
-    }
-    
-    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
-        print("Hello")
-    }
-    
-    func a() {
-        view1.text = self.i.description
-        self.i += 1
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let indexPath = sender as? IndexPath {
-            print(indexPath.row)
         }
     }
 }
