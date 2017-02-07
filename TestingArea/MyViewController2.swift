ecdd42d5a049800384df24ace95eb51b34268762
Sweeper777 <sumulang@gmail.com>
2017-02-07 07:18:16 +0000

    started testing text field accessory views

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -9,10 +9,16 @@ import LTMorphingLabel
 class MyViewController2: UIViewController {
     @IBOutlet var label: LTMorphingLabel!
     
+    var textField: UITextField!
+    
     override func viewDidLoad() {
         label.morphingEffect = .evaporate
         Timer.runThisAfterDelay(seconds: 3) {
             self.label.text = "60:00"
         }
+    
+        textField = UITextField(x: 20, y: 200, w: 100, h: 30)
+        textField.borderStyle = .roundedRect
+        self.view.addSubview(textField)
     }
 }
