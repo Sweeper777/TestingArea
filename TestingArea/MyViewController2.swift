0bbef9153b5c17ae4b4d8c89afd10a9f767d7e82
Sweeper777 <sumulang@gmail.com>
2017-02-08 07:18:43 +0000

    added NSRange extension

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -62,4 +62,12 @@ extension UITextInput {
     }
 }
 
+extension NSRange {
+    func toTextRange(textInput:UITextInput) -> UITextRange? {
+        if let rangeStart = textInput.position(from: textInput.beginningOfDocument, offset: location),
+            let rangeEnd = textInput.position(from: rangeStart, offset: length) {
+            return textInput.textRange(from: rangeStart, to: rangeEnd)
+        }
+        return nil
+    }
 }
