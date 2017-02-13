409b105e08a4633023493df0b374b79a2084fb18
Sweeper777 <sumulang@gmail.com>
2017-02-13 12:59:42 +0000

    tested selected closure

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -8,7 +8,7 @@ class MyViewController2: UIViewController {
     @IBAction func btnClicked(sender: UIButton) {
         let homeItem = YCXMenuItem("Home", image: #imageLiteral(resourceName: "home"), target: self, action: #selector(home))!
         let settingsItem = YCXMenuItem("Settings", image: #imageLiteral(resourceName: "settings"), target: self, action: #selector(settings))!
-        YCXMenu.show(in: self.view, from: sender.frame, menuItems: [homeItem, settingsItem], selected: nil)
+        YCXMenu.show(in: self.view, from: sender.frame, menuItems: [homeItem, settingsItem], selected: { print("\($0) \($1)") })
         YCXMenu.setHasShadow(true)
         YCXMenu.setBackgrounColorEffect(YCXMenuBackgrounColorEffectGradient)
     }
