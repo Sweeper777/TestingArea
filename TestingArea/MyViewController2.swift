529396919c56d649f69df78d0be6fe74409af377
Sweeper777 <sumulang@gmail.com>
2017-02-13 12:49:43 +0000

    created a simple menu

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -2,9 +2,21 @@ import UIKit
 import SwiftyUtils
 import EZSwiftExtensions
 import ASToast
+import YCXMenu
 
 class MyViewController2: UIViewController {
     @IBAction func btnClicked(sender: UIButton) {
+        let homeItem = YCXMenuItem("Home", image: #imageLiteral(resourceName: "home"), target: self, action: #selector(home))
+        let settingsItem = YCXMenuItem("Settings", image: #imageLiteral(resourceName: "settings"), target: self, action: #selector(settings))
+        YCXMenu.show(in: self.view, from: sender.frame, menuItems: [homeItem, settingsItem], selected: nil)
         
+    }
+    
+    func home() {
+        print("Home tapped")
+    }
+    
+    func settings() {
+        print("Settings tapped")
     }
 }
