332639d2332721eeb028ec5011b6e26e6f9f0782
Sweeper777 <sumulang@gmail.com>
2017-02-13 12:51:23 +0000

    tested shadow

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -6,10 +6,10 @@ import YCXMenu
 
 class MyViewController2: UIViewController {
     @IBAction func btnClicked(sender: UIButton) {
-        let homeItem = YCXMenuItem("Home", image: #imageLiteral(resourceName: "home"), target: self, action: #selector(home))
-        let settingsItem = YCXMenuItem("Settings", image: #imageLiteral(resourceName: "settings"), target: self, action: #selector(settings))
+        let homeItem = YCXMenuItem("Home", image: #imageLiteral(resourceName: "home"), target: self, action: #selector(home))!
+        let settingsItem = YCXMenuItem("Settings", image: #imageLiteral(resourceName: "settings"), target: self, action: #selector(settings))!
         YCXMenu.show(in: self.view, from: sender.frame, menuItems: [homeItem, settingsItem], selected: nil)
-        
+        YCXMenu.setHasShadow(true)
     }
     
     func home() {
