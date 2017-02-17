646ee06e8c070f64aa5acaf8c6bb121964268655
Sweeper777 <sumulang@gmail.com>
2017-02-17 08:35:05 +0000

    tested simple alert with 2 text fields

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -2,16 +2,18 @@ import UIKit
 import SwiftyUtils
 import EZSwiftExtensions
 import ASToast
-import YCXMenu
+import SCLAlertView
 
 class MyViewController2: UIViewController {
     @IBAction func btnClicked(sender: UIButton) {
-        let homeItem = YCXMenuItem("Home", image: #imageLiteral(resourceName: "home"), target: self, action: #selector(home))!
-        let settingsItem = YCXMenuItem("Settings", image: #imageLiteral(resourceName: "settings"), target: self, action: #selector(settings))!
-        YCXMenu.show(in: self.view, from: sender.frame, menuItems: [homeItem, settingsItem], selected: { print("\($0) \($1)") })
-        YCXMenu.setHasShadow(true)
-        YCXMenu.setTintColor(UIColor(hex: "5abb5a"))
-        YCXMenu.setSelectedColor(UIColor(hex: "3b7b3b"))
+        let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(showCloseButton: false))
+        let txtLink = alert.addTextField("Enter Link")
+        let txtDisplay = alert.addTextField("Enter Display Text")
+        alert.addButton("OK", action: {
+            print(txtLink.text)
+            print(txtDisplay.text)
+        })
+        alert.showEdit("Add Hyperlink", subTitle: "")
     }
     
     func home() {
