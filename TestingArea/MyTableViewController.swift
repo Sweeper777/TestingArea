6aa8c56c1ec6671abc14190c912f9ceb266b8017
Sweeper777 <sumulang@gmail.com>
2017-02-23 07:16:49 +0000

    tested Pull To Refresh

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -1,22 +1,29 @@
 import UIKit
+import ESPullToRefresh
 
 class MyTableViewController: UITableViewController {
-     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "People"
-    }
+    var array = ["John", "Tom", "Jerry"]
+    
      override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
      override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return array.count
     }
      override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-        cell!.textLabel!.text = "Hello \(indexPath.row)"
+        cell!.textLabel!.text = "Hello \(array[indexPath.row])"
         return cell!
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("selected")
+    override func viewDidLoad() {
+        tableView.es_addPullToRefresh {
+            [weak self] in
+            Timer.runThisAfterDelay(seconds: 1) {
+                self?.array = ["Everest", "Eugene", "Boris"]
+                self?.tableView.reloadData()
+                self?.tableView.es_stopPullToRefresh()
+            }
+        }
     }
 }
