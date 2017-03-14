950d50e5ef917a2a8e8f3e554ef5271b862392d5
Sweeper777 <sumulang@gmail.com>
2017-03-14 07:01:36 +0000

    added person model

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,13 +1,23 @@
 import UIKit
 import SwiftyUtils
 import EZSwiftExtensions
-import ASToast
-import SCLAlertView
-import SwiftyJSON
-import Alamofire
+import RealmSwift
 
 class MyViewController2: UIViewController {
     override func viewDidLoad() {
         
     }
+class Person: Object {
+    static var nextID = 0
+    
+    dynamic var name: String = "Unnamed"
+    dynamic var age: Int = 0
+    dynamic var isMarried: Bool = false
+    dynamic var id: Int = Person.nextID
+    
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
 }
