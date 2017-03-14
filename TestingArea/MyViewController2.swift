2c000075f381909a8479a79fcded86c356ab7ecc
Sweeper777 <sumulang@gmail.com>
2017-03-14 07:01:56 +0000

    tested writing to realm

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -7,6 +7,28 @@ class MyViewController2: UIViewController {
     override func viewDidLoad() {
         
     }
+    
+    func test1() {
+        let realm = (UIApplication.shared.delegate as! AppDelegate).realm!
+        let tom = Person()
+        Person.nextID += 1
+        tom.name = "Tom"
+        tom.age = 30
+        tom.isMarried = false
+        
+        let john = Person()
+        Person.nextID += 1
+        john.name = "John"
+        john.age = 20
+        john.isMarried = true
+        
+        try! realm.write {
+            realm.add(tom)
+            realm.add(john)
+        }
+        
+    }
+    
 class Person: Object {
     static var nextID = 0
     
