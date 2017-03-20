790c66a7eb621402fc3bc6b22bece2a065fa58b3
Sweeper777 <sumulang@gmail.com>
2017-03-20 07:37:06 +0000

    tested creating simple JSON

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,12 +1,15 @@
 import UIKit
 import SwiftyUtils
 import EZSwiftExtensions
+import SwiftyJSON
 
 class MyViewController2: UIViewController {
     override func viewDidLoad() {
     }
     
     func test1() {
+        let json = JSON(dictionaryLiteral: ("name", "My Quiz"), ("questions", [Any]()))
+        print(json)
     }
     
     func test2() {
