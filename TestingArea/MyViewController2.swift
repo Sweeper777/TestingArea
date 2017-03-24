020a206da4b9985f90892964468fece8c9234698
Sweeper777 <sumulang@gmail.com>
2017-03-24 06:52:26 +0000

    tested constructing complex JSON

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -13,6 +13,13 @@ class MyViewController2: UIViewController {
     }
     
     func test2() {
+        var json = JSON(dictionaryLiteral: ("name", "My Quiz"), ("questions", [Any]()))
+        json["questions"] = [["text": "", "type": "", "answer": "", "hint": ""]]
+        json["questions"][0]["text"] = "What is your name?"
+        json["questions"][0]["type"] = "fib"
+        json["questions"][0]["answer"] = "Mulang"
+        json["questions"][0]["hint"] = "Begins with an 'M'"
+        print(json)
     }
     
     func test3() {
