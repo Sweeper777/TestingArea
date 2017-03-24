b9b5bedb4ee5ed28631f3454cdde58c76525b5d2
Sweeper777 <sumulang@gmail.com>
2017-03-24 06:52:44 +0000

    tested turning JSON into data

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -5,6 +5,7 @@ import SwiftyJSON
 
 class MyViewController2: UIViewController {
     override func viewDidLoad() {
+        test3()
     }
     
     func test1() {
@@ -23,5 +24,13 @@ class MyViewController2: UIViewController {
     }
     
     func test3() {
+        var json = JSON(dictionaryLiteral: ("name", "My Quiz"), ("questions", [Any]()))
+        json["questions"] = [["text": "", "type": "", "answer": "", "hint": ""]]
+        json["questions"][0]["text"] = "What is your name?"
+        json["questions"][0]["type"] = "fib"
+        json["questions"][0]["answer"] = "Mulang"
+        json["questions"][0]["hint"] = "Begins with an 'M'"
+        let data = try! json.rawData()
+        print(String.init(data: data, encoding: .utf8)!)
     }
 }
