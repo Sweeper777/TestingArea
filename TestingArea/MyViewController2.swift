5d2fd7f1c902a04a949437c24cfad9660f6fc57b
Sweeper777 <sumulang@gmail.com>
2017-03-30 06:57:36 +0100

    tested requesting exchange rates at a date

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -2,14 +2,25 @@ import UIKit
 import SwiftyUtils
 import EZSwiftExtensions
 import SwiftyJSON
+import Alamofire
 
 class MyViewController2: UIViewController {
     @IBOutlet var textfield: UITextField!
     @IBOutlet var textview: UITextView!
     override func viewDidLoad() {
-        test3()
+        
     }
     
     @IBAction func click() {
+        Alamofire.request("https://api.fixer.io/\(textfield.text!)?base=HKD&symbols=GBP").responseString {
+            response in
+            if let error = response.error {
+                self.textview.text = "An error occured: \(error.localizedDescription)"
+                return
+            }
+            DispatchQueue.main.async {
+                self.textview.text = response.result.value
+            }
+        }
     }
 }
