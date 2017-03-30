e8961a031e3c59d9f73785d0aa22833e592c9866
Sweeper777 <sumulang@gmail.com>
2017-03-30 06:57:16 +0100

    connected outlets and actions

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -4,33 +4,12 @@ import EZSwiftExtensions
 import SwiftyJSON
 
 class MyViewController2: UIViewController {
+    @IBOutlet var textfield: UITextField!
+    @IBOutlet var textview: UITextView!
     override func viewDidLoad() {
         test3()
     }
     
-    func test1() {
-        let json = JSON(dictionaryLiteral: ("name", "My Quiz"), ("questions", [Any]()))
-        print(json)
-    }
-    
-    func test2() {
-        var json = JSON(dictionaryLiteral: ("name", "My Quiz"), ("questions", [Any]()))
-        json["questions"] = [["text": "", "type": "", "answer": "", "hint": ""]]
-        json["questions"][0]["text"] = "What is your name?"
-        json["questions"][0]["type"] = "fib"
-        json["questions"][0]["answer"] = "Mulang"
-        json["questions"][0]["hint"] = "Begins with an 'M'"
-        print(json)
-    }
-    
-    func test3() {
-        var json = JSON(dictionaryLiteral: ("name", "My Quiz"), ("questions", [Any]()))
-        json["questions"] = [["text": "", "type": "", "answer": "", "hint": ""]]
-        json["questions"][0]["text"] = "What is your name?"
-        json["questions"][0]["type"] = "fib"
-        json["questions"][0]["answer"] = "Mulang"
-        json["questions"][0]["hint"] = "Begins with an 'M'"
-        let data = try! json.rawData()
-        print(String.init(data: data, encoding: .utf8)!)
+    @IBAction func click() {
     }
 }
