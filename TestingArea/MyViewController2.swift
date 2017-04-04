7e46a9a295fcd19d538cf4725267d98b76f7195e
Sweeper777 <sumulang@gmail.com>
2017-04-04 08:09:46 +0100

    compared the sizes of different ways to store JSON

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -7,7 +7,7 @@ class MyViewController2: UIViewController {
     @IBOutlet var textfield: UITextField!
     @IBOutlet var textview: UITextView!
     override func viewDidLoad() {
-        
+        test1()
     }
     
     @IBAction func click() {
@@ -21,5 +21,20 @@ class MyViewController2: UIViewController {
                 self.textview.text = response.result.value
             }
         }
+    }
+    
+    func test1() {
+        var json = JSON(dictionaryLiteral: ("name", "My Quiz"), ("questions", [Any]()))
+        json["questions"] = [["text": "", "type": "", "answer": "", "hint": ""]]
+        json["questions"][0]["text"] = "What is your name?"
+        json["questions"][0]["type"] = "fib"
+        json["questions"][0]["answer"] = "Mulang"
+        json["questions"][0]["hint"] = "Begins with an 'M'"
+        let data = try! json.rawData()
+        print("JSON Data Size: \(data.count) Bytes")
+        let jsonStringData = json.rawString()!.data(using: .utf8)!
+        print("JSON String Data Size: \(jsonStringData.count) Bytes")
+        let encodedJSON = data.base64EncodedString().data(using: .utf8)!
+        print("Encoded JSON Size: \(encodedJSON.count) Bytes")
     }
 }
