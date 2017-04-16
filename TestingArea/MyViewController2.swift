dab984667539ea1a833455b3c77aca81c0d1da29
Sweeper777 <sumulang@gmail.com>
2017-04-16 08:33:13 +0100

    prepared for the testing of FTPopoverMenu

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -2,35 +2,17 @@ import UIKit
 import SwiftyUtils
 import SwiftyJSON
 import Alamofire
+import FTPopOverMenu_Swift
 
 class MyViewController2: UIViewController {
     @IBOutlet var textfield: UITextField!
     @IBOutlet var textview: UITextView!
+    @IBOutlet var button: UIButton!
     override func viewDidLoad() {
-        textfield.alpha = 0
+        
     }
     
     @IBAction func click() {
-        print(textfield.isHidden)
         
-        
-        view.backgroundColor = #colorLiteral(red: 0.4131736755, green: 0.7676505446, blue: 0.4273042679, alpha: 1)
-        let imageView = UIImageView()
-        imageView.image = #imageLiteral(resourceName: "home")
-    }
-    
-    func test1() {
-        var json = JSON(dictionaryLiteral: ("name", "My Quiz"), ("questions", [Any]()))
-        json["questions"] = [["text": "", "type": "", "answer": "", "hint": ""]]
-        json["questions"][0]["text"] = "What is your name?"
-        json["questions"][0]["type"] = "fib"
-        json["questions"][0]["answer"] = "Mulang"
-        json["questions"][0]["hint"] = "Begins with an 'M'"
-        let data = try! json.rawData()
-        print("JSON Data Size: \(data.count) Bytes")
-        let jsonStringData = json.rawString()!.data(using: .utf8)!
-        print("JSON String Data Size: \(jsonStringData.count) Bytes")
-        let encodedJSON = data.base64EncodedString().data(using: .utf8)!
-        print("Encoded JSON Size: \(encodedJSON.count) Bytes")
     }
 }
