da56a4b870eb4c8a7fdd9b5e84d266d163c397f3
Sweeper777 <sumulang@gmail.com>
2017-04-22 08:35:49 +0100

    created a very basic segmented control

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -3,16 +3,24 @@ import SwiftyUtils
 import SwiftyJSON
 import Alamofire
 import FTPopOverMenu_Swift
+import SJFluidSegmentedControl
 
-class MyViewController2: UIViewController {
+class MyViewController2: UIViewController, SJFluidSegmentedControlDataSource {
     @IBOutlet var textfield: UITextField!
     @IBOutlet var textview: UITextView!
     @IBOutlet var button: UIButton!
+    
+    @IBOutlet var segmentedControl: SJFluidSegmentedControl!
+    
     override func viewDidLoad() {
-        
+        segmentedControl.dataSource = self
     }
     
     @IBAction func click() {
         
+    }
+    
+    func numberOfSegmentsInSegmentedControl(_ segmentedControl: SJFluidSegmentedControl) -> Int {
+        return 4
     }
 }
