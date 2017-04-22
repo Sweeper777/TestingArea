1ee2b1312e66009f9629864f35500a6c2b2385b3
Sweeper777 <sumulang@gmail.com>
2017-04-22 08:22:29 +0100

    deleted old tests

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -2,61 +2,8 @@ import UIKit
 import MessageUI
 import CoreData
 
-class MyRenderer: UIPrintPageRenderer {
-    override func drawPrintFormatter(_ printFormatter: UIPrintFormatter, forPageAt pageIndex: Int) {
-        printFormatter.draw(in: self.printableRect, forPageAt: pageIndex)
-    }
-}
-
 class MyViewController: UIViewController, MFMailComposeViewControllerDelegate {
     @IBOutlet var webView: UIWebView!
     @IBOutlet var webView2: UIWebView!
-    
-    override func viewDidLoad() {
-        webView.loadRequest(URLRequest(url: URL(string: "https://en.wikipedia.org/wiki/Papists_Act_1778")!))
-        webView2.loadRequest(URLRequest(url: URL(string: "https://en.wikipedia.org/wiki/Ekaterina_Bolshova")!))
-    }
-    
-    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
-        let printController = UIPrintInteractionController.shared
-        // 2
-        let printInfo = UIPrintInfo(dictionary:nil)
-        printInfo.outputType = UIPrintInfoOutputType.general
-        printInfo.jobName = "print Job"
-        printController.printInfo = printInfo
-        
-        let formatter = webView.viewPrintFormatter()
-        let formatter1 = webView2.viewPrintFormatter()
-        
-        formatter.contentInsets = UIEdgeInsets(top: 72, left: 72, bottom: 72, right: 72)
-        formatter1.contentInsets = UIEdgeInsets(top: 72, left: 72, bottom: 72, right: 72)
-        
-        let renderer = MyRenderer()
-        renderer.addPrintFormatter(formatter, startingAtPageAt: 0)
-        print(formatter.pageCount)
-        renderer.addPrintFormatter(formatter1, startingAtPageAt: formatter.pageCount)
-        
-        printController.printPageRenderer = renderer
-        
-        // 4
-        printController.present(animated: true, completionHandler: nil)
-    }
 }
 
-
-extension Date {
-    func isTheSameDayAs(other: Date) -> Bool {
-        let calendar = Calendar(identifier: Calendar.Identifier.gregorian)
-        var components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: other)
-        components.hour = 00
-        components.minute = 00
-        components.second = 00
-        let startDate = calendar.date(from: components)!
-        components.hour = 23
-        components.minute = 59
-        components.second = 59
-        let endDate = calendar.date(from: components)!
-        
-        return (startDate.compare(self) == .orderedAscending || startDate.compare(self) == .orderedSame) && (endDate.compare(self) == .orderedDescending || endDate.compare(self) == .orderedSame)
-    }
-}
--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -4,72 +4,15 @@ import SwiftyJSON
 import Alamofire
 import FTPopOverMenu_Swift
 
-class Card {
-    init() {
-        print("Card init")
-    }
-}
-
 class MyViewController2: UIViewController {
     @IBOutlet var textfield: UITextField!
     @IBOutlet var textview: UITextView!
     @IBOutlet var button: UIButton!
     override func viewDidLoad() {
-        let config = FTConfiguration.shared
-        config.textColor = UIColor.black
-        config.backgoundTintColor = UIColor.white
-        config.borderColor = UIColor.lightGray
-        config.menuSeparatorColor = UIColor.lightGray
-        config.textAlignment = .center
-        config.textFont = UIFont.systemFont(ofSize: 14)
-        config.menuRowHeight = 40
-        config.cornerRadius = 6
         
-        var cards = [Card]()
-        
-        for _ in 0..<3 {
-            let card = Card() // Screenshot #1
-            cards.append(card) // Screenshot #2
-        }
     }
     
     @IBAction func click() {
-        FTPopOverMenu.showForSender(sender: button, with: [
-                "Item 1", "Item2", "Item 3", "Item 4"
-            ], menuImageArray: [
-                "settings", "home", "settings", "home"
-            ], done: { index in
-                print(index)
-        }, cancel: {})
-    }
-}
-
-@IBDesignable
-class RoundedImageView: UIImageView {
-    override init(image: UIImage?) {
-        super.init(image: image)
-        super.layer.cornerRadius = super.frame.size.height / 2
-        self.layer.cornerRadius = self.frame.size.height / 2
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        super.layer.cornerRadius = super.frame.size.height / 2
-        self.layer.cornerRadius = self.frame.size.height / 2
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        super.layer.cornerRadius = super.frame.size.height / 2
-        self.layer.cornerRadius = self.frame.size.height / 2
-    }
-    
-    override func draw(_ layer: CALayer, in ctx: CGContext) {
-        layer.cornerRadius = self.frame.size.height / 2
-    }
-    
-    override func layoutSubviews() {
-        super.layer.cornerRadius = super.frame.size.height / 2
-        self.layer.cornerRadius = self.frame.size.height / 2
+        
     }
 }
