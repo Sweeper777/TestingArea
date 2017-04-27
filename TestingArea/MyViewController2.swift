003af6ce27f3e2b746b7790bf4806740001aa823
Sweeper777 <sumulang@gmail.com>
2017-04-27 07:19:59 +0100

    added an example multivalued section

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -3,6 +3,8 @@ import SwiftyUtils
 import SwiftyJSON
 import Alamofire
 import FTPopOverMenu_Swift
+import Eureka
+
 
 class MyViewController2: FormViewController {
     @IBOutlet var textfield: UITextField!
@@ -10,6 +12,26 @@ class MyViewController2: FormViewController {
     @IBOutlet var button: UIButton!
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        form +++
+            MultivaluedSection(multivaluedOptions: [.Reorder, .Insert, .Delete],
+                               header: "Multivalued TextField",
+                               footer: ".Insert adds a 'Add Item' (Add New Tag) button row as last cell.") {
+                                $0.addButtonProvider = { section in
+                                    return ButtonRow(){
+                                        $0.title = "Add New Tag"
+                                    }
+                                }
+                                $0.multivaluedRowToInsertAt = { index in
+                                    return NameRow() {
+                                        $0.placeholder = "Tag Name"
+                                    }
+                                }
+                                $0 <<< NameRow() {
+                                    $0.placeholder = "Tag Name"
+                                }
+                                $0.tag = "my tag"
+        }
     }
     
     @IBAction func click() {
