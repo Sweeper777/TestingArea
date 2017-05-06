d4e97a6a6f6c2067a51a3136b889bf2ebe527eec
Sweeper777 <sumulang@gmail.com>
2017-05-06 07:59:32 +0100

    tested uploading image

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -29,11 +29,24 @@ class MyViewController2: UIViewController, GIDSignInUIDelegate {
     override func viewDidLoad() {
         storage = FIRStorage.storage().reference(forURL: storageURL)
         test1()
+        test2()
     }
     
     func test1() {
         GIDSignIn.sharedInstance().uiDelegate = self
         GIDSignIn.sharedInstance().signIn()
+    }
+    
+    func test2() {
+        let uploadRef = storage.child("images/settings.png")
+        let data = UIImagePNGRepresentation(#imageLiteral(resourceName: "settings"))
+        _ = uploadRef.put(data!, metadata: nil, completion: { (_, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Upload successful!")
+            }
+        })
     }
     
     @IBAction func click() {
