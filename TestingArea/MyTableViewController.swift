6659d0fc312be4f0cbfe6c300671ebc534b3a9aa
Sweeper777 <sumulang@gmail.com>
2017-05-10 06:51:53 +0100

    tested new row

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -4,6 +4,16 @@ import Eureka
 class MyTableViewController: FormViewController {
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        form +++ MultiSelectSegmentedRow("Hi") {
+            row in
+            row.title = "Answers"
+            row.value = NSMutableArray(array: ["A", "D"])
+            }.onChange {
+                row in
+                print(row.value!)
         }
     }
 }
