4a9c886fe4b86944ff062d4ffb5042ded78a305c
Sweeper777 <sumulang@gmail.com>
2017-05-10 06:49:52 +0100

    changed MyTableViewController to a form

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -1,21 +1,9 @@
 import UIKit
+import Eureka
 
-class MyTableViewController: UITableViewController {
+class MyTableViewController: FormViewController {
     
-     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
     
-    @IBAction func activity(_ sender: UIBarButtonItem) {
-        let activityViewController = UIActivityViewController(activityItems: ["hello world"], applicationActivities: nil)
-        navigationController?.present(activityViewController, animated: true) {
-            
         }
     }
 }
