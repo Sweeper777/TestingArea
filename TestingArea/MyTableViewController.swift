ca48f7b1d4b7c7ed6fdb3334ebefdb35a1dc986a
Sweeper777 <sumulang@gmail.com>
2017-05-13 07:46:13 +0100

    deleted old tests

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -1,19 +1,10 @@
 import UIKit
-import Eureka
 
-class MyTableViewController: FormViewController {
+class MyTableViewController: UITableViewController {
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        form +++ MultiSelectSegmentedRow("Hi") {
-            row in
-            row.title = "Answers"
-            row.value = NSMutableArray(array: ["A", "D"])
-            }.onChange {
-                row in
-                print(row.value!)
-        }
     }
 }
--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -5,20 +5,7 @@ import Alamofire
 import FTPopOverMenu_Swift
 import Eureka
 import FirebaseAuth
-import FirebaseStorage
-import FirebaseRemoteConfig
 import GoogleSignIn
-
-public struct MyOptions: OptionSet {
-    public let rawValue: Int
-    
-    public init(rawValue: Int) {
-        self.rawValue = rawValue
-    }
-    
-    public static let one = MyOptions(rawValue: 1 << 0)
-    public static let two = MyOptions(rawValue: 1 << 1)
-}
 
 class MyViewController2: UIViewController, GIDSignInUIDelegate {
     @IBOutlet var textfield: UITextField!
@@ -26,15 +13,8 @@ class MyViewController2: UIViewController, GIDSignInUIDelegate {
     @IBOutlet var button: UIButton!
     @IBOutlet var imageView: UIImageView!
     
-    var storage: FIRStorageReference!
-    var remoteConfig: FIRRemoteConfig!
-    
     override func viewDidLoad() {
-        storage = FIRStorage.storage().reference(forURL: storageURL)
-        remoteConfig = FIRRemoteConfig.remoteConfig()
-        remoteConfig.setDefaults(["wordOfTheDay": "Default" as NSObject])
         test1()
-        test10()
     }
     
     func test1() {
@@ -42,121 +22,7 @@ class MyViewController2: UIViewController, GIDSignInUIDelegate {
         GIDSignIn.sharedInstance().signIn()
     }
     
-    func test2() {
-        let uploadRef = storage.child("images/settings.png")
-        let data = UIImagePNGRepresentation(#imageLiteral(resourceName: "settings"))
-        _ = uploadRef.put(data!, metadata: nil, completion: { (_, error) in
-            if error != nil {
-                print(error!)
-            } else {
-                print("Upload successful!")
-            }
-        })
-    }
-    
-    func test3() {
-        let uploadRef = storage.child("images/settings.png")
-        let data = UIImagePNGRepresentation(#imageLiteral(resourceName: "settings"))
-        let metadata = FIRStorageMetadata()
-        metadata.contentType = "image/png"
-        _ = uploadRef.put(data!, metadata: metadata, completion: { (_, error) in
-            if error != nil {
-                print(error!)
-            } else {
-                print("Upload successful!")
-            }
-        })
-    }
-    
-    func test4() {
-        let downloadRef = storage.child("images/my_image.png")
-        downloadRef.data(withMaxSize: 1024 * 1024) { (data, error) in
-            if error != nil {
-                print(error!)
-            } else {
-                self.imageView.image = UIImage(data: data!)
-            }
-        }
-    }
-    
-    func test5() {
-        let deleteRef = storage.child("images/settings.png")
-        deleteRef.delete { (error) in
-            if error != nil {
-                print(error!)
-            } else {
-                print("Deleted successfully")
-            }
-        }
-    }
-    
-    func test6() {
-        let uploadRef = storage.child("texts/dummy.txt")
-        let data = "Hello World".data(using: .utf8)
-        let metadata = FIRStorageMetadata()
-        metadata.contentType = "text/plain"
-        uploadRef.put(data!, metadata: metadata) { (_, error) in
-            if error != nil {
-                print(error!)
-            } else {
-                print("Success!")
-            }
-        }
-    }
-    
-    func test7() {
-        let downloadRef = storage.child("texts/dummy.txt")
-        downloadRef.data(withMaxSize: 1024 * 1024) { (data, error) in
-            if error != nil {
-                print(error!)
-            } else {
-                self.textview.text = String(data: data!, encoding: .utf8)
-            }
-        }
-    }
-    
-    func test8() {
-        let uploadRef = storage.child("texts/dummy.txt")
-        let data = "Hello World".data(using: .utf8)
-        let metadata = FIRStorageMetadata()
-        metadata.contentType = "text/plain"
-        uploadRef.put(data!, metadata: metadata) { (_, error) in
-            if error != nil {
-                print(error!)
-            } else {
-                print("First: Success!")
-                let uploadRef2 = self.storage.child("texts/dummy.txt")
-                let data2 = "Hello World Number 2!".data(using: .utf8)
-                let metadata2 = FIRStorageMetadata()
-                metadata2.contentType = "text/plain"
-                uploadRef2.put(data2!, metadata: metadata2) { (_, error) in
-                    if error != nil {
-                        print(error!)
-                    } else {
-                        print("Second: Success!")
-                    }
-                }
-            }
-        }
-    }
-    
-    func test9() {
-        let wordOfTheDay = remoteConfig["wordOfTheDay"].stringValue!
-        self.textview.text = "Word of the day: \(wordOfTheDay)"
-    }
-    
-    func test10() {
-        remoteConfig.fetch(withExpirationDuration: 10, completionHandler: { (status, error) in
-            if error != nil {
-                print(error!)
-            } else {
-                self.remoteConfig.activateFetched()
-                self.test9()
-            }
-        })
-    }
-    
     @IBAction func click() {
-        test10()
+        
     }
 }
