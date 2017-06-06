67e22c7640bd8ba46d898e24395ab5cc8bd6648c
Sweeper777 <sumulang@gmail.com>
2017-06-06 07:18:33 +0100

    tested an SO question

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -2,8 +2,12 @@ import UIKit
 import MessageUI
 import CoreData
 
-class MyViewController: UIViewController, MFMailComposeViewControllerDelegate {
-    @IBOutlet var webView: UIWebView!
-    @IBOutlet var webView2: UIWebView!
+class MyViewController: UINavigationController {
+    override func viewDidLoad() {
+        navigationBar.topItem?.title = "Carpool"
+        
+        let languagesSpinner = UIBarButtonItem(title: "PT", style: .done, target: nil, action: nil)
+        navigationItem.setRightBarButton(languagesSpinner, animated: true)
+    }
 }
 
