6f9a69517516768bd807d78238f72af54c246a03
Sweeper777 <sumulang@gmail.com>
2017-06-11 08:05:39 +0100

    removed some tests

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -4,7 +4,6 @@ import FirebaseAuth
 import GoogleSignIn
 import Eureka
 import Alamofire
-import PromiseKit
 
 class MyViewController2: FormViewController, GIDSignInUIDelegate {
     @IBOutlet var textfield: UITextField!
@@ -13,10 +12,7 @@ class MyViewController2: FormViewController, GIDSignInUIDelegate {
     @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        
-        
     }
     
     func test1() {
@@ -32,34 +28,4 @@ class MyViewController2: FormViewController, GIDSignInUIDelegate {
     }
     
     
-}
-
-public protocol Protocol1 {
-    associatedtype RequestType
-    associatedtype ResultType
-    func execute(req: RequestType) -> Promise<ResultType>
-}
-
-struct Implemented1 : Protocol1 {
-    typealias ResultType = Bool
-    typealias RequestType = String
-    func execute(req : String) -> Promise<Bool> {
-        return Promise { fulfill, reject in
-            fulfill(true)
-        }
-    }
-}
-func login() -> Promise<NSDictionary> {
-    return Promise.init { fulfill, reject in
-        Alamofire.request(try! URLRequest(url: "", method: .get))
-            .validate()
-            .responseJSON { response in
-                switch response.result {
-                case .success(let dict):
-                    fulfill(dict as! NSDictionary)
-                case .failure(let error):
-                    reject(error)
-                }
-        }
-    }
 }
