db0cc64ffc714fc380966099da8ef3bf9c5d481b
Sweeper777 <sumulang@gmail.com>
2017-06-21 06:56:30 +0100

    added model and dispose bag

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -1,6 +1,10 @@
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MyTableViewController: UITableViewController {
+    let model = Variable<[String]>(["Tom", "Dick", "Harry", "Donald", "Lucy", "Lily", "Olivia"])
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         
