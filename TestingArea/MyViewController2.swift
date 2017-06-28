30013e0d7ae9924ba47e9cd5044ea372ecc07eb1
Sweeper777 <sumulang@gmail.com>
2017-06-28 06:54:12 +0100

    tested how different text styles look like

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -12,13 +12,18 @@ class MyViewController2: UIViewController, GIDSignInUIDelegate {
     @IBOutlet var button: UIButton!
     
     let disposeBag = DisposeBag()
-    
+    let fontStyles: [UIFontTextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     override func viewDidLoad() {
         super.viewDidLoad()
         textview.text = "Testing Testing Testing"
     }
-        
+    
+    var i = 0
     @IBAction func click() {
-        
+        textview.font = UIFont.preferredFont(forTextStyle: fontStyles[i])
+        i += 1
+        if i == fontStyles.count {
+            i = 0
+        }
     }
 }
