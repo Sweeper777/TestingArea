86de3b02d3fd501e6bdab5ee2c83841bd8d76d2c
Sweeper777 <sumulang@gmail.com>
2017-06-28 06:55:00 +0100

    recorded results when large text

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -14,6 +14,7 @@ class MyViewController2: UIViewController, GIDSignInUIDelegate {
     let disposeBag = DisposeBag()
     let fontStyles: [UIFontTextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
+    // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
     override func viewDidLoad() {
         super.viewDidLoad()
         textview.text = "Testing Testing Testing"
