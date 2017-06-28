e9e327ed1c53c155f8f9e3e7224f3813281ae728
Sweeper777 <sumulang@gmail.com>
2017-06-28 06:54:43 +0100

    recorded results under normal settings

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -13,6 +13,7 @@ class MyViewController2: UIViewController, GIDSignInUIDelegate {
     
     let disposeBag = DisposeBag()
     let fontStyles: [UIFontTextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
+    // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
     override func viewDidLoad() {
         super.viewDidLoad()
         textview.text = "Testing Testing Testing"
