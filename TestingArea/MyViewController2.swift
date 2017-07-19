f8f7bf1054e896f9082cfcddb3f8a0bbab78f53d
Sweeper777 <sumulang@gmail.com>
2017-07-19 08:21:21 +0800

    added a clock using code instead

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -3,6 +3,7 @@ import SwiftyUtils
 import RxSwift
 import CoreLocation
 import AddressBookUI
+import EZClockView
 
 class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
@@ -16,6 +17,36 @@ class MyViewController2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         textview.text = "Testing Testing Testing"
+        
+        let clock = EZClockView(frame: view.bounds)
+        
+        // Setup time
+        clock.hours = 7
+        clock.minutes = 12
+        clock.seconds = 47
+        
+        // Customize face with border thickness and background color
+        clock.faceBorderWidth = 3
+        clock.faceBackgroundColor = UIColor(white: 0.9, alpha: 1)
+        
+        // Set the thickness of any needle
+        clock.hoursThickness = 5
+        
+        // Set the length of any needle (1 means the needle is as long as the face radius)
+        clock.minutesLength = 0.5
+        
+        // Offset is how far beyond the center the needle can go back.
+        clock.secondsOffset = 5
+        
+        // You can customize several markings properties
+        clock.markingBorderSpacing = 5
+        clock.markingHourLength = 10
+        clock.markingMinuteLength = 5
+        clock.markingHourThickness = 3
+        
+        clock.markingMinuteColor = UIColor.darkGray
+        
+        view.addSubview(clock)
     }
     
     @IBAction func click() {
