7fc6822597358e9a8da0816d0ab3c38a374d4183
Sweeper777 <sumulang@gmail.com>
2017-08-13 07:36:48 +0100

    tested solution of an SO answer

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -7,12 +7,32 @@ class MyTableViewController: UITableViewController {
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
-        tableView.delegate = nil
-        tableView.dataSource = nil
+        tableView.allowsSelection = false
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.value.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = MyCell()
+        cell.textLabel?.text = model.value[indexPath.row]
+        cell.isUserInteractionEnabled = true
+        return cell
+    }
+}
+
+class MyCell: UITableViewCell {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        backgroundColor = .red // or some other color
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        backgroundColor = .white
         
-        model.asObservable().bind(to: tableView.rx.items(cellIdentifier: "myCell", cellType: UITableViewCell.self)) {
-            row, model, cell in
-            cell.textLabel?.text = model
-        }.disposed(by: disposeBag)
     }
 }
