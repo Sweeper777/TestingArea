ee068c97f7d68a99644485f17901c20a99471f19
Sweeper777 <sumulang@gmail.com>
2017-08-18 07:48:17 +0100

    tested an SO question

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,12 +1,12 @@
 import UIKit
 import SwiftyUtils
 import RxSwift
-import Eureka
 
-class MyViewController2: FormViewController {
+class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
+    @IBOutlet var webview: UIWebView!
     
     let disposeBag = DisposeBag()
     let fontStyles: [UIFontTextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
@@ -14,39 +14,16 @@ class MyViewController2: FormViewController {
     // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Hello"
-        form +++ MySelectorRow(tag: "someTag") {
-            row in
-        }
-        
+        let html = try! String.init(contentsOf: URL(string: "https://motherfuckingwebsite.com")!) +  "<style>body { background: red !important; }<style/>Hello"
+        webview.loadHTMLString(html, baseURL: nil)
+//        let styles = "<style>body { background: red !important; }<style/>Hello"
+//        webview.loadHTMLString(styles, baseURL: URL(string: "https://motherfuckingwebsite.com"))
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        self.navigationItem.backBarButtonItem = UIBarButtonItem()
-        self.navigationItem.backBarButtonItem!.title = ""
     }
     
     @IBAction func click() {
-    }
-}
-
-class SelectorController: UIViewController, TypedRowControllerType {
-    public var onDismissCallback: ((UIViewController) -> ())?
-    var row: RowOf<Int>!
-    var completionCallback: ((UIViewController) -> ())?
-}
-
-class MySelectorRow: SelectorRow<PushSelectorCell<Int>, SelectorController> {
-    required init(tag: String?, _ initializer: ((MySelectorRow) -> ())) {
-        super.init(tag: tag)
-        initializer(self)
-        presentationMode = PresentationMode.show(controllerProvider: ControllerProvider.storyBoard(storyboardId: "first", storyboardName: "Main", bundle: nil), onDismiss: {
-            _ in
-        })
-    }
-    
-    required convenience init(tag: String?) {
-        self.init(tag: tag)
     }
 }
