f9b11c1c88010ae161431ce6a9292c96367b87ef
Sweeper777 <sumulang@gmail.com>
2017-09-06 08:23:18 +0100

    tested UIStackView responding to size classes

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,13 +1,35 @@
 import UIKit
 import MessageUI
 import CoreData
+import SwiftyUtils
 
-class MyViewController: UINavigationController {
-    override func viewDidLoad() {
-        navigationBar.topItem?.title = "Carpool"
-        
-        let languagesSpinner = UIBarButtonItem(title: "PT", style: .done, target: nil, action: nil)
-        navigationItem.setRightBarButton(languagesSpinner, animated: true)
-    }
+class MyViewController: UIViewController {
+    @IBOutlet var stackView: UIStackView!
+    @IBOutlet var heightConstraint: NSLayoutConstraint!
+    @IBOutlet var widthConstraint: NSLayoutConstraint!
+    
+//    override func viewDidLoad() {
+//        if view.height < view.width {
+//            NSLayoutConstraint.activate([widthConstraint])
+//            NSLayoutConstraint.deactivate([heightConstraint])
+//            stackView.axis = .horizontal
+//        } else {
+//            NSLayoutConstraint.deactivate([widthConstraint])
+//            NSLayoutConstraint.activate([heightConstraint])
+//            stackView.axis = .vertical
+//        }
+//    }
+//    
+//    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//        if size.height < size.width {
+//            NSLayoutConstraint.activate([widthConstraint])
+//            NSLayoutConstraint.deactivate([heightConstraint])
+//            stackView.axis = .horizontal
+//        } else {
+//            NSLayoutConstraint.deactivate([widthConstraint])
+//            NSLayoutConstraint.activate([heightConstraint])
+//            stackView.axis = .vertical
+//        }
+//    }
 }
 
