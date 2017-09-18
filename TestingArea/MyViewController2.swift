c33480f454c8d09dbfd1d4a2a4147d6e4fd74976
Sweeper777 <sumulang@gmail.com>
2017-09-18 16:33:59 +0100

    tested if I can use both image and text

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -3,12 +3,14 @@ import SwiftyUtils
 import RxSwift
 import RealmSwift
 import SwiftyAnimate
+import NGORoundedButton
 
 class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
     @IBOutlet var crosshair: CrossHair!
+    @IBOutlet var ngoBtn: NGORoundedButton!
     
     let disposeBag = DisposeBag()
     let fontStyles: [UIFontTextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
@@ -16,6 +18,11 @@ class MyViewController2: UIViewController {
     // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
     override func viewDidLoad() {
         super.viewDidLoad()
+        ngoBtn.color = NGORoundedButtonColor.green
+        ngoBtn.type = .customText
+        ngoBtn.customImage = #imageLiteral(resourceName: "home")
+        ngoBtn.customText = "Home"
+        
         
     }
     
