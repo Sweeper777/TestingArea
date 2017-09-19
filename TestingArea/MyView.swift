c2faf8ba117e84b8fa311b966d01eb59d4e1a4ee
Sweeper777 <sumulang@gmail.com>
2017-09-19 07:25:14 +0100

    tested drawing text

--- TestingArea/MyView.swift
+++ TestingArea/MyView.swift
@@ -6,33 +6,9 @@ class CrossHair: UIView {
     var touched = false
     
     override func draw(_ rect: CGRect) {
-        let newRect = CGRect(
-            x: rect.x + rect.width / 4,
-            y: rect.y + rect.height / 4,
-            width: rect.width / 2,
-            height: rect.height / 2
-        )
-        let path = UIBezierPath(ovalIn: newRect)
-        let strokeWidth = min(rect.width, rect.height) * 0.05
-        path.lineWidth = strokeWidth
-        if touched {
-            UIColor.red.setStroke()
-        } else {
-            UIColor.black.setStroke()
-        }
-        path.stroke()
+        let text = "Hello world" as NSString
+        text.draw(in: rect, withAttributes: [:])
         
-        let horizontalLine = UIBezierPath()
-        horizontalLine.move(to: CGPoint(x: rect.x, y: rect.midY))
-        horizontalLine.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
-        horizontalLine.lineWidth = strokeWidth
-        horizontalLine.stroke()
-        
-        let verticalLine = UIBezierPath()
-        verticalLine.move(to: CGPoint(x: rect.midX, y: rect.y))
-        verticalLine.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
-        verticalLine.lineWidth = strokeWidth
-        verticalLine.stroke()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
