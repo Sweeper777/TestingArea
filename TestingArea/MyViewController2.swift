abdbb4446435542e13b672bb203ca49544164ee9
Sweeper777 <sumulang@gmail.com>
2017-09-29 07:11:11 +0100

    tested an SO question

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -6,6 +6,38 @@ import SwiftyAnimate
 import NGORoundedButton
 import JTImageButton
 
+struct RandomItems
+{
+    var items : [String]
+    var seen  = 0
+    
+    init(_ items:[String])
+    { self.items = items }
+    
+    mutating func next() -> String
+    {
+        let index = Int(arc4random_uniform(UInt32(items.count - seen)))
+        let item  = items.remove(at:index)
+        items.append(item)
+        seen = (seen + 1) % items.count
+        return item
+    }
+    
+    func toPropertyList() -> [String: Any] {
+        return [
+            "items": items,
+            "seen": seen
+        ]
+    }
+    
+    init?(propertyList: [String: Any]) {
+        guard let items = propertyList["items"] as? [String] else { return nil }
+        guard let seen = propertyList["seen"] as? Int else { return nil }
+        self.items = items
+        self.seen = seen
+    }
+}
+
 class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
     @IBOutlet var textfield: UITextField!
@@ -18,6 +50,12 @@ class MyViewController2: UIViewController {
     // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
     override func viewDidLoad() {
         super.viewDidLoad()
+        let myStruct = RandomItems(["a"])
+        
+        UserDefaults.standard.set(myStruct.toPropertyList(), forKey: "myStruct")
+        let retrieved = UserDefaults.standard.object(forKey: "myStruct") as! [String: Any]
+        print(RandomItems(propertyList: retrieved)!.items)
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
