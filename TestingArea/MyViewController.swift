27bf5048765fcc1f23ef237bf99d66cd970ecf76
Sweeper777 <sumulang@gmail.com>
2017-11-17 07:17:40 +0000

    removed tests in MyVC

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,40 +1,7 @@
 import UIKit
-import MessageUI
-import CoreData
-import SwiftyUtils
-import SwiftyButton
 
 class MyViewController: UIViewController {
-    @IBOutlet var stackView: UIStackView!
-    @IBOutlet var heightConstraint: NSLayoutConstraint!
-    @IBOutlet var widthConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
-        func shouldContinueToEnlarge(targetSize: CGSize, currentSize: CGSize) -> Bool {
-            return targetSize.height > currentSize.height && targetSize.width > currentSize.width
-        }
-        
-        func fontSizeThatFits(size: CGSize, text: NSString, font: UIFont) -> CGFloat {
-            var fontToTest = font.withSize(0)
-            var currentSize = text.size(attributes: [NSFontAttributeName: fontToTest])
-            var fontSize = CGFloat(1)
-            while shouldContinueToEnlarge(targetSize: size, currentSize: currentSize) {
-                fontToTest = fontToTest.withSize(fontSize)
-                currentSize = text.size(attributes: [NSFontAttributeName: fontToTest])
-                fontSize += 1
-            }
-            return fontSize - 1
-        }
-
-        let label = UILabel(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
-        label.text = "Hello World"
-        label.textColor = .white
-        label.backgroundColor = .black
-        let fontSize = fontSizeThatFits(size: label.frame.size, text: label.text! as NSString, font: label.font)
-        label.font = label.font.withSize(fontSize)
-        self.view.addSubview(label)
-
-        print(PressableButton.Defaults.shadowHeight)
-        print(PressableButton.Defaults.colors)
     }
 }
 
