89a57375d001ba12492f4851035cedfa17784177
Sweeper777 <sumulang@gmail.com>
2017-11-18 08:31:34 +0000

    added Material Components

---
+++ Pods/Target Support Files/MDFInternationalization/MDFInternationalization-umbrella.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+#import "MDFInternationalization.h"
+#import "MDFRTL.h"
+#import "UIImage+MaterialRTL.h"
+#import "UIView+MaterialRTL.h"
+
+FOUNDATION_EXPORT double MDFInternationalizationVersionNumber;
+FOUNDATION_EXPORT const unsigned char MDFInternationalizationVersionString[];
+
