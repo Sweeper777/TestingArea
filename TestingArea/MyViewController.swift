69123e5aeb5b987a5cec570cc5940ca3deee7fa8
Sweeper777 <sumulang@gmail.com>
2017-11-18 08:31:19 +0000

    tried reading text file

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class MyViewController: UIViewController {
     override func viewDidLoad() {
+        let string = try! String(contentsOfFile: Bundle.main.path(forResource: "test", ofType: "txt")!)
+        print(string.characters.last! == "\n")
     }
 }
 
