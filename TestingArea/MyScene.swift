6e7531f0909f36bdec86a1dcd4dadefa59221423
Sweeper777 <sumulang@gmail.com>
2018-02-11 08:14:41 +0000

    tested static physics body

--- TestingArea/MyScene.swift
+++ TestingArea/MyScene.swift
@@ -6,6 +6,12 @@ class MyScene: SCNScene {
         cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
+        
         self.rootNode.addChildNode(cameraNode)
+        
+        let ground = SCNNode(geometry: SCNBox(width: 10, height: 1, length: 10, chamferRadius: 0))
+        ground.position = SCNVector3(0, -1, 0)
+        ground.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        
     }
 }
