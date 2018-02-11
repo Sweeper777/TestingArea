c35eeedc47ef400f02d80a4d98bf59d767c1b1da
Sweeper777 <sumulang@gmail.com>
2018-02-11 08:14:58 +0000

    tested dynamic physics body

--- TestingArea/MyScene.swift
+++ TestingArea/MyScene.swift
@@ -13,5 +13,9 @@ class MyScene: SCNScene {
         ground.position = SCNVector3(0, -1, 0)
         ground.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
         
+        let box = SCNNode(geometry: SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0.1))
+        box.position = SCNVector3(0, 10, 0)
+        box.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        box.physicsBody?.applyForce(SCNVector3(1, 0, 0), asImpulse: true)
     }
 }
