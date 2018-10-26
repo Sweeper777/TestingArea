691e9e71412127edaa80cf6b2f94e57c1cde4f91
Sweeper777 <sumulang@gmail.com>
2018-10-26 09:45:17 +0100

    stylised bottom VC

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -2,8 +2,9 @@ import UIKit
 
 class MyViewController: UIViewController {
     override func viewDidLoad() {
-        let string = try! String(contentsOfFile: Bundle.main.path(forResource: "test", ofType: "txt")!)
-        print(string.characters.last! == "\n")
+        view.layer.cornerRadius = 10
+        view.layer.borderWidth = 2
+        view.backgroundColor = UIColor.white.withAlphaComponent(0.7)
     }
 }
 
