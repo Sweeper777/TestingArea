799e1a5d677666c58d40fea48552764b7f6642ad
Sweeper777 <sumulang@gmail.com>
2018-10-28 10:03:27 +0000

    tested Iterable conformation

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -40,5 +40,16 @@ class MyViewController2: UIViewController {
     }
     
     @IBAction func click() {
+        var triangularArray: TriangularArray<Int> = [
+            [1],
+            [2, 3],
+            [4, 5, 6],
+            [7, 8, 9, 10],
+            [11, 12, 13, 14, 15]
+        ]
+        
+        for element in triangularArray {
+            print(element)
+        }
     }
 }
