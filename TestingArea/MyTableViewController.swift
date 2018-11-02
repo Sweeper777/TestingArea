97d1f5e964f3b40e40e484fe4ab9fea5053a0a8a
Sweeper777 <sumulang@gmail.com>
2018-11-02 08:17:17 +0000

    deleted previous tests in MyTableVC

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -1,20 +1,10 @@
 import UIKit
-import RxSwift
-import RxCocoa
 
 class MyTableViewController: UITableViewController {
-    let model = Variable<[String]>(["Tom", "Dick", "Harry", "Donald", "Lucy", "Lily", "Olivia"])
-    let disposeBag = DisposeBag()
+    
+    let model = ["Hello", "World", "Bye"]
     
     override func viewDidLoad() {
-        tableView.allowsSelection = false
-        if #available(iOS 10.0, *) {
-            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (_) in
-                print("timer")
-            }
-        } else {
-            // Fallback on earlier versions
-        }
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -22,17 +12,9 @@ class MyTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return model.value.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = model.value[indexPath.row]
-        cell.isUserInteractionEnabled = true
         return cell
-    }
-    
-    @IBAction func back() {
-        self.dismiss(animated: true, completion: nil)
     }
 }
