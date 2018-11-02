be0891398b0811376b4c5926356d4ee675b1f11d
Sweeper777 <sumulang@gmail.com>
2018-11-02 08:18:02 +0000

    populated table view with CardTableViewCells

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -13,9 +13,12 @@ class MyTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CardTableViewCell
+        cell.customLabel.text = model[indexPath.row]
         return cell
     }
 }
