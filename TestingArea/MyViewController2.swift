59ea5f8e65f64c2fb5cc10b8a7575e789a919c45
Sweeper777 <sumulang@gmail.com>
2018-11-07 06:59:39 +0000

    added dummy data for bar chart

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -7,6 +7,7 @@ class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
+    @IBOutlet var barChart: BarChartView!
     
     
     let disposeBag = DisposeBag()
@@ -15,6 +16,15 @@ class MyViewController2: UIViewController {
     // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
     override func viewDidLoad() {
         super.viewDidLoad()
+        barChart.percentages = [
+            (0.5, 0.7, 0.6, 0.3),
+            (1.0, 0.3, 0.6, 0.8),
+            (1.0, 0.5, 0.7, 0.3),
+            (0.8, 0.5, 0.5, 0.9),
+            (0.9, 0.3, 0.4, 0.5),
+            (1.0, 0.4, 0.5, 0.6),
+            (0.5, 0.9, 0.8, 0.7)
+        ]
     }
     
     @IBAction func click() {
