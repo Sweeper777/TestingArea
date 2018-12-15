097a63734531d1fdc8188b485a4e0b16259debfb
Sweeper777 <sumulang@gmail.com>
2018-12-15 16:33:07 +0200

    made MyViewController a form

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,83 +1,13 @@
 import UIKit
-import ISHPullUp
+import Eureka
 
-class MyViewController: UIViewController, ISHPullUpSizingDelegate, ISHPullUpStateDelegate {
-    @IBOutlet private weak var handleView: ISHPullUpHandleView!
-    @IBOutlet private weak var rootView: UIView!
-    @IBOutlet private weak var scrollView: UIScrollView!
-    @IBOutlet private weak var topLabel: UILabel!
-    @IBOutlet private weak var topView: UIView!
-    
-    private var firstAppearanceCompleted = false
-    weak var pullUpController: ISHPullUpViewController!
-    
-    // we allow the pullUp to snap to the half way point
-    private var halfWayPoint = CGFloat(0)
-    
+class MyViewController: FormViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        firstAppearanceCompleted = true;
-    }
-    
-    // MARK: ISHPullUpSizingDelegate
-    
-    func pullUpViewController(_ pullUpViewController: ISHPullUpViewController, maximumHeightForBottomViewController bottomVC: UIViewController, maximumAvailableHeight: CGFloat) -> CGFloat {
-        let totalHeight = rootView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
         
-        // we allow the pullUp to snap to the half way point
-        // we "calculate" the cached value here
-        // and perform the snapping in ..targetHeightForBottomViewController..
-        halfWayPoint = totalHeight / 2.0
-        return totalHeight
-    }
-    
-    func pullUpViewController(_ pullUpViewController: ISHPullUpViewController, minimumHeightForBottomViewController bottomVC: UIViewController) -> CGFloat {
-        return topView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height;
-    }
-    
-    func pullUpViewController(_ pullUpViewController: ISHPullUpViewController, targetHeightForBottomViewController bottomVC: UIViewController, fromCurrentHeight height: CGFloat) -> CGFloat {
-        // if around 30pt of the half way point -> snap to it
-        if abs(height - halfWayPoint) < 30 {
-            return halfWayPoint
-        }
-        
-        // default behaviour
-        return height
-    }
-    
-    func pullUpViewController(_ pullUpViewController: ISHPullUpViewController, update edgeInsets: UIEdgeInsets, forBottomViewController bottomVC: UIViewController) {
-        // we update the scroll view's content inset
-        // to properly support scrolling in the intermediate states
-        scrollView.contentInset = edgeInsets;
-    }
-    
-    // MARK: ISHPullUpStateDelegate
-    
-    func pullUpViewController(_ pullUpViewController: ISHPullUpViewController, didChangeTo state: ISHPullUpState) {
-        topLabel.text = textForState(state);
-        handleView.setState(ISHPullUpHandleView.handleState(for: state), animated: firstAppearanceCompleted)
-        
-        // Hide the scrollview in the collapsed state to avoid collision
-        // with the soft home button on iPhone X
-        UIView.animate(withDuration: 0.25) { [weak self] in
-            self?.scrollView.alpha = (state == .collapsed) ? 0 : 1;
-        }
-    }
-    
-    private func textForState(_ state: ISHPullUpState) -> String {
-        switch state {
-        case .collapsed:
-            return "Drag up or tap"
-        case .intermediate:
-            return "Intermediate"
-        case .dragging:
-            return "Hold on"
-        case .expanded:
-            return "Drag down or tap"
+        form +++ LatitudeSelectorRow(tag: "foo") {
+            row in
+            row.value = 30
         }
     }
 }
