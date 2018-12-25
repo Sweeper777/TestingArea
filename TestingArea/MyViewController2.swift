c6c583e6fcf0af96529dad7286b2059ecdbd162c
Sweeper777 <sumulang@gmail.com>
2018-12-25 11:57:24 +0800

    tested animating with delay

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -5,15 +5,8 @@ import UserNotifications
 @available(iOS 10.0, *)
 class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
-    @IBOutlet var textfield: UITextField! {
-        didSet {
-            print("XXXXXXXXXX \(textfield)")
-        }
-    }
+    @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
-    @IBOutlet var barChart: BarChartView!
-    
-    let center = UNUserNotificationCenter.current()
     
     
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
@@ -21,41 +14,28 @@ class MyViewController2: UIViewController {
     // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
     override func viewDidLoad() {
         super.viewDidLoad()
-        barChart.percentages = [
-            (0.5, 0.7, 0.6, 0.3),
-            (1.0, 0.3, 0.6, 0.8),
-            (1.0, 0.5, 0.7, 0.3),
-            (0.8, 0.5, 0.5, 0.9),
-            (0.9, 0.3, 0.4, 0.5),
-            (1.0, 0.4, 0.5, 0.6),
-            (0.5, 0.9, 0.8, 0.7)
-        ]
     }
     
     @IBAction func click() {
-        center.getNotificationSettings { (settings) in
-            if settings.authorizationStatus != .authorized {
-                print("Notification not authorised")
-            } else {
-                if #available(iOS 12.0, *) {
-                    let content = UNMutableNotificationContent()
-                    content.title = "Test Title"
-                    content.subtitle = "Test Subtitle"
-                    content.body = "Test Body"
-                    content.sound = UNNotificationSound.defaultCritical
-                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
-                    let request = UNNotificationRequest(identifier: "Test Notification", content: content, trigger: trigger)
-                    self.center.add(request, withCompletionHandler: { (error) in
-                        if let e = error {
-                            print(e.localizedDescription)
-                        }
-                    })
-                } else {
-                    // Fallback on earlier versions
-                }
-
-                
+        UIView.animate(withDuration: 1, animations: {
+            self.textview.transform = CGAffineTransform(scaleX: 2, y: 2)
+        })
+        DispatchQueue.main.async {
+            let result = self.doSomeWork()
+            print(result)
+            UIView.animate(withDuration: 1, animations: {
+                self.textfield.transform = CGAffineTransform(scaleX: 2, y: 2)
+            })
+        }
+    }
+    
+    func doSomeWork() -> Int {
+        var count = 0
+        for i in 0..<100000 {
+            if "\(i)".reversed() == Array("\(i)") {
+                count += 1
             }
         }
+        return count
     }
 }
