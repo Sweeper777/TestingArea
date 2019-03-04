de18656df2672eaa154de491adae14cf2d3d19e3
Sweeper777 <sumulang@gmail.com>
2019-03-04 17:48:17 +0000

    deleted old code

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -16,54 +16,11 @@ class MyViewController2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let myView = UIView(frame: CGRect(x: 10, y: 100, width: 300, height: 400))
-        view.addSubview(myView)
-        myView.backgroundColor = .gray
-        let label1 = UIView(frame: CGRect(x: 10, y: 100, width: 300, height: 50))
-        let label2 = UIView(frame: CGRect(x: 10, y: 100, width: 300, height: 50))
-        myView.addSubview(label1)
-        myView.addSubview(label2)
-        
         label1.backgroundColor = .red
         label2.backgroundColor = .blue
-        
-        label1.snp.makeConstraints { (make) in
-            make.topMargin.equalTo(20)
-            make.leftMargin.equalTo(20)
-            make.rightMargin.equalTo(-20)
-            make.height.equalToSuperview().dividedBy(3)
-        }
-        
-        label2.snp.makeConstraints { (make) in
-            make.top.equalTo(label1.snp.bottom).offset(10)
-            make.leftMargin.equalTo(20)
-            make.rightMargin.equalTo(-20)
-            make.bottomMargin.equalTo(-20)
-        }
     }
     
     @IBAction func click() {
-        let jsonString = """
-[
-{
-"sno": "21",
-"title": "title 1",
-"tableid": "table 1"
-},
-{
-"sno": "19",
-"title": "title 222",
-"tableid": "table 222"
-},
-{
-"sno": "3",
-"title": "title 333",
-"tableid": "table 333"
-}
-]
-"""
-        let json = JSON(parseJSON: jsonString)
-        print(json[0]["sno"].string)
     }
     
     override func overrideTraitCollection(forChild childViewController: UIViewController) -> UITraitCollection? {
