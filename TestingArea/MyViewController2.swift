13c8eb106a711379bd2cc69785951f39dc559527
Sweeper777 <sumulang@gmail.com>
2019-03-22 07:09:02 +0000

    deleted timer stuff

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,14 +1,12 @@
 import UIKit
 import SwiftyUtils
-import SnapKit
 import SwiftyJSON
 
 @available(iOS 10.0, *)
-class MyViewController2: UIViewController, TimerModelDelegate {
+class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
-    
     var timer: TimerModel!
     
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
@@ -16,57 +14,18 @@ class MyViewController2: UIViewController, TimerModelDelegate {
     // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
     override func viewDidLoad() {
         super.viewDidLoad()
-        timer = TimerModel()
-        timer.delegate = self
-        timer.StartTimer(time: 60)
     }
     @IBAction func click() {
     }
     
-    override func overrideTraitCollection(forChild childViewController: UIViewController) -> UITraitCollection? {
-        if view.bounds.width < view.bounds.height {
-            return UITraitCollection(horizontalSizeClass: .compact)
-        } else {
-            return UITraitCollection(horizontalSizeClass: .regular)
-        }
     }
     
-    func timerTextDidChange(_ time: TimerModel, text: String) {
-        textview.text = text
     }
-}
-
-protocol TimerModelDelegate : class {
-    func timerTextDidChange(_ time: TimerModel, text: String)
-}
-
-class TimerModel: NSObject {
-    var myTimer: Timer? = Timer()
-    var timerInterval: TimeInterval = 1.0
-    var timerEnd: TimeInterval = 0.0
-    var timerCount: TimeInterval = 86400.0 // 24 hours
-    weak var delegate: TimerModelDelegate?
-    var timerStr: String = "TIME" {
-        didSet {
-            delegate?.timerTextDidChange(self, text: timerStr)
-        }
     }
     
-    func StartTimer(time: Double) {
-        timerCount = time
-        myTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(UpdateTime), userInfo: nil, repeats: true)
     }
     
-    @objc func UpdateTime() {
-        self.timerCount-=1
-        self.timerStr = self.TimerDate(time: self.timerCount)
-        print(self.timerStr)
     }
     
-    func TimerDate(time:TimeInterval) -> String {
-        let hours = Int(time) / 3600
-        let minutes = Int(time) / 60 % 60
-        let seconds = Int(time) % 60
-        return String(format: "%02i:%02i:%02i", hours, minutes, seconds)
     }
 }
