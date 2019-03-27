016ae1dbc932b5d536033c16af6dfa33d0f4ca82
Sweeper777 <sumulang@gmail.com>
2019-03-27 07:17:19 +0000

    deleted stuff in MyTableVC

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -2,23 +2,4 @@ import UIKit
 
 class MyTableViewController: UITableViewController {
     
-    let model = ["Hello", "World", "Bye"]
-    
-    override func viewDidLoad() {
-        tableView.register(UINib(nibName: "CardTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
-    }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return model.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CardTableViewCell
-        cell.customLabel.text = model[indexPath.row]
-        return cell
-    }
 }
