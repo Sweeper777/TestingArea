e088422f160acadb531c51f7131d323777dcd3d3
Sweeper777 <sumulang@gmail.com>
2019-03-27 07:18:53 +0000

    added GameObject protocol

--- TestingArea/MyView.swift
+++ TestingArea/MyView.swift
@@ -2,6 +2,17 @@ import UIKit
 
 class GameView: UIView {
     override func draw(_ rect: CGRect) {
+}
+
+protocol GameObject {
+    var velocity: (CGFloat, CGFloat) { get set }
+    var zIndex: Int { get set }
+    var position: CGPoint { get set }
+    
+    func draw(in rect: CGRect)
+    
+    func isOutOf(rect: CGRect) -> Bool
+}
         UIColor.red.setFill()
         path.fill()
     }
