91196987f0bf855d1a8c942bedb74a70c15587ff
Sweeper777 <sumulang@gmail.com>
2019-03-28 07:46:51 +0000

    implemented Dot.draw

--- TestingArea/MyView.swift
+++ TestingArea/MyView.swift
@@ -70,6 +70,8 @@ class Dot : GameObject {
     var zIndex: Int
     var position: CGPoint
     
+    func draw(in rect: CGRect) {
+        let path = UIBezierPath(ovalIn: CGRect(x: position.x * rect.width, y: position.y * rect.height, width: 0, height: 0).insetBy(dx: -10, dy: -10))
         UIColor.red.setFill()
         path.fill()
     }
