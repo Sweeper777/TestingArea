966a0fa0ca66ec498ba71ef2df5912e51a63c0cf
Sweeper777 <sumulang@gmail.com>
2019-03-28 07:46:05 +0000

    added Line GameObject

--- TestingArea/MyView.swift
+++ TestingArea/MyView.swift
@@ -13,6 +13,26 @@ protocol GameObject {
     
     func isOutOf(rect: CGRect) -> Bool
 }
+
+class Line : GameObject {
+    
+    init(position: CGPoint, velocity: (CGFloat, CGFloat), zIndex: Int, length: CGFloat, horizontal: Bool, color: UIColor) {
+        self.zIndex = zIndex
+        self.velocity = velocity
+        self.position = position
+        self.length = length
+        self.horizontal = horizontal
+        self.color = color
+    }
+    
+    var zIndex: Int
+    var velocity: (CGFloat, CGFloat)
+    var position: CGPoint
+    var length: CGFloat
+    var horizontal: Bool
+    var color: UIColor
+    
+}
         UIColor.red.setFill()
         path.fill()
     }
