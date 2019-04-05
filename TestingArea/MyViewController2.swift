6f82dfbf4b0b99a65cffd286a5cd36dc0661ee58
Sweeper777 <sumulang@gmail.com>
2019-04-05 09:43:20 +0100

    added image views to show the results

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,12 +1,21 @@
 import UIKit
 import SwiftyUtils
 import SwiftyJSON
+import Vision
+import SwiftOCR
 
 @available(iOS 10.0, *)
 class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
+    
+    @IBOutlet var image1: UIImageView!
+    @IBOutlet var image2: UIImageView!
+    @IBOutlet var image3: UIImageView!
+    @IBOutlet var image4: UIImageView!
+    
+    let image = UIImage(named: "example")!
     
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
