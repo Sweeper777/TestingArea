e7095a470993672c69c9dc87e43cfabe3f6dc40b
Sweeper777 <sumulang@gmail.com>
2019-04-05 09:44:55 +0100

    tested showing text on the image

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -17,11 +17,31 @@ class MyViewController2: UIViewController {
     
     let image = UIImage(named: "example")!
     
+    var results: [VNTextObservation] = [] {
+        didSet {
+            if results.count == 4 {
+                image1.image = processTextObservation(results[0], originalImage: image)
+                image2.image = processTextObservation(results[1], originalImage: image)
+                image3.image = processTextObservation(results[2], originalImage: image)
+                image4.image = processTextObservation(results[3], originalImage: image)
+            }
+        }
+    }
+    
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
     // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
     override func viewDidLoad() {
         super.viewDidLoad()
+        let vnImage = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
+        let textRectsRequest = VNDetectTextRectanglesRequest { (request, error) in
+            if let results = request.results as? [VNTextObservation] {
+                self.results = results
+                print("\(results.count) Results found")
+            }
+        }
+        
+        try? vnImage.perform([textRectsRequest])
     }
     
     @IBAction func click() {
