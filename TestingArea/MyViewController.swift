45e01efd2e72ed8bc42fa698f26965f0ec85f6ef
Sweeper777 <sumulang@gmail.com>
2019-04-11 08:05:58 +0100

    tried out some basic cards

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,5 +1,26 @@
 import UIKit
+import HFCardCollectionViewLayout
 
 class MyViewController: UIViewController{
+    @IBOutlet var collectionView: UICollectionView!
     
+    override func viewDidLoad() {
+        collectionView.allowsSelection = true
+    }
+}
+
+extension MyViewController : HFCardCollectionViewLayoutDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! HFCardCollectionViewCell
+        cell.backgroundColor = .white
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        (collectionView.collectionViewLayout as! HFCardCollectionViewLayout).revealCardAt(index: indexPath.item)
+    }
 }
