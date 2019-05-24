ce2a5c057c562651cef39c1f6cb01aa8bb6c02de
Sweeper777 <sumulang@gmail.com>
2019-05-24 12:49:37 +0100

    added IBOutlet

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -10,7 +10,7 @@ class MyViewController2: UIViewController {
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
     
-    @IBOutlet var webView: WKWebView!
+    @IBOutlet var imageView: UIImageView!
 
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
