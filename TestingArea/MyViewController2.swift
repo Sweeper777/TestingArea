d6c15457ffd91941669316462a30dfab84abdf49
Sweeper777 <sumulang@gmail.com>
2019-05-24 12:51:05 +0100

    tested a stack overflow question

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -4,6 +4,14 @@ import SwiftyJSON
 import Firebase
 import WebKit
 
+private enum Letters {
+    case a
+    case b
+    case c
+    case d
+    case e
+}
+
 @available(iOS 10.0, *)
 class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
@@ -15,9 +23,11 @@ class MyViewController2: UIViewController {
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
     // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
+    
+    private var letters: [Letters] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        letters = [.a, .b, .c, .d, .e]
     }
     
     @IBAction func click() {
