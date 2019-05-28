0fccec4a46d23620c4236b8a86d1069ca23a806e
Sweeper777 <sumulang@gmail.com>
2019-05-28 08:53:06 +0100

    added StringSection

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -3,3 +3,22 @@ import UIKit
 class MyTableViewController: UITableViewController {
     
 }
+
+struct StringSection : AnimatableSectionModelType {
+    var items: [String]
+    
+    init(original: StringSection, items: [String]) {
+        self = original
+        self.items = items
+    }
+    
+    init(items: [String]) {
+        self.items = items
+    }
+    
+    typealias Item = String
+    
+    typealias Identity = String
+    
+    var identity: String { return "" }
+}
