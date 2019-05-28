5ec2345cdbf9ea44221b58c552bac483c8714502
Sweeper777 <sumulang@gmail.com>
2019-05-28 08:53:28 +0100

    tested whether deteting a cell affects selection

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -1,7 +1,31 @@
 import UIKit
+import RxSwift
+import RxDataSources
+import RxCocoa
 
 class MyTableViewController: UITableViewController {
+    let observable = Variable(["Hello", "World", "Bye", "Foo", "Bar"])
+    let disposeBag = DisposeBag()
     
+    override func viewDidLoad() {
+        tableView.dataSource = nil
+        let dataSource = RxTableViewSectionedAnimatedDataSource<StringSection>(configureCell:  {
+            (dataSource, tableView, indexPath, string) -> UITableViewCell in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+            cell.textLabel?.text = string
+            return cell
+        })
+        observable.asObservable()
+            .map {
+                [StringSection(items: $0)]
+            }
+            .bind(to: self.tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+    }
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        observable.value.remove(at: observable.value.count - 1)
+    }
 }
 
 struct StringSection : AnimatableSectionModelType {
