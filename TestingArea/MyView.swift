d808f385342476fdcf0d0b215ec83176ea2768bb
Sweeper777 <sumulang@gmail.com>
2019-05-29 08:57:21 +0100

    added MyCell class

--- TestingArea/MyView.swift
+++ TestingArea/MyView.swift
@@ -1,23 +1,15 @@
 import UIKit
 
-class MyView: UIView {
-   
-    override func draw(_ rect: CGRect) {
-        let colors = [UIColor.red, .blue, .green, .red, .blue, .green, .red, .blue, .green, .red, .blue, .green]
-        let bigCircleRect = self.bounds.insetBy(dx: 16, dy: 16)
-        let bigCircle = UIBezierPath(ovalIn: bigCircleRect)
-        bigCircle.lineWidth = 5
-        UIColor.black.setStroke()
-        bigCircle.stroke()
-        
-        for (index, angle) in stride(from: -CGFloat.pi, to: .pi - .pi / 6, by: .pi / 6).enumerated() {
-            let centerOfCircle = CGPoint(x: (bigCircleRect.width / 2) * cos(angle) + bounds.width / 2,
-                                         y: (bigCircleRect.width / 2) * sin(angle) + bounds.width / 2)
-            let smallCircleRect = CGRect(origin: centerOfCircle, size: .zero).insetBy(dx: -16, dy: -16)
-            let smallCircle = UIBezierPath(ovalIn: smallCircleRect)
-            colors[index].setFill()
-            smallCircle.fill()
-            
+class MyCell: UICollectionViewCell {
+    @IBOutlet var textLabel: UILabel!
+    
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                backgroundColor = .gray
+            } else {
+                backgroundColor = .white
+            }
         }
     }
 }
