73050c16ba7ceca8b2d4b8be0b8dc48157ca9895
Sweeper777 <sumulang@gmail.com>
2019-06-02 11:45:02 +0100

    deleted old stuff

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,8 +1,5 @@
 import UIKit
 import SwiftyUtils
-import SwiftyJSON
-import Firebase
-import WebKit
 
 private enum Letters {
     case a
@@ -31,58 +28,17 @@ class MyViewController2: UIViewController {
     }
     
     @IBAction func click() {
-        let imagePicker = UIImagePickerController()
-        imagePicker.sourceType = .photoLibrary
-        imagePicker.delegate = self
-        
-        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    }
+    
+        }
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
     }
-    
-    func getScannedImage(inputImage: UIImage) -> UIImage? {
-        
-        let openGLContext = EAGLContext(api: .openGLES2)
-        let context = CIContext(eaglContext: openGLContext!)
-        
-        let filter = CIFilter(name: "CIColorControls")
-        let coreImage = CIImage(image: inputImage)
-        
-        filter?.setValue(coreImage, forKey: kCIInputImageKey)
-        //Key value are changable according to your need.
-        filter?.setValue(7, forKey: kCIInputContrastKey)
-        filter?.setValue(1, forKey: kCIInputSaturationKey)
-        filter?.setValue(1.2, forKey: kCIInputBrightnessKey)
-        
-        if let outputImage = filter?.value(forKey: kCIOutputImageKey) as? CIImage {
-            let output = context.createCGImage(outputImage, from: outputImage.extent)
-            return UIImage(cgImage: output!)
-        }
-        return nil
-    }
 }
 
-extension MyViewController2 : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        picker.dismiss(animated: true) { [ weak self] in
-            if let image = info[.originalImage] as? UIImage {
-                self?.imageView.image = self?.getScannedImage(inputImage: image.noir!)
-            }
-        }
-    }
-}
-
-extension UIImage {
-    var noir: UIImage? {
-        let context = CIContext(options: nil)
-        guard let currentFilter = CIFilter(name: "CIPhotoEffectNoir") else { return nil }
-        currentFilter.setValue(CIImage(image: self), forKey: kCIInputImageKey)
-        if let output = currentFilter.outputImage,
-            let cgImage = context.createCGImage(output, from: output.extent) {
-            return UIImage(cgImage: cgImage, scale: scale, orientation: imageOrientation)
-        }
-        return nil
     }
 }
