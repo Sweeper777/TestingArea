8bf29bbcad42ee82a297691fd5c0590d4b7b96fd
Sweeper777 <sumulang@gmail.com>
2019-07-06 16:20:08 +0100

    set up table view

--- TestingAreaForMac/TestingAreaForMac/ViewController.swift
+++ TestingAreaForMac/TestingAreaForMac/ViewController.swift
@@ -2,4 +2,14 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet var tableView: NSTableView!
+    
+    let dataSource = ["Hello", "World", "Foo", "Bar"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
 }
