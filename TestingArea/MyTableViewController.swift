cff90c22ad6bcd75a09d0d7bc22298de35a0a586
Sweeper777 <sumulang@gmail.com>
2019-07-09 10:23:20 +0100

    changed MyTableViewController to be an actual table VC

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -4,81 +4,52 @@ import RxDataSources
 import RxCocoa
 import EmptyDataSet_Swift
 
-class MyTableViewController: UICollectionViewController {
-    let observable = Variable<[StringWrapper]>(["Hello", "World", "Bye", "Foo", "Bar"])
+class MyTableViewController: UITableViewController {
+    let observable = Variable<[String]>(["Hello", "World", "Bye", "Foo", "Bar"])
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
-        collectionView.dataSource = nil
-        collectionView.register(UINib(nibName: "MyCell", bundle: nil), forCellWithReuseIdentifier: "cell")
-        let dataSource = RxCollectionViewSectionedAnimatedDataSource<StringSection>(configureCell:  {
-            (dataSource, collectionView, indexPath, string) -> UICollectionViewCell in
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCell
-            cell.textLabel.text = string.string
+        tableView.dataSource = nil
+        tableView.register(UINib(nibName: "MyCell", bundle: nil), forCellReuseIdentifier: "cell")
+        let dataSource = RxTableViewSectionedAnimatedDataSource<StringSection>(configureCell:  {
+            (dataSource, tableView, indexPath, string) -> UITableViewCell in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyCell
+            cell.label.text = string
             return cell
         })
+        tableView.separatorStyle = .none
         
-        collectionView.emptyDataSetView { (view) in
+        tableView.emptyDataSetView { (view) in
             view.titleLabelString(NSAttributedString(string: "Nothing to see here!"))
             view.shouldDisplay(true)
         }
         
         observable.asObservable()
             .map {
-                [StringSection(items: $0.map(StringWrapperStruct.init))]
+                [StringSection(items: $0)]
             }
-            .bind(to: self.collectionView.rx.items(dataSource: dataSource))
+            .bind(to: self.tableView.rx.items(dataSource: dataSource))
             .disposed(by: disposeBag)
-        DispatchQueue.main.async {
-            self.collectionView.selectItem(at: IndexPath(item: 0, section: 0), animated: false, scrollPosition: .top)
-        }
     }
     
-    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        observable.value[0] = "Hello"
     }
 }
 
 struct StringSection : AnimatableSectionModelType {
-    var items: [StringWrapperStruct]
+    var items: [String]
     
-    init(original: StringSection, items: [StringWrapperStruct]) {
+    init(original: StringSection, items: [String]) {
         self = original
         self.items = items
     }
     
-    init(items: [StringWrapperStruct]) {
+    init(items: [String]) {
         self.items = items
     }
     
-    typealias Item = StringWrapperStruct
+    typealias Item = String
     
     typealias Identity = String
     
     var identity: String { return "" }
-}
-
-
-class StringWrapper : ExpressibleByStringLiteral {
-    typealias StringLiteralType = String
-    
-    var wrapped = ""
-    
-    init(_ string: String) {
-        wrapped = string
-    }
-    
-    required convenience init(stringLiteral value: String) {
-        self.init(value)
-    }
-}
-
-struct StringWrapperStruct : IdentifiableType, Equatable {
-    let string: String
-    init(wrapper: StringWrapper) {
-        string = wrapper.wrapped
-    }
-    
-    typealias Identity = String
-    var identity: String { return string }
 }
