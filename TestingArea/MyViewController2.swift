51f5c6e3e0fe35305cd5b07bf986b32c25bda10c
Sweeper777 <sumulang@gmail.com>
2019-07-13 09:35:15 +0100

    added a bunch of IBActions

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -10,6 +10,7 @@ class MyViewController2: UIViewController {
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
     
+    @IBOutlet var shadowView: UIView!
 
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
@@ -23,9 +24,23 @@ class MyViewController2: UIViewController {
        
     }
     
+    @IBAction func offsetX(_ sender: UISlider) {
+        shadowView.layer.shadowOffset = CGSize(width: CGFloat(sender.value), height: shadowView.layer.shadowOffset.height)
     }
     
+    @IBAction func offsetY(_ sender: UISlider) {
+        shadowView.layer.shadowOffset = CGSize(width: shadowView.layer.shadowOffset.width, height: CGFloat(sender.value))
     }
+    
+    @IBAction func opacity(_ sender: UISlider) {
+        shadowView.layer.shadowOpacity = sender.value
     }
+    
+    @IBAction func radius(_ sender: UISlider) {
+        shadowView.layer.shadowRadius = CGFloat(sender.value)
+    }
+    
+    @IBAction func useBoundsAsPath(_ sender: UISwitch) {
+        shadowView.layer.shadowPath = sender.isOn ? UIBezierPath(rect: shadowView.bounds).cgPath : nil
     }
 }
