c0e84cf9f9ff4e620440fad8790ba78192d2b7e3
Sweeper777 <sumulang@gmail.com>
2019-07-16 09:27:05 +0100

    added completion handler for NSAlert

--- TestingAreaForMac/TestingAreaForMac/ViewController.swift
+++ TestingAreaForMac/TestingAreaForMac/ViewController.swift
@@ -11,6 +11,23 @@ class ViewController: NSViewController {
         test3()
     }
     
+    func completionHandler(_ response: NSApplication.ModalResponse) {
+        switch response {
+        case .abort:
+            print("abort")
+        case .alertFirstButtonReturn:
+            print("alert first button return")
+        case .alertSecondButtonReturn:
+            print("alert second button return")
+        case .alertThirdButtonReturn:
+            print("alert third button return")
+        case .stop:
+            print("stop")
+        case .continue:
+            print("continue")
+        default:
+            print("something else")
+        }
     }
 }
 
