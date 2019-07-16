f745320da13d066c55adda187f15511d0c747aa6
Sweeper777 <sumulang@gmail.com>
2019-07-16 09:26:48 +0100

    added IBAction for button

--- TestingAreaForMac/TestingAreaForMac/ViewController.swift
+++ TestingAreaForMac/TestingAreaForMac/ViewController.swift
@@ -1,28 +1,16 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
-    @IBOutlet var tableView: NSTableView!
-    
-    let dataSource = ["Hello", "World", "Foo", "Bar"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.dataSource = self
-        tableView.delegate = self
-    }
-}
-
-extension ViewController : NSTableViewDelegate, NSTableViewDataSource {
-    func numberOfRows(in tableView: NSTableView) -> Int {
-        return dataSource.count
     }
     
-    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "cell"), owner: nil) as? NSTableCellView
-        cell?.textField?.stringValue = dataSource[row]
-        return cell
+    @IBAction func buttonPressed(sender: NSButton) {
+        test3()
+    }
+    
     }
 }
 
