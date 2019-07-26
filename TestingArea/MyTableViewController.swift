63d658fa087c18ea1ba445602156328c806829b9
Sweeper777 <sumulang@gmail.com>
2019-07-26 09:37:37 +0100

    deleted old tests

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -2,56 +2,7 @@ import UIKit
 import RxSwift
 import RxDataSources
 import RxCocoa
-import EmptyDataSet_Swift
 
 class MyTableViewController: UITableViewController {
-    let observable = Variable<[String]>(["Hello", "World", "Bye", "Foo", "Bar"])
-    let disposeBag = DisposeBag()
-    
-    override func viewDidLoad() {
-        tableView.dataSource = nil
-        tableView.register(UINib(nibName: "MyCell", bundle: nil), forCellReuseIdentifier: "cell")
-        let dataSource = RxTableViewSectionedAnimatedDataSource<StringSection>(configureCell:  {
-            (dataSource, tableView, indexPath, string) -> UITableViewCell in
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyCell
-            cell.label.text = string
-            return cell
-        })
-        tableView.separatorStyle = .none
-        
-        tableView.emptyDataSetView { (view) in
-            view.titleLabelString(NSAttributedString(string: "Nothing to see here!"))
-            view.shouldDisplay(true)
-        }
-        
-        observable.asObservable()
-            .map {
-                [StringSection(items: $0)]
-            }
-            .bind(to: self.tableView.rx.items(dataSource: dataSource))
-            .disposed(by: disposeBag)
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 61
-    }
-}
-
-struct StringSection : AnimatableSectionModelType {
-    var items: [String]
-    
-    init(original: StringSection, items: [String]) {
-        self = original
-        self.items = items
-    }
-    
-    init(items: [String]) {
-        self.items = items
-    }
-    
-    typealias Item = String
-    
-    typealias Identity = String
-    
-    var identity: String { return "" }
+ 
 }
--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -9,8 +9,6 @@ class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
-    
-    @IBOutlet var shadowView: UIView!
 
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
@@ -18,34 +16,8 @@ class MyViewController2: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        shadowView.layer.shadowColor = UIColor.black.cgColor
-        shadowView.layer.shadowRadius = 0
-        shadowView.layer.opacity = 0.5
-        shadowView.layer.shadowOffset = CGSize(width: 0, height: 0)
     }
     
     @IBAction func click() {
-       
-    }
-    
-    @IBAction func offsetX(_ sender: UISlider) {
-        shadowView.layer.shadowOffset = CGSize(width: CGFloat(sender.value), height: shadowView.layer.shadowOffset.height)
-    }
-    
-    @IBAction func offsetY(_ sender: UISlider) {
-        shadowView.layer.shadowOffset = CGSize(width: shadowView.layer.shadowOffset.width, height: CGFloat(sender.value))
-    }
-    
-    @IBAction func opacity(_ sender: UISlider) {
-        shadowView.layer.shadowOpacity = sender.value
-    }
-    
-    @IBAction func radius(_ sender: UISlider) {
-        shadowView.layer.shadowRadius = CGFloat(sender.value)
-    }
-    
-    @IBAction func useBoundsAsPath(_ sender: UISwitch) {
-        shadowView.layer.shadowPath = sender.isOn ? UIBezierPath(rect: shadowView.bounds).cgPath : nil
     }
 }
