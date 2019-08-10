182b6452396fe23c450afdc0b3b0ba9d1a5256dd
Sweeper777 <sumulang@gmail.com>
2019-08-10 10:24:18 +0800

    tested NSWindow.setFrame with preferredContentSize

--- TestingAreaForMac/TestingAreaForMac/SheetViewController.swift
+++ TestingAreaForMac/TestingAreaForMac/SheetViewController.swift
@@ -1,7 +1,15 @@
 import Cocoa
 
 class SheetViewController : NSViewController {
+    override func viewDidLoad() {
+        preferredContentSize = CGSize(width: 100, height: 100)
+    }
+    
     @IBAction func buttonPressed(_ sender: NSButton) {
-        view.window?.setFrame(CGRect(x: 0, y: 0, width: 300, height: 300), display: true, animate: true)
+        preferredContentSize = .zero
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { [weak self] in
+            self?.view.window?.setFrame(CGRect(x: 0, y: 0, width: 300, height: 300), display: true, animate: true)
+        }
+        
     }
 }
