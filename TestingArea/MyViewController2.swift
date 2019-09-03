2114a5a536e561b163ef87dd68cb722ac0da11cc
Mulang Su <sumulang@gmail.com>
2019-09-03 08:41:37 +0100

    synthesized some speech

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -18,12 +18,12 @@ class MyViewController2: UIViewController {
     }
     
     @IBAction func click() {
-        // Instantiate a new music player
-        let myMediaPlayer = MPMusicPlayerApplicationController.applicationQueuePlayer
-        // Add a playback queue containing all songs on the device
-        myMediaPlayer.setQueue(with: MPMediaQuery.songs())
-        // Start playing from the beginning of the queue
-        myMediaPlayer.play()
+        let utterance = AVSpeechUtterance(string: "Hello World, my name is Sweeper")
+        utterance.rate = AVSpeechUtteranceMinimumSpeechRate
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        synthesiser.delegate = self
+        synthesiser.speak(utterance)
+    }
 }
 
 extension MyViewController2 : AVSpeechSynthesizerDelegate {
