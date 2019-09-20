89b84b6b2256134b0fbf4c23506011975ab2948d
Mulang Su <sumulang@gmail.com>
2019-09-20 21:06:53 +0100

    deleted MyCell

--- TestingArea/MyView.swift
+++ TestingArea/MyView.swift
@@ -1,32 +1,5 @@
 import UIKit
 
-class MyCell: UITableViewCell {
-    @IBOutlet var label: UILabel!
-    @IBOutlet var container: UIView!
-    
-    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
-        if highlighted {
-            container.backgroundColor = .gray
-        } else {
-            container.backgroundColor = .white
         }
-    }
-    
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        if selected {
-            container.backgroundColor = .gray
-        } else {
-            container.backgroundColor = .white
-        }
-    }
-    
-    override func awakeFromNib() {
-        container.layer.shadowColor = UIColor.black.cgColor
-        container.layer.shadowOpacity = 0.7
-        container.layer.shadowOffset = CGSize(width: 3, height: 9)
-        container.layer.shadowRadius = 4
-        container.layer.cornerRadius = 4
-        container.layer.shadowPath = UIBezierPath(roundedRect: container.bounds, cornerRadius: 4).cgPath
-        selectionStyle = .none
     }
 }
