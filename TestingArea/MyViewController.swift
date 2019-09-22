7174e9cebee6000b208b88fd488bcf28e3454511
Mulang Su <sumulang@gmail.com>
2019-09-22 09:23:37 +0100

    added constraints for blur VC views

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -6,5 +6,15 @@ class MyViewController: UIViewController{
     @IBOutlet var text: UITextView!
     
     override func viewDidLoad() {
+        blur.snp.makeConstraints { (make) in
+            make.width.equalToSuperview()
+            make.height.equalToSuperview()
+        }
+        
+        text.snp.makeConstraints { (make) in
+            make.width.equalTo(240)
+            make.height.equalTo(128)
+            make.center.equalToSuperview()
+        }
     }
 }
