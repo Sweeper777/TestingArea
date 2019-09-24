0c3335a0c9ca34bce4334c3658b8a79db6386c95
Mulang Su <sumulang@gmail.com>
2019-09-24 07:16:59 +0100

    tested added 3 icon segments

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -20,6 +20,27 @@ class MyViewController: UIViewController{
         betterSC.cornerRadius = 20
             make.center.equalToSuperview()
         }
+    
+    func test1() {
+        betterSC.segments = [
+            IconSegment(
+                icon: UIImage(named: "home")!,
+                iconSize: CGSize(width: 25, height: 25),
+                normalIconTintColor: UIColor.blue.darker(),
+                selectedIconTintColor: .white),
+            IconSegment(
+                icon: UIImage(named: "settings")!,
+                iconSize: CGSize(width: 25, height: 25),
+                normalIconTintColor: UIColor.blue.darker(),
+                selectedIconTintColor: .white),
+            IconSegment(
+                icon: UIImage(named: "icons8-repeat_filled")!,
+                iconSize: CGSize(width: 25, height: 25),
+                normalIconTintColor: UIColor.blue.darker(),
+                selectedIconTintColor: .white),
+        ]
+    }
+    
     }
     
     override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
