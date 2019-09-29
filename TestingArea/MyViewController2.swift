30fadcdb9ebf135b1b3dfd0b8c0e9e236a78ce99
Mulang Su <sumulang@gmail.com>
2019-09-29 10:12:47 +0100

    deleted previous tests

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,5 +1,4 @@
 import UIKit
-import MediaPlayer
 import RxSwift
 
 @available(iOS 10.0, *)
@@ -7,9 +6,7 @@ class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
-    @IBOutlet var label: UILabel!
     
-    let synthesiser = AVSpeechSynthesizer()
     let disposeBag = DisposeBag()
 
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
@@ -18,25 +15,12 @@ class MyViewController2: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        synthesiser.delegate = self
-        textview.attributedText = NSAttributedString(string: textview.text, attributes: [.font: UIFont.systemFont(ofSize: 19)])
     }
     
     @IBAction func click() {
-//        Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
-//            .take(30).map { String($0) }
-//            .bind(to: label.rx.text)
-//            .disposed(by: disposeBag)
     }
     
 }
 
-extension MyViewController2 : AVSpeechSynthesizerDelegate {
-    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {
-        let attributedString = NSMutableAttributedString(attributedString: textview.attributedText)
-        attributedString.removeAttribute(NSAttributedString.Key.backgroundColor, range: NSRange(location: 0, length: attributedString.length))
-        attributedString.addAttribute(NSAttributedString.Key.backgroundColor, value: UIColor.yellow, range: characterRange)
-        textview.attributedText = attributedString
-        textview.scrollRangeToVisible(characterRange)
     }
 }
