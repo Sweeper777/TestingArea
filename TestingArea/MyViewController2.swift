30449b1af20c13edc7b79419f19814e73a8a6ec7
Mulang Su <sumulang@gmail.com>
2019-10-03 07:24:57 +0100

    deleted all the manual captrue session stuff

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -11,12 +11,6 @@ class MyViewController2: UIViewController {
     @IBOutlet var cameraView: UIView!
     
     let disposeBag = DisposeBag()
-    
-    var videoDataOutput: AVCaptureVideoDataOutput!
-    var videoDataOutputQueue: DispatchQueue!
-    var previewLayer:AVCaptureVideoPreviewLayer!
-    var captureDevice : AVCaptureDevice!
-    let session = AVCaptureSession()
 
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
@@ -24,79 +18,13 @@ class MyViewController2: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupAVCapture()
-    }
-    
-    @IBAction func click() {
-        if session.isRunning {
-            session.stopRunning()
-        } else {
-            session.startRunning()
         }
     }
     
-}
-
-extension MyViewController2:  AVCaptureVideoDataOutputSampleBufferDelegate{
-    func setupAVCapture(){
-        session.sessionPreset = AVCaptureSession.Preset.photo
-        guard let device = AVCaptureDevice
-            .default(AVCaptureDevice.DeviceType.builtInWideAngleCamera,
-                     for: .video,
-                     position: AVCaptureDevice.Position.back) else {
-                        return
-        }
-        captureDevice = device
-        beginSession()
     }
     
-    func beginSession(){
-        var deviceInput: AVCaptureDeviceInput!
-        
-        do {
-            deviceInput = try AVCaptureDeviceInput(device: captureDevice)
-            guard deviceInput != nil else {
-                print("error: cant get deviceInput")
-                return
-            }
-            
-            if self.session.canAddInput(deviceInput){
-                self.session.addInput(deviceInput)
-            }
-            
-            videoDataOutput = AVCaptureVideoDataOutput()
-            videoDataOutput.alwaysDiscardsLateVideoFrames=true
-            videoDataOutputQueue = DispatchQueue(label: "VideoDataOutputQueue")
-            videoDataOutput.setSampleBufferDelegate(self, queue:self.videoDataOutputQueue)
-            
-            if session.canAddOutput(self.videoDataOutput){
-                session.addOutput(self.videoDataOutput)
-            }
-            
-            videoDataOutput.connection(with: .video)?.isEnabled = true
-            
-            previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
-            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-            
-            let rootLayer :CALayer = self.cameraView.layer
-            rootLayer.masksToBounds=true
-            previewLayer.frame = rootLayer.bounds
-            rootLayer.addSublayer(self.previewLayer)
-            session.startRunning()
-        } catch let error as NSError {
-            deviceInput = nil
-            print("error: \(error.localizedDescription)")
-        }
     }
     
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        // do stuff here
-    }
-    
-    // clean up AVCapture
-    func stopCamera(){
-        session.stopRunning()
     }
     
 }
