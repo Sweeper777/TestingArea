ab748214dc6f3812ded9e294e90841a259ea7a7a
Mulang Su <sumulang@gmail.com>
2019-10-05 09:48:39 +0100

    deleted watch app

--- Watch Extension/MyRowController.swift
+++
@@ -1,7 +0,0 @@
-import WatchKit
-
-class MyRowController: NSObject {
-    @IBOutlet var btn1: WKInterfaceButton!
-    @IBOutlet var btn2: WKInterfaceButton!
-    @IBOutlet var btn3: WKInterfaceButton!
-}
