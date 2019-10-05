753f01d08216ebf578c56032dc9817e165bcb03f
Mulang Su <sumulang@gmail.com>
2019-10-05 09:54:45 +0100

    Create TodayViewController.swift

---
+++ MyTodayExtension/TodayViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+import NotificationCenter
+import RealmSwift
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
