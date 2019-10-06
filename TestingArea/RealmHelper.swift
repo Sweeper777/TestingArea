b7e6f270cb5417404fe2d24d00b2490ec0f92e8d
Mulang Su <sumulang@gmail.com>
2019-10-06 09:52:14 +0100

    added RealmWrapper

--- TestingArea/RealmHelper.swift
+++ TestingArea/RealmHelper.swift
@@ -1,3 +1,27 @@
+import RealmSwift
+
+
+final class RealmWrapper {
+    let entities: Results<Entity>!
+    let realm: Realm!
+    
+    private init() {
+        do {
+            realm = try Realm()
+            entities = realm.objects(Entity.self)
+        } catch let error {
+            print(error)
+            fatalError()
+        }
+    }
+    
+    private static var _shared: RealmWrapper?
+    
+    static var shared: RealmWrapper {
+        _shared = _shared ?? RealmWrapper()
+        return _shared!
+    }
+}
 
 class Entity: Object {
     @objc dynamic var number = 0
