ce3663d5ce4631ce7857c1d3bb8740d092a2617e
Mulang Su <sumulang@gmail.com>
2019-10-06 09:54:04 +0100

    tried to get realm data from today VC

--- MyTodayExtension/TodayViewController.swift
+++ MyTodayExtension/TodayViewController.swift
@@ -4,7 +4,15 @@ import RealmSwift
 
 class TodayViewController: UIViewController, NCWidgetProviding {
     
+    @IBOutlet var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.text = "Max number is: Fetching..."
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let sorted = RealmWrapper.shared.realm.objects(Entity.self).sorted(byKeyPath: "number", ascending: false)
+        label.text = "Max number is: \(sorted.first?.number.description ?? "None")"
     }
 }
