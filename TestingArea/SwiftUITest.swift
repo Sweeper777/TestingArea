88da662108cf7ff92f24b299d42bd0008fed8aaa
Mulang Su <sumulang@gmail.com>
2019-10-12 10:09:13 +0100

    Create SwiftUITest.swift

---
+++ TestingArea/SwiftUITest.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct SwiftUITest: View {
+    var body: some View {
+    }
+}
+
+struct SwiftUITest_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUITest()
+    }
+}
+
+
