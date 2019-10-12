d189c8a3b271b555ef19264df0558e1e193f9805
Mulang Su <sumulang@gmail.com>
2019-10-12 10:09:44 +0100

    added simple UI

--- TestingArea/SwiftUITest.swift
+++ TestingArea/SwiftUITest.swift
@@ -1,7 +1,12 @@
 import SwiftUI
 
 struct SwiftUITest: View {
+    @State private var count = 0
+    
     var body: some View {
+        VStack(alignment: .center, spacing: 70) {
+            Text(verbatim: "\(count)")
+        }
     }
 }
 
