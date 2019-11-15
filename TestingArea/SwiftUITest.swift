62a639761f7afcfb3b4ed53d64097de4a21cea8e
Mulang Su <sumulang@gmail.com>
2019-11-15 08:44:57 +0000

    tested an SO question

--- TestingArea/SwiftUITest.swift
+++ TestingArea/SwiftUITest.swift
@@ -4,9 +4,28 @@ import Combine
 
 struct SwiftUITest: View {
     
+    @State var interval = 1
+
     var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Picker.init("Every", selection: $interval) {
+                        ForEach(1 ..< 1000) { weeks in
+                            Text("\(weeks) week\(weeks == 1 ? "" : "s")")
+                                .tag(weeks)
+                        }
+                        }.pickerStyle(WheelPickerStyle())
+                }
+            }
+        }
+        .onTapGesture {
+            self.updateRruleString()
+        }
     }
 
+    public func updateRruleString() {
+        
     }
 }
 struct SwiftUITest_Previews: PreviewProvider {
@@ -19,6 +38,10 @@ struct SwiftUITest_Previews: PreviewProvider {
 class MyHostingController: UIHostingController<SwiftUITest> {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder, rootView: SwiftUITest())
+        
+    }
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         
     }
 }
