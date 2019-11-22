8eadd8b3eb9c887d5ea2ddb8f9181128a5b52cc8
Mulang Su <sumulang@gmail.com>
2019-11-22 16:28:05 +0000

    tested custom drawing in macOS

--- TestingAreaForMac/TestingAreaForMac/MyView.swift
+++ TestingAreaForMac/TestingAreaForMac/MyView.swift
@@ -2,4 +2,17 @@ import AppKit
 
 class MyView : NSView {
     
+    override func draw(_ dirtyRect: NSRect) {
+        let path1 = NSBezierPath(rect: CGRect(x: 0, y: 0, width: bounds.midX, height: bounds.midY))
+        NSColor.black.setStroke()
+        NSColor.red.setFill()
+        path1.lineWidth = 3
+        path1.stroke()
+        path1.fill()
+        let path2 = NSBezierPath(rect: CGRect(x: bounds.midX, y: bounds.midY, width: bounds.midX, height: bounds.midY))
+        NSColor.green.setFill()
+        path2.lineWidth = 3
+        path2.stroke()
+        path2.fill()
+    }
 }
