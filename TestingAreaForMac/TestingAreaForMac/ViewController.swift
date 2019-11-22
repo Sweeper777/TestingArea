700cc4d7c91582293f460e0abfc617c9485390cb
Mulang Su <sumulang@gmail.com>
2019-11-22 16:28:57 +0000

    tried to flip but layer is nil

--- TestingAreaForMac/TestingAreaForMac/ViewController.swift
+++ TestingAreaForMac/TestingAreaForMac/ViewController.swift
@@ -11,6 +11,13 @@ class ViewController: NSViewController {
         
     }
     
+    override func viewDidAppear() {
+//        myView.layer?.transform = CATransform3D(m11: 1, m12: 0, m13: 0, m14: 0,
+//                                                m21: 0, m22: -2, m23: 0, m24: 0,
+//                                                m31: 0, m32: 0, m33: 1, m34: 0,
+//                                                m41: 0, m42: 0, m43: 0, m44: 1)
+    }
+    
     @IBAction func buttonPressed(sender: NSButton) {
         
     }
