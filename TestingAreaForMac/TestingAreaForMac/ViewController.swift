72d440eb2936d1a9382f914b1875480d06f7a6e3
Mulang Su <sumulang@gmail.com>
2019-11-22 16:28:40 +0000

    added myView to VC's view

--- TestingAreaForMac/TestingAreaForMac/ViewController.swift
+++ TestingAreaForMac/TestingAreaForMac/ViewController.swift
@@ -2,8 +2,12 @@ import Cocoa
 
 class ViewController: NSViewController {
     
+    var myView: MyView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myView = MyView(frame: CGRect(x: 30, y: 30, width: 100, height: 100))
+        view.addSubview(myView)
         
     }
     
