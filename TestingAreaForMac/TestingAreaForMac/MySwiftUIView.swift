60bec836c07e54100d02ad8c0ed54c69f42060b6
Mulang Su <sumulang@gmail.com>
2019-11-23 10:06:45 +0000

    tried out SwiftUI on macOS
    but I don't have macOS 15...

---
+++ TestingAreaForMac/TestingAreaForMac/MySwiftUIView.swift
@@ -0,0 +1,23 @@
+//
+//  MySwiftUIView.swift
+//  TestingAreaForMac
+//
+//  Created by Mulang Su on 23/11/2019.
+//  Copyright © 2019 Mulang Su. All rights reserved.
+//
+
+import SwiftUI
+
+@available(OSX 10.15.0, *)
+struct MySwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+@available(OSX 10.15, *)
+struct MySwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        MySwiftUIView()
+    }
+}
