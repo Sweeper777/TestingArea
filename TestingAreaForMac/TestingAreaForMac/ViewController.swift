28cd0c56a4e268113b93180ca4cd9664cdd395d4
Mulang Su <sumulang@gmail.com>
2020-01-07 18:27:43 +0000

    decoded CSV in downloads folder

--- TestingAreaForMac/TestingAreaForMac/ViewController.swift
+++ TestingAreaForMac/TestingAreaForMac/ViewController.swift
@@ -1,4 +1,5 @@
 import Cocoa
+import CSV
 
 class ViewController: NSViewController {
     
@@ -6,12 +7,22 @@ class ViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
     
     @IBAction func buttonPressed(sender: NSButton) {
-        textfield.stringValue = "Changed!"
+        var records = [Vessel]()
+        do {
+            let csvString = try String(contentsOfFile: "/Users/mulangsu/Downloads/csv.csv")
+            let reader = try CSVReader(string: csvString, hasHeaderRow: true)
+            let decoder = CSVRowDecoder()
+            while reader.next() != nil {
+                let row = try decoder.decode(Vessel.self, from: reader)
+                records.append(row)
+            }
+        } catch let error {
+            print(error)
+        }
+        print("Read \(records.count) Records")
     }
     
     @IBAction func item2Pressed(_ sender: NSMenuItem) {
