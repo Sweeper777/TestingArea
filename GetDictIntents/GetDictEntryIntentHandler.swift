dcdd6c76d8fe3dd9ac937f263eb2a5b9954699cf
Mulang Su <sumulang@gmail.com>
2020-01-08 07:50:29 +0000

    implemented a bunch of protocol methods

--- GetDictIntents/GetDictEntryIntentHandler.swift
+++ GetDictIntents/GetDictEntryIntentHandler.swift
@@ -9,4 +9,19 @@ class GetDictEntryIntentHandler: NSObject, GetDictEntryIntentHandling {
         "4": "value1",
     ]
     
+    func resolveKey(for intent: GetDictEntryIntent, with completion: @escaping (INStringResolutionResult) -> Void) {
+        completion(INStringResolutionResult.success(with: intent.key!))
+    }
+    
+    func confirm(intent: GetDictEntryIntent, completion: @escaping (GetDictEntryIntentResponse) -> Void) {
+        completion(GetDictEntryIntentResponse(code: GetDictEntryIntentResponseCode.ready, userActivity: nil))
+    }
+    
+    func handle(intent: GetDictEntryIntent, completion: @escaping (GetDictEntryIntentResponse) -> Void) {
+        guard let key = intent.key, let value = dict[key] else {
+            completion(.failure(key: intent.key ?? "nil"))
+            return
+        }
+        completion(.success(key: key, value: value))
+    }
 }
