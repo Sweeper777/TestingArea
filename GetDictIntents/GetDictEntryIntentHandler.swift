ee92d8be079cc516238036dfecf6d6d346379707
Mulang Su <sumulang@gmail.com>
2020-01-08 07:50:07 +0000

    Create GetDictEntryIntentHandler.swift

---
+++ GetDictIntents/GetDictEntryIntentHandler.swift
@@ -0,0 +1,12 @@
+import UIKit
+import Intents
+
+class GetDictEntryIntentHandler: NSObject, GetDictEntryIntentHandling {
+    let dict = [
+        "1": "value1",
+        "2": "value1",
+        "3": "value1",
+        "4": "value1",
+    ]
+    
+}
