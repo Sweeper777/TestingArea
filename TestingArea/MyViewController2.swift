c4e96fa6ac6620e9f0a148f3c531d6c8e33d42c6
Mulang Su <sumulang@gmail.com>
2020-01-18 09:45:26 +0000

    added getFib1237s

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -28,7 +28,30 @@ class MyViewController2: UIViewController {
     }
     
     @IBAction func click() {
-        
+        getFib1237s()
+        print("===========================")
+        alt()
+    }
+    
+    func getFib1237s() {
+        // Some temporary variables.
+        var a = BInt(0)
+        var b = BInt(1)
+        var m = BInt(1)
+        // Get the numbers until crash...
+        while m <= 23 {
+            let temp = a
+            a = b
+            b = b + temp
+//            print("a: ", a)
+            let str = a.asString(radix: 10)
+            let reduced = reduce(str)
+            if reduced == "1237"  {
+                print(m, "Fib 1237 number is ", a, " | Digits: ", str.count)
+                m+=1
+            }
+
+        }
     }
     
     func alt() {
