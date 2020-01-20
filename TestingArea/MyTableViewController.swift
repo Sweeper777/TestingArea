d858a43ee06092cdc837810fed0bb376b2bfd176
Mulang Su <sumulang@gmail.com>
2020-01-20 16:40:54 +0000

    added dummy data source

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -1,7 +1,13 @@
 import UIKit
 import SwiftyUtils
+import SwiftyJSON
 
 class MyTableViewController: UITableViewController {
+    var strings = [("Some Very Long Black Text That Doesn't Fit. Foo Bar Baz.", ""),
+                   ("A", "Short Text"),
+                   ("B", "Foo Bar"),
+                   ("Some Very Long Grey Text That Doesn't Fit. Foo Bar Baz", "Some Very Long Grey Text That Doesn't Fit. Foo Bar Baz"),
+                   ("C", "Another Text")]
     
     override func viewDidLoad() {
         
