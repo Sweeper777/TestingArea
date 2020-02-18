dfa6bf40d3a36f584f0bc8a6396aed226aa569ea
Mulang Su <sumulang@gmail.com>
2020-02-18 09:59:31 +0000

    added Node struct

--- TestingAreaForMac/TestingAreaForMac/MyModel.swift
+++ TestingAreaForMac/TestingAreaForMac/MyModel.swift
@@ -1,23 +1,7 @@
 import Foundation
 
-struct Vessel : Decodable {
-    let name: String
-    let yard: String
-    let yearBuilt: Int
-    
-    var age: Int {
-        Calendar.current.dateComponents([.year], from: Date()).year! - yearBuilt
-    }
-    
-    let shipClass: String
-    let grossTonnage: Int
-    let deadWeightTonnage: Int
-    let flag: String
-    let delivery: String
-    let entry: String
-    let PNI: String
-    let ETC: Double
-    let insuredValue: Double
-    let rates: Double?
-    let registeredOwner: String
+struct Node {
+    let title: String
+    let detail: String
+    let children: [Node]
 }
