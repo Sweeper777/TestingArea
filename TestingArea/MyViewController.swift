02a8bb0a977ab13acf87c9f3747fa82f698dc8e1
Mulang Su <sumulang@gmail.com>
2020-03-01 10:31:40 +0000

    tested another SO question

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -3,6 +3,17 @@ import UIKit
 class MyViewController: UIViewController{
 
     override func viewDidLoad() {
-        
+        let myText = UITextView()
+        myText.text = "Hello World!"
+        view.addSubview(myText)
+        myText.translatesAutoresizingMaskIntoConstraints = false
+        myText.textAlignment = .center
+        myText.backgroundColor = .gray
+        NSLayoutConstraint.activate([
+          myText.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+          myText.topAnchor.constraint(equalTo: view.topAnchor),
+          myText.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+          myText.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
     }
 }
