9250d80ab19070805f08098f907de35ec64776f0
Mulang Su <sumulang@gmail.com>
2020-03-13 09:03:45 +0000

    deleted some previous stuff in VC

--- TestingAreaForMac/TestingAreaForMac/ViewController.swift
+++ TestingAreaForMac/TestingAreaForMac/ViewController.swift
@@ -2,48 +2,15 @@ import Cocoa
 
 class ViewController: NSViewController {
     
-    @IBOutlet var outlineView: NSOutlineView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-}
-
-extension ViewController: NSOutlineViewDataSource, NSOutlineViewDelegate {
-    func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
-        if let node = item as? Node {
-            return node.children.count
-        }
-        return testTree.children.count
     }
     
-    func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
-        if let node = item as? Node {
-            return node.children[index]
-        }
-        return testTree.children[index]
     }
     
-    func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
-        if let node = item as? Node {
-            return node.children.count > 0
-        }
-        return false
     }
     
-    func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
-        let node = item as! Node
-        if tableColumn?.identifier.rawValue == "titleColumn" {
-            let view = outlineView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "titleCell"), owner: nil) as! NSTableCellView
-            view.textField?.stringValue = node.title
-            return view
-        } else if tableColumn?.identifier.rawValue == "descriptionColumn" {
-            let view = outlineView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "descriptionCell"), owner: nil) as! NSTableCellView
-            view.textField?.stringValue = node.detail
-            return view
-        } else {
-            return nil
-        }
-    }
 }
