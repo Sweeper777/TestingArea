4160572fd20584ceea896dab3caa21c102e18cb6
Mulang Su <sumulang@gmail.com>
2020-03-13 09:04:32 +0000

    displayed map of China

--- TestingAreaForMac/TestingAreaForMac/ViewController.swift
+++ TestingAreaForMac/TestingAreaForMac/ViewController.swift
@@ -1,14 +1,39 @@
 import Cocoa
+import SwiftyXMLParser
 
 class ViewController: NSViewController {
     
+    @IBOutlet var slider: NSSlider!
+    @IBOutlet var myView: MyView!
+    
+    var svgStrings: [String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        
+        let xmlString = try! String(contentsOfFile: Bundle.main.path(forResource: "map", ofType: "svg")!)
+        let xml = try! XML.parse(xmlString)
+        svgStrings = Array(xml["svg", "g" ,"path"])
+            .map { $0.attributes["d"]! }
+            .map(editSVGString)
+        myView.svgStrings = svgStrings
+        slider.minValue = 0
+        slider.maxValue = Double(svgStrings.count)
+        slider.integerValue = svgStrings.count
+        slider.isContinuous = true
     }
     
+    func editSVGString(_ string: String) -> String {
+        let uppercased = string.uppercased()
+//            if !uppercased.hasSuffix("Z") {
+//                return uppercased + "Z"
+//            } else {
+            return uppercased
+//            }
     }
     
     }
