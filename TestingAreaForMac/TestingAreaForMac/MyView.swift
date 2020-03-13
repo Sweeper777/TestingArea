e4897de91342918e34c42cf976137c256edacbca
Mulang Su <sumulang@gmail.com>
2020-03-13 09:05:08 +0000

    scaled the SVGs to as large as possible

--- TestingAreaForMac/TestingAreaForMac/MyView.swift
+++ TestingAreaForMac/TestingAreaForMac/MyView.swift
@@ -11,6 +11,22 @@ class MyView : NSView {
     override func draw(_ dirtyRect: NSRect) {
         guard let svg = svgStrings else { return }
         
+        if svg.count == 1 {
+            guard let path = NSBezierPath(svgString: svg.first!) else {
+                print("cannot create path")
+                return
+            }
+            let bounds = path.bounds
+            let scaleFactor = min(self.bounds.width / bounds.width, self.bounds.height / bounds.height)
+            let scale = AffineTransform(scale: scaleFactor)
+            let translate = AffineTransform(translationByX: -bounds.origin.x, byY: -bounds.origin.y)
+            path.transform(using: translate)
+            path.transform(using: scale)
+            path.lineWidth = 1
+            NSColor.textColor.setStroke()
+            path.stroke()
+            return
+        }
         
         for string in svg {
             guard let path = NSBezierPath(svgString: string) else {
