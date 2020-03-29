213fbff8b43294287d1640bfdb6cccf02b741ef1
Mulang Su <sumulang@gmail.com>
2020-03-29 12:12:53 +0100

    tested an SO question

--- TestingArea/SwiftUITest.swift
+++ TestingArea/SwiftUITest.swift
@@ -1,19 +1,54 @@
 import SwiftUI
 
-struct Word: Identifiable, Codable {
- var id: String
+struct ContentView: View {
+    @State var number: Double = 0
+    
+    let formatter : NumberFormatter = {
+        let f = NumberFormatter()
+        f.maximumFractionDigits = 0
+        return f
+    }()
+    
+    var body: some View {
+        VStack {
+            Infobox {
+                Text("Hello, World!")
+                
+            }.border(Color.red)
+            Infobox(withHeader: "Title") {
+                Text("Hello, World!")
+                
+            }.border(Color.green)
+            
+        }
+    }
 }
 
-struct TestView: View {
- var body: some View {
-   Text("Test")
-  }
+struct Infobox<Content>: View where Content: View {
+
+    let content: () -> Content
+    let headerText: String
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            if !headerText.isEmpty {
+                Text(headerText)
+                    .font(.headline)
+            }
+            content()
+        }
+    }
+
+    // note the optional parameter here
+    init(withHeader headerText: String = "", @ViewBuilder _ content: @escaping () -> Content) {
+        self.content = content
+        self.headerText = headerText
+    }
 }
 
-
-class MyHostingController: UIHostingController<TestView> {
+class MyHostingController: UIHostingController<ContentView> {
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder, rootView: TestView())
+        super.init(coder: aDecoder, rootView: ContentView())
         
     }
 }
