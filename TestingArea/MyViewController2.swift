8c3fd21069b0b6f6e28a8f5a9a208ee579da6c61
Mulang Su <sumulang@gmail.com>
2020-04-06 09:30:07 +0100

    tested an SO question about TooltipView

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -5,7 +5,6 @@ class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
-    @IBOutlet var myView: UIView!
     
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
@@ -13,25 +12,12 @@ class MyViewController2: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let test = ToolTipView(frame: CGRect(x: 0 , y: self.view.center.y, width: self.view.frame.size.width, height: 50))
+        self.view.addSubview(test)
     }
     
-    override func viewDidLayoutSubviews() {
-        myView.rounded()
-    }
     
     @IBAction func click() {
         
     }
-}
-
-extension UIView {
-    func rounded(cornerRadius: CGFloat? = nil) {
-        if let cornerRadius = cornerRadius {
-            layer.cornerRadius = cornerRadius
-        } else {
-            layer.cornerRadius = min(frame.size.height, frame.size.width) / 2
-        }
-        clipsToBounds = true
-    }
-
 }
