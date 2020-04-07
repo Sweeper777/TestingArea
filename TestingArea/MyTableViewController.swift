23194a0ba4b1d8b592e01ac93104914acdb6f124
Mulang Su <sumulang@gmail.com>
2020-04-07 08:31:44 +0100

    made the green cell always square

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -3,6 +3,14 @@ import SwiftyUtils
 import SwiftyJSON
 
 class MyTableViewController: UITableViewController {
+    @IBOutlet var myView: UIView!
     
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 1 && indexPath.row == 0 {
+            return tableView.width
+        } else {
+            return UITableView.automaticDimension
+        }
+    }
 }
 
