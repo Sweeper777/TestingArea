265f72ff5e7a0eb4859a4ea648b46ad82a26a894
Mulang Su <sumulang@gmail.com>
2020-04-17 18:29:12 +0100

    tested my answer for an SO question

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,11 +1,15 @@
 import UIKit
 import DCTextEngine
+import SwiftyJSON
+import Combine
 
 @available(iOS 10.0, *)
 class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
+    
+    var cancellable : AnyCancellable!
     
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
@@ -21,6 +25,13 @@ class MyViewController2: UIViewController {
         fullString.append(NSAttributedString(string: " button"))
         textview.attributedText = fullString
 
+        cancellable = Timer.publish(every: 0.2, on: .main, in: .common)
+            .autoconnect()
+        .b
+            .buffer(Timer.publish(every: 2, on: .main, in: .common).autoconnect())
+            .sink() { array in
+                print ("timer fired: \(array.map { $0.timeIntervalSince1970 })")
+        }
     }
     
     
@@ -35,4 +46,66 @@ class MyViewController2: UIViewController {
     
     @available(*, unavailable)
     @objc func foo() {}
+}
+
+struct AnyObserver<Output, Failure: Error> {
+    let onNext: ((Output) -> Void)
+    let onError: ((Failure) -> Void)
+    let onCompleted: (() -> Void)
+}
+
+struct Disposable {
+    let dispose: () -> Void
+}
+
+extension AnyPublisher {
+    static func create(subscribe: @escaping (AnyObserver<Output, Failure>) -> Disposable) -> Self {
+        let subject = PassthroughSubject<Output, Failure>()
+        var disposable: Disposable?
+        return subject
+            .handleEvents(receiveSubscription: { subscription in
+                disposable = subscribe(AnyObserver(
+                    onNext: { output in subject.send(output) },
+                    onError: { failure in subject.send(completion: .failure(failure)) },
+                    onCompleted: { subject.send(completion: .finished) }
+                ))
+            }, receiveCancel: { disposable?.dispose() })
+            .eraseToAnyPublisher()
+    }
+}
+
+extension Publisher {
+
+    /// collects elements from the source sequence until the boundary sequence fires. Then it emits the elements as an array and begins collecting again.
+    func buffer<T: Publisher, U>(_ boundary: T) -> AnyPublisher<[Output], Failure> where T.Output == U {
+        return AnyPublisher.create { observer in
+            var buffer: [Output] = []
+            let lock = NSRecursiveLock()
+            let boundaryDisposable = boundary.sink(receiveCompletion: {
+                _ in
+            }, receiveValue: {_ in
+                lock.lock(); defer { lock.unlock() }
+                observer.onNext(buffer)
+                buffer = []
+            })
+            let disposable = self.sink(receiveCompletion: { (event) in
+                lock.lock(); defer { lock.unlock() }
+                switch event {
+                case .finished:
+                    observer.onNext(buffer)
+                    observer.onCompleted()
+                case .failure(let error):
+                    observer.onError(error)
+                    buffer = []
+                }
+            }) { (element) in
+                lock.lock(); defer { lock.unlock() }
+                buffer.append(element)
+            }
+            return Disposable {
+                disposable.cancel()
+                boundaryDisposable.cancel()
+            }
+        }
+    }
 }
