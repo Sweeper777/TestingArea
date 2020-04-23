9094d59229f835f942076f1455bdf6c718a638b6
Mulang Su <sumulang@gmail.com>
2020-04-23 17:47:47 +0100

    tested an SO question about SwiftUI sheets

--- TestingArea/SwiftUITest.swift
+++ TestingArea/SwiftUITest.swift
@@ -4,35 +4,27 @@ struct SomeItem: Identifiable { // identifiable item
     var id: Int // identity
 }
 
-struct ContentView: View {
-
-    @State private var selectedItem: SomeItem? = nil
-
+struct ContentView : View {
+    @State var text: String = "Hello"
+    @State var isEditing: Bool = false
+    
     var body: some View {
         VStack {
-            ScrollView (.vertical, showsIndicators: false) {
-                ForEach (0..<5) { i in
-
-                    Text("Item \(i)").padding()
-                    .onTapGesture {
-                        self.selectedItem = SomeItem(id: i)
-
-                        // below simulate change identity while alert is shown
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-//                            self.selectedItem = nil                    // works !!
-
-                             self.selectedItem?.id = 100             // crash !!
-                            // self.selectedItem = SomeItem(id: 100)  // crash !!
-                        }
-                    }
-                }
+            TextField("Placeholder", text: $text)
+                .background(Color.white)
+            Button(action: { self.isEditing = true }) {
+                Text("Button")
             }
-        }
-        .alert(item: self.$selectedItem) { item in
-             Alert(title: Text("Alert"), message: Text("For item \(item.id)"), dismissButton: .default(Text("OK")))
+            }.padding().sheet(
+            isPresented: self.$isEditing
+        ) {
+            Form {
+                Text("Hello")
+            }.scaleEffect(0.5)
         }
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
