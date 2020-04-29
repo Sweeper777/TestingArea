c67d31c4958c97b0783048851054e825b4d183d6
Mulang Su <sumulang@gmail.com>
2020-04-29 18:23:32 +0100

    wrote an MCVE for an SO question

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,5 +1,7 @@
 import UIKit
 import DCTextEngine
+import RxSwift
+import RxCocoa
 
 @available(iOS 10.0, *)
 class MyViewController2: UIViewController {
@@ -10,6 +12,10 @@ class MyViewController2: UIViewController {
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
     // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
+    
+    let disposeBag = DisposeBag()
+    
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,4 +38,62 @@ class MyViewController2: UIViewController {
     
     @available(*, unavailable)
     @objc func foo() {}
+}
+
+class Timer {
+    var paused = true
+    {
+        didSet {
+            rxPaused.accept(paused)
+        }
+    }
+    var ended = false
+    
+    let rxPaused = BehaviorRelay(value: true)
+    let disposeBag = DisposeBag()
+    var timerEvents: Observable<Int>!
+    
+    private var currentState: Int
+    private let resetState: Int
+    
+    private init(currentState: Int, resetState: Int) {
+        self.currentState = currentState
+        self.resetState = resetState
+        reset()
+    }
+    
+    static func newCountDownInstance(countDownTime: Int) -> Timer {
+        Timer(currentState: countDownTime,
+              resetState: countDownTime)
+    }
+    
+    func start() {
+        if !ended {
+            paused = false
+        }
+    }
+    
+    func pause() {
+        paused = true
+    }
+    
+    func reset() {
+        ended = false
+        currentState = resetState
+        pause()
+        timerEvents = rxPaused.asObservable()
+            .flatMapLatest {  isRunning in
+                isRunning ? .empty() : Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+            }
+            .enumerated().flatMap { (index, int) in Observable.just(index) }
+        .map { [weak self] x in (self?.resetState ?? x) - x }
+            .take(self.resetState)
+        timerEvents.subscribe(onNext: { [weak self]
+            timerEvent in
+            self?.currentState -= 1
+            }, onCompleted: {
+                [weak self] in
+                self?.ended = true
+        }).disposed(by: disposeBag)
+    }
 }
