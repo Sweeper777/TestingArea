9c01c5747a3f0109320be8af98fa5c9a8a53336d
Mulang Su <sumulang@gmail.com>
2020-05-27 10:43:29 +0100

    tried the evaporate animation, and with system font

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,15 +1,14 @@
 import UIKit
-import DCTextEngine
 import RxSwift
 import RxCocoa
-import Vision
-import CoreMedia
+import LTMorphingLabel
 
 @available(iOS 10.0, *)
 class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
+    @IBOutlet var label: LTMorphingLabel!
     
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
@@ -21,14 +20,13 @@ class MyViewController2: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.morphingEnabled = true
+        label.morphingEffect = .evaporate
         button.setImage(UIImage(named: "home"), for: .normal)
     }
     
     
     @IBAction func click() {
-        textview.font = .systemFont(ofSize: textview.font!.pointSize)
+        self.label.text = "00:00"
     }
-    
-    @available(*, unavailable)
-    @objc func foo() {}
 }
