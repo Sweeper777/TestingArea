18b71002bd5fe50973e8ecf708c5631d40c8f44e
Mulang Su <sumulang@gmail.com>
2020-06-07 10:19:46 +0100

    tested code from an SO question

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -1,16 +1,34 @@
 import UIKit
-import SwiftyUtils
-import SwiftyJSON
+import Combine
 
-class MyTableViewController: UITableViewController {
-    @IBOutlet var myView: UIView!
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.section == 1 && indexPath.row == 0 {
-            return tableView.width
-        } else {
-            return UITableView.automaticDimension
-        }
+class MyTableViewController: UIViewController {
+
+    var cancelble = Set<AnyCancellable>()
+    let publisher = PassthroughSubject<Date,Never>()
+    lazy var delayPublisher = publisher.delay(for: .seconds(0.5), scheduler: DispatchQueue.main)
+    override func viewDidLoad() {
+
+
+        //let timerPublisher = Timer
+        //    .publish(every: 1.0, on: RunLoop.main, in: .common)
+        //    .autoconnect()
+        //    .subscribe(publisher)
+
+        
+        delayPublisher
+            .sink { (date) in
+                print("delay publisher value :\(date)")
+        }.store(in: &cancelble)
+
+        publisher
+            .sink { (value) in
+                print("publisher value :\(value)")
+        }.store(in: &cancelble)
+
+
+
+
+        publisher.send(Date())
     }
 }
 
