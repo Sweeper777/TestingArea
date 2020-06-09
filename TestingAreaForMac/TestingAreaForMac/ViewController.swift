6943001532b51ad8e7bd6133c14d5c4230248f64
Mulang Su <sumulang@gmail.com>
2020-06-09 10:21:09 +0100

    tried making an NSPopoverButton expand programmatically

--- TestingAreaForMac/TestingAreaForMac/ViewController.swift
+++ TestingAreaForMac/TestingAreaForMac/ViewController.swift
@@ -1,65 +1,15 @@
 import Cocoa
-import SwiftyXMLParser
-import Automator
 
 class ViewController: NSViewController {
     
-    @IBOutlet var slider: NSSlider!
-    @IBOutlet var myView: MyView!
-    
-    var svgStrings: [String]!
+    @IBOutlet var button: NSPopUpButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    override func viewDidAppear() {
-        super.viewDidAppear()
-        
-        let xmlString = try! String(contentsOfFile: Bundle.main.path(forResource: "map", ofType: "svg")!)
-        let xml = try! XML.parse(xmlString)
-        svgStrings = Array(xml["svg", "g" ,"path"])
-            .map { $0.attributes["d"]! }
-            .map(editSVGString)
-        myView.svgStrings = svgStrings
-        slider.minValue = 0
-        slider.maxValue = Double(svgStrings.count)
-        slider.integerValue = svgStrings.count
-        slider.isContinuous = true
+    @IBAction func f(_ sender: Any) {
+        button.performClick(nil)
     }
     
-    override func viewDidAppear() {
-        super.viewDidAppear()
-
-        let dd = dateFromString("Aug 20", format: "MMM yy")
-        
-        print("")
-    }
-    
-    func dateFromString(_ dateString:String,  format: String = "dd/MM/yy") -> Date?
-    {
-        let dueDateFormatter = DateFormatter()
-        dueDateFormatter.timeZone = TimeZone(identifier: "UTC")!
-        dueDateFormatter.dateFormat = format
-        let date = dueDateFormatter.date(from: dateString)
-        return date
-     }
-    
-    func editSVGString(_ string: String) -> String {
-        let uppercased = string.uppercased()
-//            if !uppercased.hasSuffix("Z") {
-//                return uppercased + "Z"
-//            } else {
-            return uppercased
-//            }
-    }
-    
-    @IBAction func sliderChanged(_ sender: Any) {
-        myView.svgStrings = [svgStrings[slider.integerValue - 1]]
-    }
-    
-}
-
-struct Foo {
-    let a: Date
 }
