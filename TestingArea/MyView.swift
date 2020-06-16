4bd90d3a0a26910798d6590e7618008a188f89a6
Mulang Su <sumulang@gmail.com>
2020-06-16 09:06:33 +0100

    added commonInit

--- TestingArea/MyView.swift
+++ TestingArea/MyView.swift
@@ -4,10 +4,22 @@ import SwiftyUtils
 class AnalogClockView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
+        commonInit()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        commonInit()
     }
+    
+    func commonInit() {
+        labels.append(contentsOf: (1...12).map {
+            let label = UILabel()
+            label.text = "\($0)"
+            return label
+        })
+        labels.forEach(self.addSubview(_:))
+    }
+    
     }
 }
