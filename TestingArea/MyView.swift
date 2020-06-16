80c470d5322ab6b76d889bedd959f0a08925e71a
Mulang Su <sumulang@gmail.com>
2020-06-16 09:05:54 +0100

    added AnalogClockView

--- TestingArea/MyView.swift
+++ TestingArea/MyView.swift
@@ -1,44 +1,13 @@
 import UIKit
+import SwiftyUtils
 
-class ToolTipView: UIView {
-
-    let tipOffset : CGFloat = 10.0
-
+class AnalogClockView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
-
-        print(frame)
-
-        let mainRect = CGRect(x: bounds.minX, y: bounds.minY, width: bounds.width, height: bounds.height - tipOffset)
-        let roundRectBez = UIBezierPath(roundedRect: mainRect, cornerRadius: 3.0)
-        let mainShape = CAShapeLayer()
-        mainShape.path = roundRectBez.cgPath
-        mainShape.fillColor = UIColor.darkGray.cgColor
-        self.layer.addSublayer(mainShape)
-
-        let trianglePath = createTip(_frame: bounds)
-        self.layer.insertSublayer(trianglePath, at: 0)
     }
-
+    
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
-
-    private func createTip(_frame: CGRect) -> CAShapeLayer{
-        let shape = CAShapeLayer()
-
-        let tipRect = CGRect(x: _frame.minX, y: _frame.minY, width: _frame.width, height: _frame.height - tipOffset)
-
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: tipRect.midX - 10, y: tipRect.maxY))
-        path.addLine(to: CGPoint(x: tipRect.midX, y: tipRect.maxY + tipOffset))
-        path.addLine(to: CGPoint(x: tipRect.midX + 10, y: tipRect.maxY))
-        path.addLine(to: CGPoint(x: tipRect.midX - 10, y: tipRect.maxY))
-        path.close()
-
-        shape.path = path.cgPath
-        shape.fillColor = UIColor.darkGray.cgColor
-
-        return shape
     }
 }
