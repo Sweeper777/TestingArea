641d592732a47784d48f1df4c0f35749e5364850
Mulang Su <sumulang@gmail.com>
2020-06-19 11:07:03 +0800

    updated the analog clock implementation so as to profile it

--- TestingArea/MyView.swift
+++ TestingArea/MyView.swift
@@ -5,14 +5,22 @@ class AnalogClockView: UIView {
     
     var labels = [UILabel]()
     
-    var dateComponents: DateComponents!
+    var date: Date!
     var displayLink: CADisplayLink!
+    
+    var font = UIFont.monospacedDigitSystemFont(ofSize: 20, weight: .regular) {
+        didSet {
+            setNeedsLayout()
+        }
+    }
     
     var clockFrame: CGRect {
         if width > height {
             return CGRect(x: (width - height) / 2, y: 0, width: height, height: height)
+                .insetBy(dx: clockSize * borderWidthScale / 2, dy: clockSize * borderWidthScale / 2)
         } else {
             return CGRect(x: 0, y: (height - width) / 2, width: width, height: width)
+                .insetBy(dx: clockSize * borderWidthScale / 2, dy: clockSize * borderWidthScale / 2)
         }
     }
     
@@ -42,11 +50,17 @@ class AnalogClockView: UIView {
     }
     
     @objc func updateDisplay() {
-        let newDateComponents = Calendar.autoupdatingCurrent.dateComponents([.hour, .minute, .second], from: Date())
-        if dateComponents == nil || newDateComponents != dateComponents {
-            dateComponents = newDateComponents
+        let newDate = Date()
+        if date == nil || Int(date.timeIntervalSince1970) != Int(newDate.timeIntervalSince1970) {
+            date = newDate
             setNeedsDisplay()
         }
+    }
+    
+    struct HandInfo {
+        let longRadius: CGFloat
+        let shortRadius: CGFloat
+        let widthScale: CGFloat
     }
     
     let digitsInset = 0.3.f
@@ -56,22 +70,16 @@ class AnalogClockView: UIView {
     let thickMarkingInset = 0.16.f
     let thinMarkingWidthScale = 0.01.f
     let thickMarkingWidthScale = 0.014.f
-    let secondHandLongRadius = 0.9.f
-    let secondHandShortRadius = 0.1.f
-    let secondHandWidthScale = 0.01.f
-    let minuteHandLongRadius = 0.85.f
-    let minuteHandShortRadius = 0.095.f
-    let minuteHandWidthScale = 0.02.f
-    let hourHandLongRadius = 0.5.f
-    let hourHandShortRadius = 0.05.f
-    let hourHandWidthScale = 0.03.f
+    let secondHand = HandInfo(longRadius: 0.9, shortRadius: 0.1, widthScale: 0.01)
+    let minuteHand = HandInfo(longRadius: 0.85, shortRadius: 0.095, widthScale: 0.02)
+    let hourHand = HandInfo(longRadius: 0.5, shortRadius: 0.05, widthScale: 0.03)
     
     override func layoutSubviews() {
         super.layoutSubviews()
         let center = CGPoint(x: clockFrame.midX, y: clockFrame.midY)
         let radius = clockSize * (1 - digitsInset) / 2
         for (label, angle) in zip(labels, stride(from: CGFloat.pi / -3, to: 5 * .pi / 3, by: .pi / 6)) {
-            label.font = UIFont.monospacedDigitSystemFont(ofSize: clockSize * fontScale, weight: .regular)
+            label.font = font.withSize(clockSize * fontScale)
             label.sizeToFit()
             label.center = center.applying(CGAffineTransform(translationX: radius * cos(angle), y: radius * sin(angle)))
         }
@@ -94,7 +102,7 @@ class AnalogClockView: UIView {
             }
         }
         
-        guard let dc = dateComponents else { return }
+        guard let dc = date.map({ Calendar.autoupdatingCurrent.dateComponents([.hour, .minute, .second], from: $0) }) else { return }
         func calculateHourAngle() -> CGFloat {
             let hourPart = dc.hour!.f / 12.f
             let minutePart = dc.minute!.f / (12 * 60).f
@@ -103,9 +111,9 @@ class AnalogClockView: UIView {
         }
         let hourAngle = calculateHourAngle()
         drawHand(angle: hourAngle - .pi / 2,
-                 longRadius: clockSize * hourHandLongRadius / 2,
-                 shortRadius: clockSize * hourHandShortRadius / 2,
-                 lineWidth: clockSize * hourHandWidthScale,
+                 longRadius: clockSize * hourHand.longRadius / 2,
+                 shortRadius: clockSize * hourHand.shortRadius / 2,
+                 lineWidth: clockSize * hourHand.widthScale,
                  color: .label)
         
         func calculateMinuteAngle() -> CGFloat {
@@ -115,16 +123,16 @@ class AnalogClockView: UIView {
         }
         let minuteAngle = calculateMinuteAngle()
         drawHand(angle: minuteAngle - .pi / 2,
-                 longRadius: clockSize * minuteHandLongRadius / 2,
-                 shortRadius: clockSize * minuteHandShortRadius / 2,
-                 lineWidth: clockSize * minuteHandWidthScale,
+                 longRadius: clockSize * minuteHand.longRadius / 2,
+                 shortRadius: clockSize * minuteHand.shortRadius / 2,
+                 lineWidth: clockSize * minuteHand.widthScale,
                  color: .label)
         
         let secondAngle = dc.second!.f / 60 * 2 * .pi
         drawHand(angle: secondAngle - .pi / 2,
-                 longRadius: clockSize * secondHandLongRadius / 2,
-                 shortRadius: clockSize * secondHandShortRadius / 2,
-                 lineWidth: clockSize * secondHandWidthScale,
+                 longRadius: clockSize * secondHand.longRadius / 2,
+                 shortRadius: clockSize * secondHand.shortRadius / 2,
+                 lineWidth: clockSize * secondHand.widthScale,
                  color: .systemRed)
     }
     
