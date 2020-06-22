343872ba7ddad344f31ba0ec2bb494800fc34680
Mulang Su <sumulang@gmail.com>
2020-06-22 08:35:55 +0800

    tested some SwiftUI code from an SO question

--- TestingArea/SwiftUITest.swift
+++ TestingArea/SwiftUITest.swift
@@ -1,27 +1,21 @@
 import SwiftUI
 import Combine
-struct SomeItem: Identifiable { // identifiable item
-    var id: Int // identity
-}
+
 
 struct ContentView : View {
-    @State var text: String = "Hello"
-    @State var isEditing: Bool = false
-    
+
     var body: some View {
-        VStack {
-            TextField("Placeholder", text: $text)
-                .background(Color.white)
-            Button(action: { self.isEditing = true }) {
-                Text("Button")
-            }
-            }.padding().sheet(
-            isPresented: self.$isEditing
-        ) {
-            Form {
-                Text("Hello")
-            }.scaleEffect(0.5)
-        }
+        Form{
+            Section(header: Text("Sort")){
+                List { ForEach(0...10, id: \.self) { item in
+                    Text("\(item)")
+                }
+                .onMove(perform: {_, _ in}) }
+            } // Sort Section
+        } // Form
+        .navigationBarItems(trailing:
+               EditButton()
+        )
     }
 }
 
