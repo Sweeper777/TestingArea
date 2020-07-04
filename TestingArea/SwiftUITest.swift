74f181a989af468758f7f6a8143cbba636272cda
Mulang Su <sumulang@gmail.com>
2020-07-04 11:21:35 +0800

    tested SwiftUI code from an SO question

--- TestingArea/SwiftUITest.swift
+++ TestingArea/SwiftUITest.swift
@@ -4,54 +4,36 @@ import Combine
 
 import SwiftUI
 
-struct GradientView: View {
-    @State private var angle: Double = 0
-    var foreverAnimation: Animation {
-            Animation.easeInOut(duration: 3)
-            .repeatForever()
-     }
-
+struct ContentView: View {
+    @State private var model = (0...20).map(\.description)
+    
     var body: some View {
-        VStack(alignment: .leading){
-            Button(action: {
-                self.angle += 360
-            }) {
-                Text("Tap here")
-                    .foregroundColor(.white)
-                    .padding()
-                    .rotationEffect(.degrees(angle))
-                    .animation(.easeInOut)
+        VStack(alignment: .leading) {
+            Text("Text 1")
+            Text("Text 2")
+            Text("Text 3")
+            Text("Text 4")
+
+            List {
+                ForEach(self.model, id: \.description) { fooItem in
+                   Text(fooItem)
+                }
             }
+
+            Text("Want to pin this to the bottom")
         }
-        .frame(minWidth: 0,
-               maxWidth: .infinity,
-               minHeight: 0,
-               maxHeight: .infinity,
-               alignment: .topLeading)
-        .background(
-            AngularGradient(
-                gradient: Gradient(colors:
-                    [.red, .yellow,.green,.blue,.purple,.red]
-                ),
-                center: .center,
-                startAngle: .degrees(self.angle),
-                endAngle: .degrees(self.angle + 360)
-            )
-            .edgesIgnoringSafeArea(.all)
-        )
-        .animation(foreverAnimation)
     }
 }
 
-struct GradientView_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        GradientView()
+        ContentView()
     }
 }
 
-class MyHostingController: UIHostingController<GradientView> {
+class MyHostingController: UIHostingController<ContentView> {
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder, rootView: GradientView())
+        super.init(coder: aDecoder, rootView: ContentView())
         
     }
 }
