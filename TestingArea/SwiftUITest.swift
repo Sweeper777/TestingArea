01b6f7eead54bdc78bfc15b3de9ab8aa540a13dd
Mulang Su <sumulang@gmail.com>
2020-07-11 10:17:34 +0800

    tested some code from an SO question

--- TestingArea/SwiftUITest.swift
+++ TestingArea/SwiftUITest.swift
@@ -5,22 +5,19 @@ import Combine
 import SwiftUI
 
 struct ContentView: View {
-    @State private var model = (0...20).map(\.description)
-    
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Text 1")
-            Text("Text 2")
-            Text("Text 3")
-            Text("Text 4")
-
-            List {
-                ForEach(self.model, id: \.description) { fooItem in
-                   Text(fooItem)
+        NavigationView {
+            VStack {
+                List {
+                    Text("Hello, World!")
                 }
+                
+                Spacer()
+                
+                Rectangle()
+                    .frame(height: 150)
             }
-
-            Text("Want to pin this to the bottom")
+            .edgesIgnoringSafeArea(.bottom)
         }
     }
 }
