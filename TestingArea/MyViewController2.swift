d8d480a03e5ad867c23024bb4e8cb1a5538bfde9
Mulang Su <sumulang@gmail.com>
2020-07-18 16:24:13 +0800

    tried using picker as input view

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,6 +1,5 @@
 import UIKit
-import RxSwift
-import RxCocoa
+import Combine
 import SwiftyJSON
 
 @available(iOS 10.0, *)
@@ -9,105 +8,51 @@ class MyViewController2: UIViewController {
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
     
-    @IBOutlet var view1: UIView!
-    @IBOutlet var view2: UIView!
     @IBOutlet var segmentedControl: UISegmentedControl!
     
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
     // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
     
-    let disposeBag = DisposeBag()
-    
-    private let infoButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.backgroundColor = .clear
-        button.adjustsImageWhenHighlighted = false
-        button.setTitle("Press Me!", for: .normal)
-        button.addTarget(self, action: #selector(click), for: .touchUpInside)
-        return button
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        infoButton.frame = CGRect(x: 200, y: 200, width: 100, height: 100)
-        view.addSubview(infoButton)
+        let picker = UIPickerView()
+        picker.tag = 1
+        picker.delegate = self
+        picker.dataSource = self
+        textfield.inputView = picker
+        textview.tag = 2
+        
+        
     }
     
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        print("changed")
+    }
     
+    var cancellable: AnyCancellable!
     @IBAction private func click() {
-        print("Hello")
+        cancellable = Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .default)
+            .autoconnect()
+            .throttle(for: 5, scheduler: DispatchQueue.main, latest: false)
+            .sink { (date) in
+                print(date)
+        }
+        
     }
 }
 
-public class CustomView: UIView {
-
-    var topConstraint: NSLayoutConstraint!
-
-    public let customButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Save", for: .normal)
-        button.layer.cornerRadius = 10
-        button.layer.borderWidth = 1
-        button.layer.borderColor = UIColor.red.cgColor
-        button.setTitleColor(.red, for: .normal)
-        
-        return button
-    }()
-
-    public var buttonWidth: CGFloat = 100
-
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        initConstraints()
-    }
-
-
-    private func initConstraints(){
-        addSubview(customButton)
-        topConstraint = customButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 80)
-        NSLayoutConstraint.activate([
-            
-            topConstraint,
-            customButton.centerXAnchor.constraint(equalTo: centerXAnchor),
-            customButton.widthAnchor.constraint(equalToConstant: buttonWidth)
-
-        ])
-    }
-
-    public required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+extension MyViewController2 : UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
     }
     
-}
-
-class FrameworkView: UIView {
-
-    let customButton: CustomView = {
-        let cusomView = CustomView()
-        cusomView.translatesAutoresizingMaskIntoConstraints = false
-        return cusomView
-    }()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        initConst()
-    }
-
-    func initConst()  {
-        
-        addSubview(customButton)
-        NSLayoutConstraint.activate([
-            customButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 100),
-            customButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25),
-            customButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25),
-//            customButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -100)
-        ])
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        3
     }
     
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        ["a", "b", "c"][row]
+    }
 }
