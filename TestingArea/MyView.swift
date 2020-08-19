211553f2c65f7a3b133a8d9847c48b791cd6b9d0
Mulang Su <sumulang@gmail.com>
2020-08-19 08:57:28 +0800

    added a view from an SO question

--- TestingArea/MyView.swift
+++ TestingArea/MyView.swift
@@ -1,43 +1,57 @@
 import UIKit
 
-class TimelineItemCell: UITableViewCell {
-    let lineLeftMargin: CGFloat = 120
+@IBDesignable class CustomView: UIView {
     
-    var style: TimelineItemCellStyle = .sole {
-        didSet {
-            setNeedsDisplay()
-        }
-    }
+    private lazy var shapeLayer = CAShapeLayer()
     
-    override func draw(_ rect: CGRect) {
-        let line = UIBezierPath()
-        if [TimelineItemCellStyle.end, .middle].contains(style) {
-            line.move(to: CGPoint(x: lineLeftMargin, y: 0))
-        } else {
-            line.move(to: CGPoint(x: lineLeftMargin, y: bounds.midY))
-        }
-        if [TimelineItemCellStyle.start, .middle].contains(style) {
-            line.addLine(to: CGPoint(x: lineLeftMargin, y: bounds.maxY))
-        } else {
-            line.addLine(to: CGPoint(x: lineLeftMargin, y: bounds.midY))
-        }
-        line.lineWidth = 2
-        UIColor.black.setStroke()
-        line.stroke()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
         
-        let circle = UIBezierPath(ovalIn:
-            CGRect(x: lineLeftMargin, y: bounds.midY, width: 0, height: 0)
-                .insetBy(dx: -12, dy: -12))
-        circle.lineWidth = 2
-        UIColor.white.setFill()
-        circle.stroke()
-        circle.fill()
     }
-}
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        shapeLayer.frame = bounds
+        shapeLayer.fillColor = UIColor.black.cgColor
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 3
+        layer.addSublayer(shapeLayer)
+        
+    }
+    
+    override func layoutSubviews() {
+        
+        shapeLayer.path = drawSunShape(bounds)
+    }
+    private func drawSunShape(_ group:CGRect) -> CGPath {
+        let bezierPath = UIBezierPath()
+        let radius = group.maxX/2
+        let center =  CGPoint(x: group.midX, y: group.midY)
+        bezierPath.addArc(withCenter:center, radius: radius, startAngle: -.pi/2, endAngle: 3 * .pi * 0.5 , clockwise: true)
+        return bezierPath.cgPath
+    }
+    
+    private func drawMoonShape(_ group:CGRect) -> CGPath {
+         
+         let big = UIBezierPath()
 
-enum TimelineItemCellStyle {
-    case start
-    case end
-    case middle
-    case sole
+        big.addArc(withCenter: CGPoint(x: group.midX, y: group.midY), radius: group.width / 2, startAngle:-.pi/2, endAngle: .pi/2.0, clockwise: true)
+        big.addQuadCurve(to: CGPoint(x: group.width / 2, y: 0), controlPoint: CGPoint(x: group.width, y: group.width / 2))
+//        big.apply(.init(rotationAngle: .pi / 4))
+        
+        
+        return big.cgPath
+    }
+    func animateToOffShape()  {
+        
+        let anim = CABasicAnimation(keyPath: "path")
+        anim.toValue = drawMoonShape(bounds)
+        anim.duration = 5.0
+        shapeLayer.add(anim, forKey: nil)
+    }
 }
