767d3607af277e7d53b7ce4f60a2ebc899f921a0
Mulang Su <sumulang@gmail.com>
2020-09-05 10:36:16 +0800

    tried appending links to texts...

--- TestingArea/SwiftUITest.swift
+++ TestingArea/SwiftUITest.swift
@@ -5,20 +5,11 @@ import Combine
 import SwiftUI
 
 struct ContentView: View {
-    @State var text = "你好世界"
-    
+
      var body: some View {
-        NavigationView() {
-            Text("Hello")
-                .navigationBarTitle("Hello")
-                .navigationBarItems(trailing: Button(action: { }) {
-                    Text("Hello").foregroundColor(.red)
-                })
-        }.onAppear {
-            let standardAppearance = UINavigationBarAppearance()
-            standardAppearance.configureWithTransparentBackground()
-            UINavigationBar.appearance().standardAppearance = standardAppearance
-        }
+        Text("Hello World, ") +
+        Link("Link") +
+        Text(" is a link.")
      }
 }
 
@@ -33,22 +24,4 @@ class MyHostingController: UIHostingController<ContentView> {
         super.init(coder: aDecoder, rootView: ContentView())
         
     }
-}
-
-extension Collection {
-    
-    func chunked(by distance: Int) -> [[Element]] {
-        precondition(distance > 0, "distance must be greater than 0") // prevents infinite loop
-
-        var index = startIndex
-        let iterator: AnyIterator<Array<Element>> = AnyIterator({
-            let newIndex = self.index(index, offsetBy: distance, limitedBy: self.endIndex) ?? self.endIndex
-            defer { index = newIndex }
-            let range = index ..< newIndex
-            return index != self.endIndex ? Array(self[range]) : nil
-        })
-        
-        return Array(iterator)
-    }
-    
 }
