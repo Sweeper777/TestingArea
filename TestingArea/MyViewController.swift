ec7c030ecbb303f17e702ea8d36cb9d6b0b274ed
Mulang Su <sumulang@gmail.com>
2020-09-29 10:03:23 +0800

    tried out some code that I posted as an answer

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -2,11 +2,24 @@ import UIKit
 import SCLAlertView
 import RxSwift
 import RxCocoa
+import Stencil
 
 class MyViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        do {
+            let environment = Environment(loader: FileSystemLoader(bundle: [Bundle.main]))
+            let template = try environment.loadTemplate(name: "someLogic.stencil")
+            let context = [
+                "array1": [["property1": "Hello"], ["property1": "World"], ["property1": "Bye"]],
+                "array2": [["property1": "A"], ["property1": "B"], ["property1": "C"]],
+                "someLogic.stencil": template
+            ] as [String : Any]
+            let rendered = try environment.renderTemplate(name: "template2.stencil", context: context)
+            print(rendered)
+        } catch {
+            print(error)
+        }
     }
     
     deinit {
