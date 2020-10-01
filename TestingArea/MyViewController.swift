c3ac09f947554403f76ff4a93b241502b882e88a
Mulang Su <sumulang@gmail.com>
2020-10-01 09:07:13 +0800

    tried out code from an SO question

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -7,22 +7,49 @@ import Stencil
 class MyViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
-        do {
-            let environment = Environment(loader: FileSystemLoader(bundle: [Bundle.main]))
-//            let template = try environment.loadTemplate(name: "someLogic.stencil")
-            let context = [
-                "array1": [["property1": "Hello"], ["property1": "World"], ["property1": "Bye"]],
-                "array2": [["property1": "A"], ["property1": "B"], ["property1": "C"]],
-//                "someLogic.stencil": template
-            ] as [String : Any]
-            let rendered = try environment.renderTemplate(name: "template1.stencil", context: context)
-            print(rendered)
-        } catch {
-            print(error)
-        }
+        let usernameTextField = GFTextField(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.addSubview(usernameTextField)
+
+        NSLayoutConstraint.activate([
+          usernameTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 48),
+          usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+          usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+          usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
     
     deinit {
         print("deinit")
     }
+}
+
+class GFTextField: UITextField {
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+    
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+    
+  private func configure() {
+    translatesAutoresizingMaskIntoConstraints = false
+        
+    layer.cornerRadius          = 10
+    layer.borderWidth           = 2
+    layer.borderColor           = UIColor.systemGray4.cgColor
+        
+    textColor                   = .label
+    tintColor                   = .label
+    textAlignment               = .center
+    font                        = UIFont.preferredFont(forTextStyle: .title2)
+    adjustsFontSizeToFitWidth   = true
+    minimumFontSize             = 12
+        
+    backgroundColor             = .tertiarySystemBackground
+    autocorrectionType          = .no
+    placeholder                 = "Enter a username"
+  }
 }
