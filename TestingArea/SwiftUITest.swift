bcc8a02b1870c1930cdab3a841e361e01c670a28
Mulang Su <sumulang@gmail.com>
2020-11-07 10:14:25 +0800

    tried using navigationBarTitle(_:displayMode:)

--- TestingArea/SwiftUITest.swift
+++ TestingArea/SwiftUITest.swift
@@ -2,24 +2,10 @@ import SwiftUI
 import Combine
 
 
-import SwiftUI
-
-struct ContentView: View {
-
-     var body: some View {
-        Text("Hello World, ")
-     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
-class MyHostingController: UIHostingController<ContentView> {
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder, rootView: ContentView())
-        
+struct DetailView: View {
+    var title: String = ""
+    var body: some View {
+        Text("Hi!")
+            .navigationBarTitle("", displayMode: .inline)
     }
 }
