cd85ea6894eb2a7438af4b53132f5e0a14c1c9d3
Mulang Su <sumulang@gmail.com>
2020-11-09 14:43:10 +0800

    added simple table view

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -4,6 +4,10 @@ import Alamofire
 import SCLAlertView
 
 class MyTableViewController: UITableViewController {
+    @IBOutlet var moreButton: UIBarButtonItem!
+    
+    let data = ["Hello", "World", "Some", "Text"]
+
     override func viewDidLoad() {
         moreButton.menu = UIMenu(title: "Title", image: UIImage(systemName: "ellipsis"), identifier: nil, options: .destructive, children: [
             UIAction(title: "Action 1", image: UIImage(systemName: "mappin"), handler: { (_) in
@@ -39,6 +43,20 @@ class MyTableViewController: UITableViewController {
     
     @objc func foo() {
         print("Hello World")
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
     
     }
