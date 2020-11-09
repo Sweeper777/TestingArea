2c6a02b6947592a0943053b2232aabdeb272ba50
Mulang Su <sumulang@gmail.com>
2020-11-09 14:42:55 +0800

    tried adding a submenu

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -19,6 +19,21 @@ class MyTableViewController: UITableViewController {
                 print("Hello World!")
             }),
             UICommand(title: "Action 5", action: #selector(foo)),
+            UIMenu(title: "Title", image: UIImage(systemName: "ellipsis"), identifier: nil, options: .displayInline, children: [
+                UIAction(title: "Action 1", image: UIImage(systemName: "mappin"), handler: { (_) in
+                    print("Hello World!")
+                }),
+                UIAction(title: "Action 2", state: .on, handler: { (_) in
+                    print("Hello World!")
+                }),
+                UIAction(title: "Action 3", state: .mixed, handler: { (_) in
+                    print("Hello World!")
+                }),
+                UIAction(title: "Action 4", attributes: [.destructive], handler: { (_) in
+                    print("Hello World!")
+                }),
+                UICommand(title: "Action 5", action: #selector(foo)),
+            ])
         ])
     }
     
