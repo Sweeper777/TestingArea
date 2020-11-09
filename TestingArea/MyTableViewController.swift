36481dcafc7dfcb1c7428f4963c67799f6b56004
Mulang Su <sumulang@gmail.com>
2020-11-09 14:42:26 +0800

    tried making a UIMenu

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -5,7 +5,21 @@ import SCLAlertView
 
 class MyTableViewController: UITableViewController {
     override func viewDidLoad() {
-       
+        moreButton.menu = UIMenu(title: "Title", image: UIImage(systemName: "ellipsis"), identifier: nil, options: .destructive, children: [
+            UIAction(title: "Action 1", image: UIImage(systemName: "mappin"), handler: { (_) in
+                print("Hello World!")
+            }),
+            UIAction(title: "Action 2", state: .on, handler: { (_) in
+                print("Hello World!")
+            }),
+            UIAction(title: "Action 3", state: .mixed, handler: { (_) in
+                print("Hello World!")
+            }),
+            UIAction(title: "Action 4", attributes: [.destructive], handler: { (_) in
+                print("Hello World!")
+            }),
+        ])
+    }
     }
 }
 
