86c0895abc554fe72d4ffb1b1610ef8776d2ea32
Mulang Su <sumulang@gmail.com>
2020-11-12 21:44:50 +0800

    added CLTokenInputView to MyVC2

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,12 +1,11 @@
 import UIKit
 import RxSwift
 import RxCocoa
-import SCLAlertView
-import SwiftyJSON
-import PhotosUI
+import CLTokenInputView
+import SwiftyUtils
 
 @available(iOS 10.0, *)
-class MyViewController2: UICollectionViewController {
+class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
@@ -18,10 +17,22 @@ class MyViewController2: UICollectionViewController {
     // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
     let disposeBag = DisposeBag()
     
+    var tokenView: CLTokenInputView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print(UIScreen.main.scale)
+        tokenView = CLTokenInputView(frame: CGRect(x: 0, y: 0, width: 100, height: 44))
+        view.addSubview(tokenView)
+        tokenView.translatesAutoresizingMaskIntoConstraints = false
+        tokenView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        tokenView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        tokenView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        tokenView.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        tokenView.delegate = self
+        tokenView.layer.borderWidth = 1
+        tokenView.layer.borderColor = UIColor.black.cgColor
+        Bundle.main.appBuild
     }
     
     @IBAction private func click() {
