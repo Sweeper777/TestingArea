a1eb63925fb52dcaa26e71ebafd79dcd2bd72ec6
Mulang Su <sumulang@gmail.com>
2020-11-16 21:18:22 +0800

    tried reproducing an SO question about SwiftUI

--- TestingArea/SwiftUITest.swift
+++ TestingArea/SwiftUITest.swift
@@ -3,9 +3,20 @@ import Combine
 
 
 struct DetailView: View {
-    var title: String = ""
+    @State var value: Int = 10
+    
     var body: some View {
-        Text("Hi!")
-            .navigationBarTitle("", displayMode: .inline)
+        VStack {
+            Text("not updating: \(value == 10 ? "looks like a ten" : "must be something else")")
+            Button(action: { value = 9 }, label: {
+                Text("Button")
+            })
+        }
+    }
+}
+
+public struct SwiftUITest_Previews: PreviewProvider {
+    public static var previews: some View {
+        DetailView()
     }
 }
