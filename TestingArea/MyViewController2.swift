03fbb991dbb0172ef43e9a99a8f7ede704874ab1
Mulang Su <sumulang@gmail.com>
2020-11-26 22:05:53 +0800

    added a PDFView

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -1,9 +1,8 @@
 import UIKit
 import RxSwift
 import RxCocoa
-import TagWriteView
 import SwiftyUtils
-import TagsPanelView
+import PDFKit
 
 @available(iOS 10.0, *)
 class MyViewController2: UIViewController {
@@ -18,47 +17,28 @@ class MyViewController2: UIViewController {
     // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
     let disposeBag = DisposeBag()
     
-    var tagWriteView: TagWriteView!
-    var tagsPanelView: TagsPanelView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(UIScreen.main.scale)
-        tagWriteView = TagWriteView()
-        tagWriteView.frame = CGRect(x: 0, y: 0, width: 100, height: 44)
-        view.addSubview(tagWriteView)
-        tagWriteView.translatesAutoresizingMaskIntoConstraints = false
-        tagWriteView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        tagWriteView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        tagWriteView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        tagWriteView.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        tagWriteView.delegate = self
-        tagWriteView.layer.borderWidth = 1
-        tagWriteView.layer.borderColor = UIColor.black.cgColor
         
-        tagsPanelView = TagsPanelView(frame: CGRect(x: 0, y: 0, width: 100, height: 44))
-        view.addSubview(tagsPanelView)
-        tagsPanelView.translatesAutoresizingMaskIntoConstraints = false
-        tagsPanelView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        tagsPanelView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        tagsPanelView.topAnchor.constraint(equalTo: tagWriteView.bottomAnchor, constant: 20).isActive = true
-        tagsPanelView.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        self.view.subviews.forEach { $0.removeFromSuperview() }
         
-        tagsPanelView.tagArray = ["hello", "world", "many", "tags", "more", "cool", "stuff", "overflow"]
-        tagsPanelView.tagsBackgroundColorsArray = Array(repeating: .blue, count: 8)
+        let documentURL = Bundle.main.url(forResource: "test", withExtension: "pdf")!
+        let document = PDFDocument(url: documentURL)!
+        
+        let pdfView = PDFView()
+
+        pdfView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pdfView)
+
+        pdfView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        pdfView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        pdfView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        
+        
         
     }
     
     @IBAction private func click() {
 //        test2()
-    }
-}
-
-extension MyViewController2 : TagWriteViewDelegate {
-    func tagWriteView(view: TagWriteView!, didMakeTag tag: String!) {
-        print("Made tag: \(tag!)")
-    }
-    func tagWriteView(view: TagWriteView!, didRemoveTag tag: String!) {
-        print("Removed tag: \(tag!)")
     }
 }
