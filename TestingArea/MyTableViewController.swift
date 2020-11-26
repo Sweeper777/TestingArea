8910276d9fc1428deba449d35e217a0b5a5d634e
Mulang Su <sumulang@gmail.com>
2020-11-26 22:05:25 +0800

    deleted the UIMenu stuff from before

--- TestingArea/MyTableViewController.swift
+++ TestingArea/MyTableViewController.swift
@@ -7,39 +7,6 @@ class MyTableViewController: UITableViewController {
     @IBOutlet var moreButton: UIBarButtonItem!
     
     let data = ["Hello", "World", "Some", "Text"]
-
-    override func viewDidLoad() {
-        moreButton.menu = UIMenu(title: "Title", image: UIImage(systemName: "ellipsis"), identifier: nil, options: .destructive, children: [
-            UIAction(title: "Action 1", image: UIImage(systemName: "mappin"), handler: { (_) in
-                print("Hello World!")
-            }),
-            UIAction(title: "Action 2", state: .on, handler: { (_) in
-                print("Hello World!")
-            }),
-            UIAction(title: "Action 3", state: .mixed, handler: { (_) in
-                print("Hello World!")
-            }),
-            UIAction(title: "Action 4", attributes: [.destructive], handler: { (_) in
-                print("Hello World!")
-            }),
-            UICommand(title: "Action 5", action: #selector(foo)),
-            UIMenu(title: "Title", image: UIImage(systemName: "ellipsis"), identifier: nil, options: .displayInline, children: [
-                UIAction(title: "Action 1", image: UIImage(systemName: "mappin"), handler: { (_) in
-                    print("Hello World!")
-                }),
-                UIAction(title: "Action 2", state: .on, handler: { (_) in
-                    print("Hello World!")
-                }),
-                UIAction(title: "Action 3", state: .mixed, handler: { (_) in
-                    print("Hello World!")
-                }),
-                UIAction(title: "Action 4", attributes: [.destructive], handler: { (_) in
-                    print("Hello World!")
-                }),
-                UICommand(title: "Action 5", action: #selector(foo)),
-            ])
-        ])
-    }
     
     @objc func foo() {
         print("Hello World")
@@ -57,25 +24,6 @@ class MyTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
         cell.textLabel?.text = data[indexPath.row]
         return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
-        UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { (_) -> UIMenu? in
-            UIMenu(title: self.data[indexPath.row], image: UIImage(systemName: "ellipsis"), identifier: nil, options: .displayInline, children: [
-                UIAction(title: "Action 1", image: UIImage(systemName: "mappin"), handler: { (_) in
-                    print("Hello World!")
-                }),
-                UIAction(title: "Action 2", state: .on, handler: { (_) in
-                    print("Hello World!")
-                }),
-                UIAction(title: "Action 3", state: .mixed, handler: { (_) in
-                    print("Hello World!")
-                }),
-                UIAction(title: "Action 4", attributes: [.destructive], handler: { (_) in
-                    print("Hello World!")
-                }),
-            ])
-        }
     }
 }
 
