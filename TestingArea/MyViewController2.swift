7837cc3df1917e98b45abcfec3a02aa241a94331
Mulang Su <sumulang@gmail.com>
2020-12-20 15:40:39 +0800

    tried code in an SO question

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -3,6 +3,17 @@ import RxSwift
 import RxCocoa
 import SwiftyUtils
 
+class SimpleViewModel {
+    var list = BehaviorRelay<[String]>(value: [])
+    var randomString = BehaviorRelay<String>(value: "")
+    
+    func fetch() {
+        // Request...
+        list.accept(["result1", "result2", "result3"])
+        randomString.accept("Random...")
+    }
+}
+
 @available(iOS 10.0, *)
 class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
@@ -11,6 +22,9 @@ class MyViewController2: UIViewController {
     @IBOutlet var image: UIImageView!
     @IBOutlet var segmentedControl: UISegmentedControl!
     
+    let tableView = UITableView()
+    let viewModel = SimpleViewModel()
+    
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
     // large text(7):   23, 22, 18, 17, 19, 23(b), 21, 34, 28, 26
@@ -18,7 +32,12 @@ class MyViewController2: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        Observable.combineLatest(viewModel.list, viewModel.randomString)
+            .map { (list, randomString) in list.map { (element: $0, randomString: randomString) } }
+    .bind(to: tableView.rx.items(cellIdentifier: "cell")) {
+    (index, model, cell) in
+        cell.textLabel?.text = model.element + model.randomString
+    }.disposed(by: disposeBag)
     }
     
     
