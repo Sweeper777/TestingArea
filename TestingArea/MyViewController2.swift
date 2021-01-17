b5050a4e75f39fe8f1af4530d2a397fe3f30731c
Mulang Su <sumulang@gmail.com>
2021-01-17 21:01:13 +0800

    deleted the generating pdf test

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -18,21 +18,6 @@ class MyViewController2: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let format = UIGraphicsPDFRendererFormat()
-        format.documentInfo = [
-            kCGPDFContextCreator as String: "My App",
-            kCGPDFContextAuthor as String: "Sweeper777"
-          ]
-        let paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar, massa vitae malesuada sollicitudin, mauris purus condimentum metus, eget gravida nunc mauris sit amet orci. Cras nec magna turpis. Cras risus justo, ullamcorper id ex quis, lobortis mattis elit. Nunc a ligula auctor dolor cursus lobortis id ac velit. Cras eget tincidunt dui, eget auctor purus. Nunc quam urna, vulputate at est at,\n\n\n\n\n\n sodales aliquam neque. Praesent tincidunt, justo nec pellentesque imperdiet, nulla justo tristique ligula, quis maximus elit nulla eget diam. Praesent a nunc id orci rhoncus tempus et vitae mi. Donec commodo elementum neque a eleifend. Praesent faucibus pretium faucibus. In hac habitasse platea dictumst. Mauris rhoncus nisi nisi, at venenatis diam ornare auctor."
-
-        let data = PDFCreator().pdfWithText(paragraph + "\n\n\n\n" + paragraph, attributes: [.font: UIFont.monospacedSystemFont(ofSize: 16, weight: .regular)])
-        do {
-            let url = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent("test.pdf")
-            try data.write(to: url, options: .atomic)
-            print("Written PDF file to \(url)")
-        } catch {
-            print(error)
-        }
     }
     
     
