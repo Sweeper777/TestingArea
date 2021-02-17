66c35b5fd7276083751532d62ec299b1253317c7
Mulang Su <sumulang@gmail.com>
2021-02-17 22:00:14 +0800

    added generateLabel

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -13,4 +13,16 @@ class MyViewController: UIViewController{
     }
     
     
+    func generateLabel(text: String, textColor: UIColor = .black, bgColor: UIColor = .white) -> UILabel {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.backgroundColor = bgColor
+        label.textColor = textColor
+        label.layer.borderColor = UIColor.red.cgColor
+        label.layer.borderWidth = 1
+        label.textAlignment = .center
+        return label
+    }
+    
 }
