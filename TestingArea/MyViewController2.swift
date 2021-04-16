7dc4f8399f5d92625e09096d1eb32ba598c35069
Mulang Su <sumulang@gmail.com>
2021-04-16 10:03:10 +0800

    tried dismissing date picker

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -9,8 +9,8 @@ class MyViewController2: UIViewController {
     @IBOutlet var textview: UITextView!
     @IBOutlet var textfield: UITextField!
     @IBOutlet var button: UIButton!
-    @IBOutlet var image: UIImageView!
     @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var datePicker: UIDatePicker!
     
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
@@ -19,17 +19,14 @@ class MyViewController2: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        textfield.delegate = foo
+        datePicker.addTarget(self, action: #selector(click), for: .valueChanged)
     }
-    
-    private let foo = Foo()
     
     @IBAction private func click() {
+        presentedViewController?.dismiss(animated: true, completion: nil)
     }
     
-    private class Foo: NSObject, UITextFieldDelegate {
-        func textFieldDidBeginEditing(_ textField: UITextField) {
-            print("DID BEGIN EDITING!")
-        }
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
     }
 }
