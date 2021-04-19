567853a70ccf1f0781c92d0ccc71ec8e16299933
Mulang Su <sumulang@gmail.com>
2021-04-19 09:33:42 +0800

    tried setting baselineOffset on a UILabel

--- TestingArea/MyViewController2.swift
+++ TestingArea/MyViewController2.swift
@@ -11,6 +11,7 @@ class MyViewController2: UIViewController {
     @IBOutlet var button: UIButton!
     @IBOutlet var segmentedControl: UISegmentedControl!
     @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var label: UILabel!
     
     @objc let fontStyles: [UIFont.TextStyle] = [.body, .callout, .caption1, .caption2, .footnote, .headline, .subheadline, .title1, .title2, .title3]
     // normal(4):       17, 16, 12, 11, 13, 17(b), 15, 28, 22, 20
@@ -20,13 +21,24 @@ class MyViewController2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         datePicker.addTarget(self, action: #selector(click), for: .valueChanged)
+        let str = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip jojfsfoehfhsuhfieuhsfliushfglislifhushfuhfpuhdwehfwiebfwghiguusd fhnerhfuhwof uwhfu9 whefh9 egerhg ehruhg rldf huehr gurehg erghu ehifhpr ehgpehs ghg he hr gehhrhughregh ehurhgue r ghehrg ehheru hgghehg "
+        let attrStr = NSAttributedString(string: str, attributes: [.font: UIFont.systemFont(ofSize: 16),
+                                                                       .foregroundColor: UIColor.black,
+                                                                       .baselineOffset: 10])
+        textview.isEditable = false
+        textview.isScrollEnabled = true
+
+        label.numberOfLines = 0
+
+        textview.attributedText = attrStr
+        label.attributedText = attrStr
     }
     
     @IBAction private func click() {
         presentedViewController?.dismiss(animated: true, completion: nil)
     }
     
-    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("")
     }
 }
