e4fb2be08c330a68d125ce65426689402d31a03a
Mulang Su <sumulang@gmail.com>
2021-05-21 09:42:01 +0800

    exposed some C members through the bridging header

--- TestingArea/TestingArea-Bridging-Header.h
+++ TestingArea/TestingArea-Bridging-Header.h
@@ -1,4 +1,13 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
+#include <stdlib.h>
 
+typedef struct FooStruct {
+    uint8_t bar_value;
+} FooStruct;
+
+typedef struct FooArray {
+    struct FooStruct *foos;
+    uintptr_t foo_count;
+} FooArray;
+
+void print_foo_array(struct FooArray array);
+struct FooArray initialise_foo_array(void);
