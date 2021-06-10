79371cc0703d1010165d81c17dbc930bc9b23030
Mulang Su <sumulang@gmail.com>
2021-06-10 16:23:53 +0800

    tried writing an Objective-C protocol

--- TestingArea/TestingArea-Bridging-Header.h
+++ TestingArea/TestingArea-Bridging-Header.h
@@ -1,13 +1,7 @@
-#include <stdlib.h>
+#import <Foundation/Foundation.h>
 
-typedef struct FooStruct {
-    uint8_t bar_value;
-} FooStruct;
-
-typedef struct FooArray {
-    struct FooStruct *foos;
-    uintptr_t foo_count;
-} FooArray;
-
-void print_foo_array(struct FooArray array);
-struct FooArray initialise_foo_array(void);
+@protocol PrintProtocolDelegate
+- (void)processCompleted;
+- (void)didCloseWithTrailingMetadata:(nullable NSDictionary *)trailingMetadata
+                               error:(nullable NSError *)error;
+@end
