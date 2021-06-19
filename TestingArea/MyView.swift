73ac9fc0c63f97a863ca040ebbbd0e85ec410e46
Mulang Su <sumulang@gmail.com>
2021-06-19 10:29:28 +0800

    tried detecting motion in table cell

--- TestingArea/MyView.swift
+++ TestingArea/MyView.swift
@@ -1,5 +1,12 @@
 import UIKit
 
-class MyCell: UIView {
+class MyCell: UITableViewCell {
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        super.motionBegan(motion, with: event)
+        print("Moved!")
+    }
     
+    override var canBecomeFirstResponder: Bool {
+        true
+    }
 }
