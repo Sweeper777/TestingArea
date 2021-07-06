415da103d6b64f84c47c978ec82bf7d9aed8f5c6
Mulang Su <sumulang@gmail.com>
2021-07-06 11:20:30 +0800

    tried some layout in an SO question

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -3,17 +3,9 @@ import UIKit
 import SpriteKit
 
 class MyViewController: UIViewController {
-    @IBOutlet var twoCirlcesView: TwoCirclesView!
-    
-    var displayLink: CADisplayLink?
     
     override func viewDidLoad() {
 //        twoCirlcesView.roll = false
     }
     
-    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        displayLink?.invalidate()
-        displayLink = CADisplayLink(target: twoCirlcesView!, selector: #selector(twoCirlcesView.update))
-        displayLink?.add(to: .main, forMode: .common)
-    }
 }
