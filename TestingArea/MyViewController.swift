3fb7fc0b4b59e2bd4284f0c4d634360e261527f1
Mulang Su <sumulang@gmail.com>
2021-07-14 09:08:14 +0800

    simulated gravity

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,11 +1,23 @@
 
 import UIKit
-import SpriteKit
 
 class MyViewController: UIViewController {
     
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    var collision: UICollisionBehavior!
     override func viewDidLoad() {
-//        twoCirlcesView.roll = false
+        animator = UIDynamicAnimator(referenceView: view)
+        gravity = UIGravityBehavior(items: view.subviews)
+        animator.addBehavior(gravity)
+        collision = UICollisionBehavior(items: view.subviews)
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
+        let itemBehaviour = UIDynamicItemBehavior(items: view.subviews)
+        itemBehaviour.friction = 1
+        itemBehaviour.elasticity = 0.7
+        animator.addBehavior(itemBehaviour)
+        
     }
     
 }
