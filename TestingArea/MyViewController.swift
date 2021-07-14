ce1a2acd757b842f49b2503f31a981c569c95025
Mulang Su <sumulang@gmail.com>
2021-07-14 09:08:34 +0800

    made simulated gravity respond to phone's gravity

--- TestingArea/MyViewController.swift
+++ TestingArea/MyViewController.swift
@@ -1,11 +1,14 @@
 
 import UIKit
+import CoreMotion
 
 class MyViewController: UIViewController {
     
     var animator: UIDynamicAnimator!
     var gravity: UIGravityBehavior!
     var collision: UICollisionBehavior!
+    var motionManager: CMMotionManager!
+    
     override func viewDidLoad() {
         animator = UIDynamicAnimator(referenceView: view)
         gravity = UIGravityBehavior(items: view.subviews)
@@ -18,6 +21,15 @@ class MyViewController: UIViewController {
         itemBehaviour.elasticity = 0.7
         animator.addBehavior(itemBehaviour)
         
+        motionManager = CMMotionManager()
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates(to: .main) { motion, error in
+                guard let m = motion else {
+                    return
+                }
+                self.gravity.gravityDirection = CGVector(dx: m.gravity.x, dy: -m.gravity.y)
+            }
+        }
     }
     
 }
